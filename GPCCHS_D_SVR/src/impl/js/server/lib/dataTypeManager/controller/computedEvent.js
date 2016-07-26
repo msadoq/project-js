9d67fb080ebf020fe1430084e2d1920f4de1c04a
Matthieu BONNET <matthieu.bonnet@atos.net>
2016-07-26 13:49:14 +0200

    [FT:#3622] update dataTypes management for protobuf with correction

--- GPCCHS_D_SVR/src/impl/js/server/lib/dataTypeManager/controller/computedEvent.js
+++ GPCCHS_D_SVR/src/impl/js/server/lib/dataTypeManager/controller/computedEvent.js
@@ -6,7 +6,7 @@ const ComputedEvent = JS.ComputedEvent;
 exports.binToJson = (payload) => {
   const decoded = ComputedEvent.decode(payload);
   const computedEvent = {
-    eventDate: decoded.eventDate.value,    eventClass: decoded.eventClass,    systemDate: decoded.systemDate.value,    mission: decoded.mission.value,    source: decoded.source.value,    satellite: decoded.satellite.value,    specificAttributes: decoded.specificAttributes.value,    producer: decoded.producer.value
+    eventDate: decoded.eventDate.value,    systemDate: decoded.systemDate.value,    eventClass: decoded.eventClass,    mission: decoded.mission.value,    source: decoded.source.value,    satellite: decoded.satellite.value,    specificAttributes: decoded.specificAttributes.value,    producer: decoded.producer.value
   };
   return computedEvent;
 };
--- GPCCHS_D_SVR/src/impl/js/server/lib/dataTypeManager/controller/userEvent.js
+++ GPCCHS_D_SVR/src/impl/js/server/lib/dataTypeManager/controller/userEvent.js
@@ -6,7 +6,7 @@ const UserEvent = JS.UserEvent;
 exports.binToJson = (payload) => {
   const decoded = UserEvent.decode(payload);
   const userEvent = {
-    eventDate: decoded.eventDate.value,    userProfile: decoded.userProfile.value,    systemDate: decoded.systemDate.value,    specificAttributes: decoded.specificAttributes.value,    mission: decoded.mission.value,    satellite: decoded.satellite.value,    producer: decoded.producer.value
+    eventDate: decoded.eventDate.value,    systemDate: decoded.systemDate.value,    userProfile: decoded.userProfile.value,    specificAttributes: decoded.specificAttributes.value,    mission: decoded.mission.value,    satellite: decoded.satellite.value,    producer: decoded.producer.value
   };
   return userEvent;
 };

360501ecf556e733d1d1bf9e92a4d26c8591b8e4
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-08-29 13:36:32 +0000

    [FT:#3622] Re-add adelamare DC communication test scripts

---
+++ src/impl/js/proto/tests_modules/zmqProvider/lib/zmqTestDcOneFrame.js
@@ -0,0 +1,64 @@
+
+
+
+var zmq = require("zmq"),
+    socketPull = zmq.socket("pull"),
+    protoBuf = require("protobufjs");
+
+const fs = require('fs'),
+    path = require('path');
+
+
+
+
+var builder = protoBuf.newBuilder();
+
+const { join } = require('path');
+let buildProtobuf = (builder, ...protofiles) => {
+    protofiles.map( file => protoBuf.loadProtoFile({root: join(__dirname, '../protobuf'), file : file}, builder));
+}
+
+buildProtobuf(builder,  "dataControllerUtils/NewDataMessage.proto");
+
+var root = builder.build("dataControllerUtils");
+
+let {NewDataMessage} = root.protobuf;
+
+
+var JS = require("../files/reportingParameter.proto.js"); 
+var ReportingParameter = JS.ReportingParameter;
+var Protobuf = JS.protobuf;
+var Header = require("../files/header.proto.js");
+
+var onMessage = function (header,msg) {
+    //MSG[0] IS MESSAGE HEADER, IGNORE IT.
+    console.log(msg);
+    
+    //MSG[0] is now the DataId header, decode it.
+    var newData = NewDataMessage.decode(msg);
+    console.log(newData.dataId);
+    newData.payloads.forEach( p => { 
+        var param = ReportingParameter.decode(p.payload);
+        var timestamp = p.timestamp;
+        console.log(param); //deserialized param occurence
+        console.log(timestamp); //Long
+    });
+    
+    
+    
+}
+
+console.log("CONNECT socketIn");
+
+var port= 49165; 
+var uri = 'tcp://127.0.0.1:'+port; 
+console.log(uri); 
+let socketConnect = socketPull.bind(uri);
+socketConnect.on("message",onMessage);
+
+function sendMessage (header, payload) {
+    //console.log("Sending " + header);
+    socketOut.send([header, payload]);
+}
+
+

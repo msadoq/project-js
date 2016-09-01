1292f4f2fdc61244fdbfb8596648c586bdea424e
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-09-01 15:40:40 +0000

    [FT:#3622] Comment some obsolete code files

--- GPCCHS_D_SVR/src/impl/js/server/test/models/cacheBinary.js
+++ GPCCHS_D_SVR/src/impl/js/server/test/models/cacheBinary.js
@@ -1,34 +1,34 @@
-require('../../lib/utils/test');
-const model = require('../../lib/models/cacheBinary');
-const { getDataId, getReportingParameter } = require('../../lib/utils/stubData');
-const protobuf = require('../../lib/protobuf');
-
-describe('models/cacheBinary', () => {
-  beforeEach(() => {
-    model.clear();
-  });
-
-  const dataId = getDataId();
-  const now = Date.now();
-  const payload = protobuf.encode(
-    'lpisis.decommutedParameter.ReportingParameter',
-    getReportingParameter()
-  );
-
-  describe('addRecord', () => {
-    it('one', () => {
-      model.addRecord(dataId, now, payload);
-      const records = model.find();
-      records.should.be.an('array').that.has.lengthOf(1);
-      records[0].should.be.an('object');
-      records[0].should.have.property('localId', model.getLocalId(dataId));
-      records[0].should.have.property('timestamp', now);
-      records[0].should.have.property('payload', payload);
-    });
-    it('multi', () => {
-      model.addRecord(dataId, now, payload);
-      model.addRecord(dataId, now, payload);
-      model.count().should.equal(2);
-    });
-  });
-});
+// require('../../lib/utils/test');
+// const model = require('../../lib/models/cacheBinary');
+// const { getDataId, getReportingParameter } = require('../../lib/stubs/data');
+// const protobuf = require('../../lib/protobuf');
+//
+// describe('models/cacheBinary', () => {
+//   beforeEach(() => {
+//     model.clear();
+//   });
+//
+//   const dataId = getDataId();
+//   const now = Date.now();
+//   const payload = protobuf.encode(
+//     'lpisis.decommutedParameter.ReportingParameter',
+//     getReportingParameter()
+//   );
+//
+//   describe('addRecord', () => {
+//     it('one', () => {
+//       model.addRecord(dataId, now, payload);
+//       const records = model.find();
+//       records.should.be.an('array').that.has.lengthOf(1);
+//       records[0].should.be.an('object');
+//       records[0].should.have.property('localId', model.getLocalId(dataId));
+//       records[0].should.have.property('timestamp', now);
+//       records[0].should.have.property('payload', payload);
+//     });
+//     it('multi', () => {
+//       model.addRecord(dataId, now, payload);
+//       model.addRecord(dataId, now, payload);
+//       model.count().should.equal(2);
+//     });
+//   });
+// });

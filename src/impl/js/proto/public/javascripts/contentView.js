e5a8e69476b0e6da399c86526b84a1519941a4c2
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-09-13 07:02:01 +0000

    [FT:#3622] Change the project hierarchy to match the LPISIS component structure.

--- src/impl/js/proto/public/javascripts/contentView.js
+++
@@ -1,4 +0,0 @@
-var React = require('react');
-var ReactBsTable = require('react-bootstrap-table');
-var BootstrapTable = ReactBsTable.BootstrapTable;
-var TableHeaderColumn = ReactBsTable.TableHeaderColumn;

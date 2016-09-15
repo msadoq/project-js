e002c8614ff70db6507e7d2999c29e79a269f1c5
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-09-15 18:06:14 +0000

    [FT:#3622] Comment uncomplete test file in HSS

--- GPCCHS_D_SVR/src/server/src/impl/js/server/lib/controllers/onConnectedDataOpen.spec.js
+++ GPCCHS_D_SVR/src/server/src/impl/js/server/lib/controllers/onConnectedDataOpen.spec.js
@@ -1,17 +1,17 @@
-const { should } = require('../utils/test');
-// const protobuf = require('./index');
-// const stubData = require('../stubs/data');
-const onConnectedDataOpen = require('./onConnectedDataOpen');
-
-describe.only('controllers:onConnectedDataOpen', () => {
-  it('subscribe', () => {
-    console.log(
-      onConnectedDataOpen({}, {
-        formula: 'Reporting.ATT_BC_STR1VOLTAGE<ReportingParameter>.convertedValue',
-        domain: '.*',
-        timeline: 'Session 1',
-        // filter: [],
-      })
-    );
-  });
-});
+// const { should } = require('../utils/test');
+// // const protobuf = require('./index');
+// // const stubData = require('../stubs/data');
+// const onConnectedDataOpen = require('./onConnectedDataOpen');
+//
+// describe.only('controllers:onConnectedDataOpen', () => {
+//   it('subscribe', () => {
+//     console.log(
+//       onConnectedDataOpen({}, {
+//         formula: 'Reporting.ATT_BC_STR1VOLTAGE<ReportingParameter>.convertedValue',
+//         domain: '.*',
+//         timeline: 'Session 1',
+//         // filter: [],
+//       })
+//     );
+//   });
+// });

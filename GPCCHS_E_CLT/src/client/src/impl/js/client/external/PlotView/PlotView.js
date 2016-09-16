09292fae0754600a42a8232ad24aa7c2a613d129
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-09-16 14:27:31 +0000

    [FT:#3622] Refactor view type module abstraction

--- GPCCHS_E_CLT/src/client/src/impl/js/client/external/PlotView/PlotView.js
+++ GPCCHS_E_CLT/src/client/src/impl/js/client/external/PlotView/PlotView.js
@@ -1,3 +1,34 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import _ from 'lodash';
 
-module.exports = () => <div>I'm a plotview</div>;
+export default class PlotView extends Component {
+  static propTypes = {
+    type: React.PropTypes.string.isRequired,
+    viewId: PropTypes.string.isRequired,
+    configuration: PropTypes.object.isRequired,
+    state: PropTypes.object,
+    openEditor: PropTypes.func,
+    closeEditor: PropTypes.func,
+    unmountAndRemove: PropTypes.func,
+  };
+  render() {
+    const entryPoints = _.get(this.props, 'configuration.plotViewEntryPoints');
+    if (!entryPoints || !entryPoints.length) {
+      return <div>invalid configuration for plot</div>; // TODO
+    }
+
+    const entryPointsText = _.map(entryPoints, ep => {
+      const x = this.props.state.connectedData[ep.connectedDataX.uuid];
+      const y = this.props.state.connectedData[ep.connectedDataY.uuid];
+      return `x:${x.formula} /// y:${y.formula}`;
+    });
+
+    return (
+      <div>
+        {this.props.type}
+        <br />
+        {entryPointsText}
+      </div>
+    );
+  }
+}

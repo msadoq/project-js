005dc5c08718e2d48b1748916dba19015332bcc1
Alexandra HERY <alexandra.hery.external@atos.net>
2016-09-16 12:22:53 +0000

    [FT:#3622]tests index documents client FIN

--- GPCCHS_E_CLT/src/client/src/impl/js/client/app/documents/documents.spec.js
+++ GPCCHS_E_CLT/src/client/src/impl/js/client/app/documents/documents.spec.js
@@ -1,4 +1,7 @@
 const documents = require('./index');
+const {
+  should
+} = require('../utils/test');
 
 const pathTestFMD = '/data/work/gitRepositories/LPISIS/GPCCHS/' +
   'GPCCHS_E_CLT/src/client/src/impl/js/client/data/' +
@@ -30,26 +33,64 @@ describe('functions documents/index', () => {
       documents.isReadable(documents.resolve('dev.workspace.json'), readable => {
         readable.should.equal(true);
         done();
-      })
+      });
     });
     it('isReadable: false', done => {
       documents.isReadable(documents.resolve('dev.workspace0.json'), readable => {
         readable.should.equal(false);
         done();
-      })
+      });
     });
   });
-  // describe('function < read >', () => {
-  //   it('readToFile works', done => {
-  //     documents.read(readToFile => {
-  //       readToFile.should.equal(documents.resolve('dev.workspace.json'));
-  //       readToFile.should.equal(documents.isExists(true));
-  //       readToFile.should.equal(documents.isReadable(true));
-  //       readToFile.should.be.an('object').and.have.property('data').that.is.an('object');
-  //       readToFile.data.should.have.property('content');
-  //       done();
-  //     })
-  //   });
+  describe('function < read >', () => {
+    it('readToFile works', done => {
+      documents.read(documents.resolve('dev.workspace.json'), (err, content) => {
+        should.not.exist(err);
+        should.exist(content);
 
-  // });
+        done();
+      });
+    });
+
+    it('readToFile error', done => {
+      documents.read(documents.resolve('dev.workspace0.json'), (err, content) => {
+        should.exist(err);
+        should.not.exist(content);
+        done();
+      });
+    });
+  });
+  describe('function < parse >', () => {
+    it('parseContent OK', done => {
+      documents.parse(documents.resolve('dev.workspace.json'), content => {
+        should.exist(content);
+        done();
+      });
+    });
+    it('parseContent err', done => {
+      documents.parse(documents.resolve('dev.workspace.json'), e => {
+        should.exist(e);
+        done();
+      });
+    });
+  });
+  describe('function < readJsonFromPath >', () => {
+    it('function works', done => {
+      documents.readJsonFromPath('dev.workspace.json', (err, content) => {
+        should.not.exist(err);
+        should.exist(content);
+        content.should.have.all.keys(['type', 'timeBarWindow', 'windows']);
+        content.timeBarWindow.should.be.an('object');
+        content.windows.should.be.an('array');
+        done();
+      });
+    });
+    it('readJsonFromPath error', done => {
+      documents.readJsonFromPath('dev.workspace.json', (err, content) => {
+        should.exist(err);
+        should.not.exist(content);
+        done();
+      });
+    });
+  });
 });
--- GPCCHS_E_CLT/src/client/src/impl/js/client/app/documents/index.js
+++ GPCCHS_E_CLT/src/client/src/impl/js/client/app/documents/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
-const { join } = require('path');
+const {
+  join
+} = require('path');
 
 // TODO test
 
@@ -10,6 +12,7 @@ const self = module.exports = {
   isExists: (path, callback) => fs.access(path, fs.constants.F_OK, err => callback(!err)),
   isReadable: (path, callback) => fs.access(path, fs.constants.R_OK, err => callback(!err)),
   read: (path, callback) => {
+
     self.isExists(path, exists => {
       if (!exists) {
         return callback(new Error(`File '${path}' doesn't exist`));

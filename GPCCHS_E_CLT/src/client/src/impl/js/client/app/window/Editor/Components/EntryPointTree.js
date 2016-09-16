db1974d370a4180b50a035fd9bea1a69600abbd4
Alexis BESSON <alexis.besson@atos.net>
2016-09-16 13:26:30 +0200

    [HL] debug attribute's name in PlotEditor

--- GPCCHS_E_CLT/src/client/src/impl/js/client/app/window/Editor/Components/EntryPointTree.js
+++ GPCCHS_E_CLT/src/client/src/impl/js/client/app/window/Editor/Components/EntryPointTree.js
@@ -20,18 +20,20 @@ export default class EntryPointTree extends React.Component {
   }
   render() {
     const mask = `${this.props.search}.*`;
-    const EntryPointsName = this.props.entryPoints.map((entryPoint, key_) =>
-      ((entryPoint.name.match(mask)) ?
-        <EntryPointDetails
-          key={key_}
-          idPoint={key_}
-          entryPoint={entryPoint}
-          handleEntryPoint={this.props.handleEntryPoint}
-          remove={this.props.remove}
-        /> :
-        null
-      )
-    );
+    const EntryPointsName = (this.props.entryPoints && this.props.entryPoints.length > 0) ?
+      this.props.entryPoints.map((entryPoint, key_) =>
+        ((entryPoint.name.match(mask)) ?
+          <EntryPointDetails
+            key={key_}
+            idPoint={key_}
+            entryPoint={entryPoint}
+            handleEntryPoint={this.props.handleEntryPoint}
+            remove={this.props.remove}
+          /> :
+          null
+        )
+      ) :
+      <div>No entryPoints to display</div>;
     return (
       <div className={styles.entryPointsTree}>
         {EntryPointsName}

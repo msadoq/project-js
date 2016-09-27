5c4d6df9a4e9d725fc7b401e41252c921a9e1e17
Alexandra HERY <alexandra.hery.external@atos.net>
2016-09-27 14:47:08 +0000

    [FT:#3622] implement a page 'no page' in empty window --> CSS

---
+++ GPCCHS_E_CLT/src/client/src/impl/js/client/app/window/Websocket/StatusDisconnect.js
@@ -0,0 +1,14 @@
+// import React, { Component, PropTypes } from 'react';
+// // import { connect, disconnect, getWebsocket } from '../../websocket/windowWebsocket';
+//
+// export default class Disconnect extends Component {
+//   static propTypes = {
+//     type: React.PropTypes.string,
+//     windowId: PropTypes.string.isRequired,
+//   };
+//   render() {
+//     return (
+//       <div> Window #{this.props.windowId} has disconnected type: '{this.props.type}'</div>
+//     );
+//   }
+// }

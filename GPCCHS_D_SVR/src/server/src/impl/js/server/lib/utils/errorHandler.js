0615282308b586b8ba60e160dd5d977707f6f5d7
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-09-28 17:46:23 +0000

    [FT:#3622] Add timing log on errorHandler helper

--- GPCCHS_D_SVR/src/server/src/impl/js/server/lib/utils/errorHandler.js
+++ GPCCHS_D_SVR/src/server/src/impl/js/server/lib/utils/errorHandler.js
@@ -1,12 +1,23 @@
+const _ = require('lodash');
 const debug = require('../io/debug')('utils:errorHandler');
+const monitoring = require('../io/debug')('utils:monitoring');
 
-module.exports = (name, func) => {
+module.exports = (name, thunk) => {
+  let start;
+  if (process.env.MONITORING === 'on') {
+    start = process.hrtime();
+  }
+
   try {
-    const start = process.hrtime();
-    func();
-    const stop = process.hrtime(start);
-    debug.debug(`${name} execution time`, stop);
+    thunk();
   } catch (e) {
     debug.error(e);
+  }
+
+  if (process.env.MONITORING === 'on') {
+    let duration = process.hrtime(start);
+    duration = (duration[0] * 1e3) + _.round(duration[1] / 1e6, 6);
+    const method = duration >= 1e2 ? 'error' : 'info';
+    monitoring[method](`${name} execution time ${duration} ms`);
   }
 };

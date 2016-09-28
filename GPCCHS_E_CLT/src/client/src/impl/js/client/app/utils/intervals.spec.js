fe449f9fbc2a944c1d9656111c29b663a188600a
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-09-28 08:38:23 +0000

    [FT:#3622] Add simple interval computation component on HSC

---
+++ GPCCHS_E_CLT/src/client/src/impl/js/client/app/utils/intervals.spec.js
@@ -0,0 +1,32 @@
+import { should } from './test';
+import missingIntervals from './intervals';
+
+describe('utils/intervals', () => {
+  const interval = [10, 20];
+  it('no previous', () => {
+    missingIntervals([], [10, 20]).should.eql([[10, 20]]);
+  });
+  it('same', () => {
+    missingIntervals(interval, [10, 20]).should.eql([]);
+  });
+  it('in', () => {
+    missingIntervals(interval, [10, 11]).should.eql([]);
+    missingIntervals(interval, [19, 20]).should.eql([]);
+    missingIntervals(interval, [14, 16]).should.eql([]);
+  });
+  it('fully outside', () => {
+    missingIntervals(interval, [1, 9]).should.eql([[1, 9]]);
+    missingIntervals(interval, [21, 30]).should.eql([[21, 30]]);
+  });
+  it('around', () => {
+    missingIntervals(interval, [9, 21]).should.eql([[9, 10], [20, 21]]);
+  });
+  it('lap before', () => {
+    missingIntervals(interval, [9, 11]).should.eql([[9, 10]]);
+    missingIntervals(interval, [9, 10]).should.eql([[9, 10]]);
+  });
+  it('lap after', () => {
+    missingIntervals(interval, [19, 21]).should.eql([[20, 21]]);
+    missingIntervals(interval, [20, 21]).should.eql([[20, 21]]);
+  });
+});

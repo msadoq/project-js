8a2fd3c85edb7a365e400fa5ce9f958cd1e86312
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-10-04 13:36:46 +0000

    [HL] . . . . .

--- GPCCHS_D_SVR/src/server/src/impl/js/server/lib/protobuf/index.js
+++ GPCCHS_D_SVR/src/server/src/impl/js/server/lib/protobuf/index.js
@@ -117,4 +117,11 @@ module.exports = {
       ? removeEmpty(builder.mapper.decode(raw))
       : removeEmpty(raw);
   },
+  decodeFaster: (type, buffer) => {
+    const builder = getType(type);
+    const message = builder.decode(buffer);
+    return builder.mapper
+      ? builder.mapper.decode(message)
+      : message;
+  },
 };
---
+++ GPCCHS_D_SVR/src/server/src/impl/js/server/protobufProfiling.js
@@ -0,0 +1,32 @@
+const stubs = require('./lib/stubs/data');
+const protobuf = require('./lib/protobuf');
+
+const payloads = [];
+for (let i = 0; i < 1000; i += 1) {
+  payloads.push(stubs.getDataPayload());
+}
+
+const newDataMessage = stubs.getNewDataMessageProtobuf({
+  payloads,
+});
+const dcMessage = stubs.getWrappedNewDataMessageProtobuf({
+  messageType: 'NEW_DATA_MESSAGE',
+  payload: newDataMessage,
+});
+
+function fire(method) {
+  const m = protobuf.decode('dc.dataControllerUtils.DcServerMessage', dcMessage);
+  const newDataMessageIncoming = protobuf.decode('dc.dataControllerUtils.NewDataMessage', m.payload);
+  const start = process.hrtime();
+
+  for (let i = 0; i < newDataMessageIncoming.payloads.length; i += 1) {
+    protobuf[method]('lpisis.decommutedParameter.ReportingParameter', newDataMessageIncoming.payloads[i].payload);
+  }
+
+  const duration = process.hrtime(start);
+  console.log(duration[0], Math.floor(duration[1] / 1e6), 's');
+}
+
+fire('decode');
+fire('decodeFaster');
+

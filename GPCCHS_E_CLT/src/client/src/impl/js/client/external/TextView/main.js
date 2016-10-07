3df26846c8fd21c44518369c542c9a9ba8874481
Audrey LEAL <audrey.leal.external@atos.net>
2016-10-07 12:41:04 +0000

    [FT:#3622] Reducer and selector for dataCache

--- GPCCHS_E_CLT/src/client/src/impl/js/client/external/TextView/main.js
+++ GPCCHS_E_CLT/src/client/src/impl/js/client/external/TextView/main.js
@@ -43,4 +43,26 @@ module.exports = {
         : list;
     } , []);
   },
+  getUsedValues: function(stateLocalId, field, visuWindow, offset, remoteIdPayload) {
+    const current = visuWindow.current - offset;
+
+    let final;
+    if (stateLocalId) {
+      final = Object.assign({}, stateLocalId);
+    } else {
+      final = { data: {}, index: []};
+    }
+
+    _.each(remoteIdPayload, (val, time) => {
+      if (time <= current) {
+        const oldTime = _.head(Object.keys(final.data));
+        if (!oldTime || oldTime < time) {
+          final.data = {};
+          final.data[time] = val[field] ;
+          final.index = [ time ];
+        }
+      }
+    });
+    return final;
+  }
 };

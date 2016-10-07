5ada731e913b5e58e0919f0b92e8171afbb66e13
Audrey LEAL <audrey.leal.external@atos.net>
2016-10-07 15:57:36 +0000

    [FT:#3622] Fix bug following file renaming

--- GPCCHS_E_CLT/src/client/src/impl/js/client/app/store/observers/data.js
+++ GPCCHS_E_CLT/src/client/src/impl/js/client/app/store/observers/data.js
@@ -1,6 +1,6 @@
 import * as constants from '../../constants';
 import debug from '../../utils/mainDebug';
-import { getStatus as getAppStatus } from '../mutations/hscReducer';
+import { getStatus as getAppStatus } from '../selectors/hsc';
 
 const logger = debug('store:observers:data');
 

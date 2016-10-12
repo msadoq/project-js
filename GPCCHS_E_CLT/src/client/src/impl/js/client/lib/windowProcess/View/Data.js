5c23469f53415712264d2aee8307902b3db22c1d
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-10-12 16:44:59 +0000

    [FT:#3622] Prepare dataCache retrieving for view implementation

--- GPCCHS_E_CLT/src/client/src/impl/js/client/lib/windowProcess/View/Data.js
+++ GPCCHS_E_CLT/src/client/src/impl/js/client/lib/windowProcess/View/Data.js
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 import { getTimebar } from '../../store/selectors/timebars';
-import { makeEntryPointsMap } from '../../mainProcess/data/dataMap';
+import makeEntryPointsMap from '../../mainProcess/data/entryPointsMap';
 
 const selector = makeEntryPointsMap();
 
@@ -22,19 +22,20 @@ const mapStateToProps = (state, ownProps) => {
   const timebar = getTimebar(state, ownProps.timebarId);
 
   // data
-  const cds = selector(state, ownProps);
-  const data = _.reduce(cds, (list, { localIds }, remoteId) => {
-    return _.reduce(localIds, (l, localIdData, localId) => {
-      return _.set(
-        list,
-        [remoteId, localId, 'data'],
-        _.get(state, ['dataCache', remoteId, localId, 'data'], [])
-      );
-    }, list)
-  }, {});
+  const map = selector(state, ownProps);
+  const data = {};
+  // const cds = selector(state, ownProps);
+  // const data = _.reduce(cds, (list, { localIds }, remoteId) => {
+  //   return _.reduce(localIds, (l, localIdData, localId) => {
+  //     return _.set(
+  //       list,
+  //       [remoteId, localId, 'data'],
+  //       _.get(state, ['dataCache', remoteId, localId, 'data'], [])
+  //     );
+  //   }, list)
+  // }, {});
 
   return {
-    ...ownProps,
     interval: timebar.visuWindow,
     data,
   };

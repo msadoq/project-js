c1b48a7c75a559b5c930676ed1cfe0d3c57e54fc
Aziz Gharbi <aziz.gharbi@atos.net>
2016-10-12 07:49:28 +0000

    [HL]Updating the lokitest to fix it

--- GPCCHS_D_SVR/src/server/src/impl/js/server/perfTesting/lokiTest.js
+++ GPCCHS_D_SVR/src/server/src/impl/js/server/perfTesting/lokiTest.js
@@ -1,136 +1,88 @@
 const databaseLokiv1 = require('../lib/io/loki');
 const Loki = require('lokijs');
 const databaseLokiv2 = new Loki('test.db');
+const async = require('async');
 
-
-var v1 = databaseLokiv1.addCollection('randomData');
-var v2 = databaseLokiv2.addCollection('randomData');
-var v1Indexed = databaseLokiv1.addCollection('randomData', { indices: ['timestamp'] });
-var v2Indexed = databaseLokiv2.addCollection('randomData', { indices: ['timestamp'] });
-var v1adptive = databaseLokiv1.addCollection('randomData', { adaptiveBinaryIndices: true, indices: ['timestamp'] });
-var v2adptive = databaseLokiv2.addCollection('randomData', { adaptiveBinaryIndices: true, indices: ['timestamp'] });
-let initTime = 3568;
+const NUMBER_OF_OPERATIONS = 1e6;
+const NUMBER_OF_ENTRYS = 1e6;
+const DELAY = 500;
+// TODO const
+const v1 = databaseLokiv1.addCollection('randomData');
+const v2 = databaseLokiv2.addCollection('randomData');
+const v1Indexed = databaseLokiv1.addCollection('randomData', { indices: ['timestamp'] });
+const v2Indexed = databaseLokiv2.addCollection('randomData', { indices: ['timestamp'] });
+const v2Adptive = databaseLokiv2.addCollection('randomData', { adaptiveBinaryIndices: true, indices: ['timestamp'] });
+const initTime = 3568;
 let tstamp = 0;
 
-function genRandomVal() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-for (var i = 0; i < 20; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  return text;
-}
-
-function generateCollection(collection, numberOfEntrtys){
-  for (let i = 1; i < numberOfEntrtys; i++) {
+function generateCollection(collection) {
+  for (let i = 1; i < NUMBER_OF_ENTRYS; i++) {
     if (i % 3 === 0) {
       tstamp = i * initTime;
       collection.insert({
         timestamp: tstamp,
-        stringData: genRandomVal()
+        stringData: `id${i}`
       });
     }
     if (i % 2 === 0) {
       tstamp = initTime / i;
       collection.insert({
         timestamp: tstamp,
-        stringData: genRandomVal()
+        stringData: `id${i}`
       });
     }
     else {
-      tstamp = initTime + i*i;
+      tstamp = initTime + i * i;
       collection.insert({
         timestamp: tstamp,
-        stringData: genRandomVal()
+        stringData: `id${i}`
       });
     }
   }
 }
 
-function testWR(collection, numberOfOperations){
-  for (let i = 1; i <= numberOfOperations; i++){
-    if(i%2 === 0){
+function testWR(collection){
+  for (let i = 1; i <= NUMBER_OF_OPERATIONS; i++){
+    if (i%2 === 0){
       collection.insert({
         timestamp: i * 10,
-        stringData: genRandomVal()
+        stringData: `id${i}`,
       });
-    }
-    else {
-        var results = collection.where(function(obj) {
-          return (obj.timestamp >= 35 + i && obj.timestamp <=  50 + i);
-        });
+    } else {
+      const query = {
+        $and: [
+          // { remoteId },
+          { timestamp: { $gte: i * 1000 - i * 250 } },
+          { timestamp: { $lte: i * 1000 } },
+        ],
+      };
+
+      return collection.find(query);
     }
   }
 }
 
-generateCollection(v1adptive);
-generateCollection(v2adptive);
+
+function testTime(testname, collection, callback) {
+  const startTime = process.hrtime();
+  for (let i = 0 ;i < 10 ; i++){
+    testWR(collection, NUMBER_OF_OPERATIONS);
+  }
+  const stoptime = process.hrtime(startTime);
+  const timeinSec = (stoptime[0]+stoptime[1]*1e-9);
+  console.log(testname, ':', timeinSec);
+  callback(null);
+}
+generateCollection(v1);
+generateCollection(v2);
+generateCollection(v2Adptive);
 generateCollection(v1Indexed);
 generateCollection(v2Indexed);
 
-let startTime = process.hrtime();
-
-for (let i = 0 ;i < 100 ;i++){
-  testWR(v1adptive, 100);
-
-}
-let stoptime = process.hrtime(startTime);
-let timeinSec = (stoptime[0]+stoptime[1]*1e-9)/100;
-
-console.log("v1 with index and Adaptative", timeinSec);
-
-
-/////////////////////////////////////////////////////
-startTime = process.hrtime();
-for (let i = 0 ;i < 100 ;i++){
-  testWR(v2adptive, 100);
-
-}
-stoptime = process.hrtime(startTime);
-timeinSec = (stoptime[0]+stoptime[1]*1e-9)/100;
-
-console.log("v2 with index and Adaptative", timeinSec);
-
-/////////////////////////////////////////////////////
-startTime = process.hrtime();
-for (let i = 0 ;i < 100 ;i++){
-  testWR(v1Indexed, 100);
-
-}
-stoptime = process.hrtime(startTime);
-timeinSec = (stoptime[0]+stoptime[1]*1e-9)/100;
-
-console.log("v1 with index", timeinSec);
-
-/////////////////////////////////////////////////////
-startTime = process.hrtime();
-for (let i = 0 ;i < 100 ;i++){
-  testWR(v2Indexed, 100);
-
-}
-stoptime = process.hrtime(startTime);
-timeinSec = (stoptime[0]+stoptime[1]*1e-9)/100;
-console.log("v2 with index", timeinSec);
-
-/////////////////////////////////////////////////////
-startTime = process.hrtime();
-for (let i = 0 ;i < 100 ;i++){
-  testWR(v1, 100);
-
-}
-stoptime = process.hrtime(startTime);
-timeinSec = (stoptime[0]+stoptime[1]*1e-9)/100;
-
-console.log("v1", timeinSec);
-
-/////////////////////////////////////////////////////
-startTime = process.hrtime();
-for (let i = 0 ;i < 100 ;i++){
-  testWR(v2, 100);
-
-}
-stoptime = process.hrtime(startTime);
-timeinSec = (stoptime[0]+stoptime[1]*1e-9)/100;
-
-console.log("v2", timeinSec);
+async.series([
+  cb => setTimeout(() => testTime('v1', v1, cb), DELAY),
+  cb => setTimeout(() => testTime('v2', v2, cb), DELAY),
+  cb => setTimeout(() => testTime('v1 Indexed', v1Indexed, cb), DELAY),
+  cb => setTimeout(() => testTime('v2 Indexed', v2Indexed, cb), DELAY),
+  cb => setTimeout(() => testTime('v2 Indexed with adaptativeBinaryIndices', v2Adptive, cb), DELAY),
+], () => console.log('all done'));

3aba31e7ed1b3c3e6da5a3a294be1095bcadbb38
Renaud PERROT <renaud.perrot@atos.net>
2016-10-14 15:39:09 +0000

    [HL] fix the corrected code .

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/windowProcess/Window/WindowContainer.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/windowProcess/Window/WindowContainer.js
@@ -12,10 +12,11 @@ const WindowContainer = props => <Window {...props} />;
 
 const makeMapStateToProps = () => {
   const focusedPageSelector = makeGetWindowFocusedPage();
-  return (state, props) =>
-      windowId: props.windowId;
-      focusedPageId: getWindowFocusedPageId(state, props);
-      focusedPage: focusedPageSelector(state, props);
+  return (state, props) => ({
+    windowId: props.windowId,
+    focusedPageId: getWindowFocusedPageId(state, props),
+    focusedPage: focusedPageSelector(state, props),
+  });
 };
 
 export default connect(makeMapStateToProps)(WindowContainer);

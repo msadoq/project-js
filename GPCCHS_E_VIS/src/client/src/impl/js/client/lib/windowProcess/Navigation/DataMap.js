e55dfb67977bc2e6ed3256091e72290c01a4eaa2
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-10-14 15:57:46 +0000

    [HL] . . . . .

---
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/windowProcess/Navigation/DataMap.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import dataMap from '../../mainProcess/data/dataMap';
+
+export default class Timebar extends Component {
+  static propTypes = {};
+  static contextTypes = {
+    store: React.PropTypes.object.isRequired,
+  };
+  display() {
+    const state = this.context.store.getState();
+    return console.log(dataMap(state));
+  }
+  render() {
+    return (
+      <div style={{ display: 'inline' }}>
+        <Button onClick={this.display.bind(this)}>DATA MAP</Button>
+      </div>
+    );
+  }
+}
--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/windowProcess/Navigation/Navigation.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/windowProcess/Navigation/Navigation.js
@@ -1,18 +1,21 @@
-import React  from 'react';
+import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 
 import TabsContainer from './TabsContainer';
 import Debug from './Debug';
 import Timebar from './Timebar';
+import DataMap from './DataMap';
 
-const Navigation = (props) => (
+const Navigation = props => (
   <Grid fluid>
     <Row>
       <Col xs={12}>
-        <div style={{textAlign: 'right'}}>
-          <Debug  />
+        <div style={{ textAlign: 'right' }}>
+          <Debug />
           {' '}
           <Timebar />
+          {' '}
+          <DataMap />
         </div>
       </Col>
     </Row>

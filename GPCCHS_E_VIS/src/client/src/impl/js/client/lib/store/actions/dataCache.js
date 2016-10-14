d8327c978d534bbe74eaf72de6d7214dbd0e0c63
Alexandra HERY <alexandra.hery.external@atos.net>
2016-10-14 10:12:33 +0000

    [HL] corrected code to valid linter

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/actions/dataCache.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/actions/dataCache.js
@@ -9,11 +9,11 @@ import dataMap from '../../mainProcess/data/dataMap';
 export const writePayload = simple(types.DATA_IMPORT_PAYLOADS, 'payloads', 'remoteIds');
 
 export function importPayload(payload) {
-    return (dispatch, getState) => {
+  return (dispatch, getState) => {
       // make your data computing
-      const remoteIds = dataMap(getState());
-      dispatch(writePayload(payload, remoteIds));
-    }
+    const remoteIds = dataMap(getState());
+    dispatch(writePayload(payload, remoteIds));
+  }
 }
 
 

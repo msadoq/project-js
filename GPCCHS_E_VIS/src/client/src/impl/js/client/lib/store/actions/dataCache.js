59723ad0e0e852b12eb2f798056716abedbc02d4
Audrey LEAL <audrey.leal.external@atos.net>
2016-10-17 16:03:57 +0000

    [FT:#3622] Update of dataCache management .

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/actions/dataCache.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/actions/dataCache.js
@@ -1,13 +1,118 @@
+import _ from 'lodash';
 import simple from '../simpleActionCreator';
 import * as types from '../types';
 import dataMap from '../../mainProcess/data/dataMap';
+import debug from '../../common/debug/mainDebug';
+const logger = debug('store:action:dataCache');
+/**
+ * Simple actions
+ */
+export const writeOnePayload = simple(types.DATA_IMPORT_ONE_PAYLOAD, 'remoteId', 'localId', 'valuesToDisplay');
+export const writeRangePayloads = simple(types.DATA_IMPORT_RANGE_PAYLOADS, 'remoteId', 'localId',
+  'valuesToDisplay', 'interval');
 
-export const writePayload = simple(types.DATA_IMPORT_PAYLOADS, 'payloads', 'remoteIds');
+function rangeValues(rIdData, lIdParam) {
+  // rIdData = [ timestamp : value ]
+  // lIdParam =
+  // *          viewType: string,
+  // *          field: string,
+  // *          timebarId: string,
+  // *          offset: number,
+  // *          expectedInterval: [number, number],
+  const lower = lIdParam.expectedInterval[0];
+  const upper = lIdParam.expectedInterval[1];
+  const displayedData = {};
+  let lastIndex = 0;
+  let lastTimestamp = 0;
+  let indexes = [];
+  _.each(rIdData, (value) => {
+    const timestamp = value.timestamp;
+    if (timestamp < lower || timestamp > upper) {
+      return;
+    }
 
+
+    // add at the end
+    if ((displayedData.length === 0) ||
+      (timestamp > lastTimestamp && lastIndex === displayedData.length)) {
+      lastIndex = displayedData.length;
+      indexes.push(timestamp);
+    } else {  // insertion
+      const insertIndex = _.findIndex(indexes, time => time > timestamp,
+        timestamp > lastTimestamp? lastIndex: 0);
+      indexes = _.concat(_.slice(indexes, 0, insertIndex), timestamp, _.slice(indexes, insertIndex));
+      lastIndex = insertIndex;
+    }
+    displayedData[timestamp] = value.payload[lIdParam.field];
+    lastTimestamp = timestamp;
+  });
+  return displayedData;
+}
+
+function oneValue(rIdData, lIdParam, stateLocalId) {
+  let displayedData;
+  const lower = lIdParam.expectedInterval[0];
+  const upper = lIdParam.expectedInterval[1];
+  _.each(rIdData, (value) => {
+    const timestamp = value.timestamp;
+    if (timestamp < lower || timestamp > upper) {
+      return;
+    }
+
+    const data = value.payload[lIdParam.field];
+    if (!displayedData ) {
+      if (!stateLocalId || (stateLocalId && timestamp >= stateLocalId.timestamp)) {
+        displayedData = Object.assign({}, { timestamp: timestamp }, { value: data });
+      }
+    } else if (timestamp >= displayedData.timestamp) {
+      displayedData = Object.assign({}, { timestamp: timestamp }, { value: data });
+    }
+  });
+  return displayedData;
+}
+// dataCache: {
+//  'remoteId': {
+//    'localId': { timestamp: 'timestamp', value: number }, // Text
+//    'localId': {
+//      index: [timestamp],
+//      data: { 'timestamp': number }, // Plot
+//    },
+//  }
+// }
 export function importPayload(payload) {
+  logger.debug('importPayload');
   return (dispatch, getState) => {
     // make your data computing
     const remoteIds = dataMap(getState());
-    dispatch(writePayload(payload, remoteIds));
+    const valuesToDisplay = {};
+    // Loop on remote IDs
+    _.each(payload, (rIdData, remoteId) => {
+      if (!remoteIds[remoteId]) {
+        return;
+      }
+      // loop on local IDs
+      _.each(remoteIds[remoteId].localIds, (lIdParam, localId) => {
+        switch (lIdParam.viewType) {
+          case 'PlotView': {
+            const displayedData = rangeValues(rIdData, lIdParam);
+            if (Object.keys(displayedData) === 0) {
+              return;
+            }
+            dispatch(writeRangePayloads(remoteId, localId, displayedData, lIdParam.expectedInterval));
+            break;
+          }
+          case 'TextView': {
+            const stateLocalId = _.get(getState(), ['dataCache', remoteId, localId], undefined);
+            const displayedData = oneValue(rIdData, lIdParam, stateLocalId);
+            if (!displayedData) {
+              return;
+            }
+            dispatch(writeOnePayload(remoteId, localId, displayedData));
+            break;
+          }
+          default :
+        }
+      });
+    });
   };
 }
--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/reducers/dataCache.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/reducers/dataCache.js
@@ -1,29 +1,60 @@
 import _ from 'lodash';
+import u from 'updeep';
 import * as types from '../types';
-import vivl from '../../../VIVL/main';
+import debug from '../../common/debug/mainDebug';
+const logger = debug('store:reducers:dataCache');
+
 
 export default function dataCache(stateDataCache = {}, action) {
   switch (action.type) {
-    case types.DATA_IMPORT_PAYLOADS: {
-      const newState = {};
-
-      // Loop on action.payload.payload => (p, k)
-      _.each(action.payload.payloads, (p, k) => {
-        if (action.payload.remoteIds[k]) {
-        // loop on actions.payload.remoteIds[k].localIds
-          _.each(action.payload.remoteIds[k].localIds, (value, localId) => {
-            const rVal = vivl(value.viewType, 'getDisplayedValues')(
-              stateDataCache[k] ? stateDataCache[k][localId] : undefined, value.field,
-              value.expectedInterval, p);
-            if (rVal) {
-              (newState[k] ? newState[k] : newState[k] = {})[localId] = rVal;
-            }
-          });
-        }
-      });
-      return Object.assign({}, stateDataCache, newState);
+    case types.DATA_IMPORT_RANGE_PAYLOADS: {
+      const localIdState = updateRangePayloads(stateDataCache, action);
+      const update = {};
+      update[action.payload.remoteId] = {};
+      update[action.payload.remoteId][action.payload.localId] = localIdState;
+      return u(update,stateDataCache);
+    }
+    case types.DATA_IMPORT_ONE_PAYLOAD: {
+      const update = {};
+      update[action.payload.remoteId] = {};
+      update[action.payload.remoteId][action.payload.localId] = action.payload.valuesToDisplay;
+      return u(update,stateDataCache);
     }
     default:
       return stateDataCache;
   }
+}
+
+function updateRangePayloads(stateDataCache, action) {
+  const newLocalIdState = { data: {}, index: []};
+  const remoteId = action.payload.remoteId;
+  const localId = action.payload.localId;
+  const lower = action.payload.interval[0];
+  const upper = action.payload.interval[1];
+
+  if (_.get(stateDataCache, [remoteId, localId])) {
+    // Cleaning of values outside interval
+    _.each(stateDataCache[remoteId][localId].index, (timestamp) => {
+      if (timestamp >= lower && timestamp <= upper) {
+        newLocalIdState.index.push(timestamp);
+        newLocalIdState.data[timestamp] = stateDataCache[remoteId][localId].data[timestamp];
+      }
+    });
+  }
+  // Add new values
+  const keys = Object.keys(action.payload.valuesToDisplay);
+  let index = 0;
+  keys.forEach(item => {  // ordoring keys
+    const insertIndex = _.findIndex(newLocalIdState.index, time => time > item, index);
+    if (insertIndex < 0) {
+      newLocalIdState.index.push(item);
+      index = newLocalIdState.index.length - 1;
+    } else {
+      newLocalIdState.index = _.concat(_.slice(newLocalIdState.index, 0, insertIndex), item,
+      _.slice(newLocalIdState.index, insertIndex));
+      index = insertIndex;
+    }
+  });
+  Object.assign(newLocalIdState.data, action.payload.valuesToDisplay);
+  return newLocalIdState;
 }

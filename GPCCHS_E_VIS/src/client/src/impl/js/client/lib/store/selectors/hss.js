df7c29de4e317f0f89503c9b0b1475393b3e8b1d
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-10-18 17:11:19 +0000

    [HL] Fix wrong linting correction in hss selector

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/selectors/hss.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/selectors/hss.js
@@ -1,7 +1,4 @@
-/**
- * Selectors
- */
-export default function getStatus(state, identity) {
+export function getStatus(state, identity) {
   const identityObject = state.hss[identity];
   if (!identityObject) {
     return { status: 'disconnected' };

f8c787fa0b019f9129b3ee896ac8e85e869c8cee
Julien Renaux <jrenaux.external@atos.net>
2016-10-20 09:05:03 +0000

    [HL] . . . . .

--- GPCCHS_E_VIS/src/client/src/impl/js/client/VIVL/PlotView/window/PlotView.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/VIVL/PlotView/window/PlotView.js
@@ -1,11 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import { format } from "d3-format";
 import { timeFormat } from "d3-time-format";
+import { schemeCategory10 as colors } from "d3-scale";
 import { ChartCanvas, Chart, series, scale, coordinates, tooltip, axes, helper } from "react-stockcharts";
 
 const { LineSeries, AreaSeries, ScatterSeries, CircleMarker } = series;
 const { discontinuousTimeScaleProvider } = scale;
-const { CrossHairCursor, MouseCoordinateX, MouseCoordinateY } = coordinates;
+const { CrossHairCursor, MouseCoordinateX, MouseCoordinateY, CurrentCoordinate } = coordinates;
 const { OHLCTooltip } = tooltip;
 const { XAxis, YAxis } = axes;
 const { fitWidth } = helper;
@@ -27,12 +28,14 @@ export default class PlotView extends Component {
     // markers: PropTypes.array,
   };
   state = { rows: [] };
-  margin = { left: 20, right: 20, top: 20, bottom: 40 };
-  maxPoints = 1000;
+  margin = { left: 10, right: 60, top: 20, bottom: 20 };
+  maxPoints = 500;
   maxY = 10000;
   minY = 0;
   date = new Date();
   componentWillMount() {
+    // random number of lines
+    this.lines = Math.floor(Math.random() * (5 - 1)) + 1;
     this.reRender();
     // setInterval(() => {
     //   this.state.rows.shift()
@@ -42,13 +45,18 @@ export default class PlotView extends Component {
     // }, 1000)
   }
   getRandomYValue() {
+    if (Math.floor(Math.random() * 100) > 66) {
+      return undefined;
+    }
     return Math.floor(Math.random() * (this.maxY - this.minY)) + this.minY;
   }
   getRandomRow() {
     let row = {
-      date: new Date(this.date.setDate(this.date.getDate() + 1)),
-      close: this.getRandomYValue()
+      date: new Date(this.date.setDate(this.date.getDate() + 1))
     };
+    for (let i = 0; i < this.lines; i++) {
+      row[`value${i}`] = this.getRandomYValue();
+    }
     return row;
   }
   getRandomRows() {
@@ -64,33 +72,67 @@ export default class PlotView extends Component {
       rows: this.getRandomRows()
     });
   }
+  getLines() {
+    const lines = [];
+    for (let i = 0; i < this.lines; i++) {
+      lines.push(<LineSeries
+        key={`line${i}`}
+        yAccessor={d => d[`value${i}`]}
+        stroke={colors[i]}
+        />);
+      lines.push(<ScatterSeries
+        key={`scatter${i}`}
+        yAccessor={d => d[`value${i}`]}
+        marker={CircleMarker}
+        markerProps={{ r: 1, stroke: colors[i] }}
+        />);
+      lines.push(<CurrentCoordinate
+        key={`coordinate${i}`}
+        yAccessor={d => d[`value${i}`]}
+        fill={colors[i]}
+        />);
+    }
+    return lines;
+  }
   render() {
     const { rows: data } = this.state;
     const { size, ratio = 2 } = this.props;
-    const { top, right, bottom, left } = this.margin;
-    const width = size.width - right - left;
-    const height = size.height - top - bottom;
+
+    const Lines = this.getLines();
 
     return (
       <div>
-        plot view | {this.props.viewId} | {this.props.size.width}x{this.props.size.height}
         <ChartCanvas
           ratio={ratio}
-          width={width}
-          height={height}
+          width={size.width}
+          height={size.height - 50}
           margin={this.margin}
-          seriesName="MSFT"
+          seriesName="PlotView"
           data={data}
           type="hybrid"
           xAccessor={d => d.date} xScaleProvider={discontinuousTimeScaleProvider}
           xExtents={[data[0].date, data[data.length - 1].date]}
-        >
-          <Chart id={0} yExtents={d => d.close}>
+          >
+          <Chart id={1} yExtents={d => {
+            const data = [];
+            for (let i = 0; i < this.lines; i++) {
+              data.push(d[`value${i}`]);
+            }
+            return data
+          } }>
             <XAxis axisAt="bottom" orient="bottom" />
             <YAxis axisAt="right" orient="right" ticks={5} />
-            <LineSeries yAccessor={d => d.close} />
-            <ScatterSeries yAccessor={d => d.close} marker={CircleMarker} markerProps={{ r: 2 }} />
+            <MouseCoordinateX
+              at="bottom"
+              orient="bottom"
+              displayFormat={timeFormat("%Y-%m-%d")} />
+            <MouseCoordinateY
+              at="right"
+              orient="right"
+              displayFormat={format(".2f")} />
+            {Lines}
           </Chart>
+          <CrossHairCursor />
         </ChartCanvas>
       </div>
     );

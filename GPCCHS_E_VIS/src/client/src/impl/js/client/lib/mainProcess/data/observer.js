68e0679edec2dcbee763da369010671a05453c3b
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-10-20 13:33:09 +0000

    [FT:#3622] Add _.throttle delaying on HSC main process data observer to request data at most 1 time to n milliseconds.

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/mainProcess/data/observer.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/mainProcess/data/observer.js
@@ -1,4 +1,5 @@
-import _ from 'lodash';
+import _throttle from 'lodash/throttle';
+import _isObject from 'lodash/isObject';
 
 import debug from '../../common/debug/mainDebug';
 import dataMapGenerator from './dataMap';
@@ -8,26 +9,28 @@ import { addRequests } from '../../store/actions/dataRequests';
 
 const logger = debug('store:observers:data');
 
-// TODO _.throttle
-export default function data(state, dispatch) {
+export default _throttle((state, dispatch) => {
   logger.verbose('begin data synchronisation');
 
   const start = process.hrtime();
 
+  // TODO : improve memoization
   const dataMap = dataMapGenerator(state);
   // console.log(require('util').inspect(dataMap, {depth: 5}));
 
+  // TODO : improve memoization: pass dataRequests as arguments (/!\ no reselect, should never return previous requests, but maybe requesting could be done in a createSelector)
   const dataQueries = requestsMapGenerator(state, dataMap);
   // console.log(require('util').inspect(dataQueries, {depth: 5}));
 
-  if (dataQueries && _.isObject(dataQueries) && Object.keys(dataQueries).length) {
+  if (dataQueries && _isObject(dataQueries) && Object.keys(dataQueries).length) {
     getWebsocket().write({ event: 'dataQuery', payload: dataQueries });
   }
 
   dispatch(addRequests(dataQueries));
 
   const duration = process.hrtime(start)[1] / 1e6;
-  logger.debug(
-    `data requests done in ${duration}ms, ${Object.keys(dataQueries).length} remoteId requests`
-  );
-}
+  const count = Object.keys(dataQueries).length;
+  return count
+    ? logger.debug(`data requests done in ${duration}ms, ${count} remoteId requests`)
+    : logger.debug('no data to requests');
+}, 100); // TODO constant

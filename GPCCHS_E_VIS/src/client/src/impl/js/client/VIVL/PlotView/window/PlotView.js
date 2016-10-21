5516ec09fc34b631feacff74272ea6331c424b9f
Julien Renaux <jrenaux.external@atos.net>
2016-10-21 07:30:18 +0000

    [HL] simplify code, remove dependency, force data shape

--- GPCCHS_E_VIS/src/client/src/impl/js/client/VIVL/PlotView/window/PlotView.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/VIVL/PlotView/window/PlotView.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import _get from 'lodash/get';
-import _map from 'lodash/map';
 import { format } from 'd3-format';
 import { timeFormat } from 'd3-time-format';
 import { ChartCanvas, Chart, series, scale, coordinates, axes } from 'react-stockcharts';
@@ -12,7 +11,10 @@ const { XAxis, YAxis } = axes;
 
 export default class PlotView extends Component {
   static propTypes = {
-    data: PropTypes.any,
+    data: React.PropTypes.shape({
+      lines: React.PropTypes.array.isRequired,
+      columns: React.PropTypes.array.isRequired
+    }),
     // configuration: PropTypes.object.isRequired,
     size: PropTypes.object.isRequired,
     // entryPoints: PropTypes.array.isRequired,
@@ -25,7 +27,7 @@ export default class PlotView extends Component {
     // legend: PropTypes.object,
     // markers: PropTypes.array,
   };
-  yExtents = d => _map(this.props.data.lines, ({ key }) => _get(d, [key]))
+  yExtents = d => this.props.data.lines.map(({ key }) => d[key])
   renderLines(lines = []) {
     return lines.map(({ key, color }) => (
       <div key={key}>

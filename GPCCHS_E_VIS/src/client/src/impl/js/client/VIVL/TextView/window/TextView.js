94373658766f2a10f45f8bbcfbe843db10eff2ab
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-10-24 09:33:15 +0000

    [FT:#3622] Remove size props from TextView (now managed by PlotView directly)

--- GPCCHS_E_VIS/src/client/src/impl/js/client/VIVL/TextView/window/TextView.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/VIVL/TextView/window/TextView.js
@@ -3,10 +3,9 @@ import Mustache from 'mustache';
 
 export default class TextView extends Component {
   static propTypes = {
-    viewId: PropTypes.string.isRequired,
+    // viewId: PropTypes.string.isRequired,
     data: PropTypes.any,
     configuration: PropTypes.object.isRequired,
-    size: PropTypes.object.isRequired,
     // entryPoints: PropTypes.array.isRequired,
     // links: PropTypes.array,
     // defaultRatio: PropTypes.object,
@@ -24,12 +23,13 @@ export default class TextView extends Component {
   getMarkup = () => Mustache.render(
     this.state.template,
     this.props.data
-  )
+  );
 
   render() {
     const innerHTML = this.getMarkup();
     return (
       <div>
+        {/* eslint-disable-next-line react/no-danger */}
         <div dangerouslySetInnerHTML={{ __html: innerHTML }} />
       </div>
     );

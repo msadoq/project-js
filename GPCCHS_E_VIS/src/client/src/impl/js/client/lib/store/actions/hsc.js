995d67cf7fc485df23486f951c2bd73ccdea5bcf
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-10-24 12:37:43 +0000

    [HL] . . . . .

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/actions/hsc.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/actions/hsc.js
@@ -1,7 +1,4 @@
 import simple from '../simpleActionCreator';
 import * as types from '../types';
 
-/**
- * Simple actions
- */
 export const updateStatus = simple(types.HSC_UPDATE_STATUS, 'status');

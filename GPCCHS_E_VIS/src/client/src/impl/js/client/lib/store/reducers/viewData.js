c7e38d69f934a4356c9663b400be0e4faa7aa830
Audrey LEAL <audrey.leal.external@atos.net>
2016-10-27 11:13:23 +0000

    [FT:#3622] Fix view data store .

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/reducers/viewData.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/reducers/viewData.js
@@ -1,4 +1,4 @@
-import { each, findIndex, findLastIndex, concat, omit } from 'lodash';
+import { each, findIndex, findLastIndex, concat, omit, head, last } from 'lodash';
 import * as types from '../types';
 import debug from '../../common/debug/mainDebug';
 
@@ -16,8 +16,8 @@ export default function viewData(stateViewData = {}, action) {
             break;
           }
           case 'range': {
-            newState[viewId] = { index: [], columns: [] };
-            if (stateViewData[viewId]) {
+            newState[viewId] = {}; // index: [], columns: [] };
+            if (stateViewData[viewId] && view.remove) {
               newState[viewId] = cleanRangeData(stateViewData[viewId],
                 { type: 'DATA_IMPORT_VIEWDATA', payload: view.remove });
             }
@@ -39,12 +39,14 @@ export default function viewData(stateViewData = {}, action) {
 }
 // Clean values outside the expected interval
 export function cleanRangeData(viewSubState, action) {
+  if (!viewSubState || !viewSubState.index.length) {
+    return viewSubState;
+  }
   switch (action.type) {
     case types.DATA_IMPORT_VIEWDATA: {
       // master's timestamp (arbitrary determined from the first entryPoint)
       const lower = action.payload.lower;
       const upper = action.payload.upper;
-
       const newState = { index: [], columns: [] };
 
       const iLower = findIndex(viewSubState.index, t => t >= lower);
@@ -91,22 +93,26 @@ export function updateRangeData(viewSubState, action) {
   switch (action.type) {
     case types.DATA_IMPORT_VIEWDATA: {
       const newState = { ...viewSubState };
+      if (!newState.index) {
+        newState.index = [];
+        newState.columns = [];
+      }
       // Parcours des nouvelles données dans l'ordre temporel
       let lastIndex = 0;
       // newState[masterTime][epName] =
       //   { x: value.payload[ep.fieldX], value: value.payload[ep.fieldY] };
 
       each(Object.keys(action.payload), (time) => {
-        const timestamp = parseInt(time);
+        const timestamp = parseInt(time, 10);
         const value = action.payload[time];
         if (lastIndex === -1) {
-          newState.columns.push({ ...value, x: new Date(timestamp) });
+          newState.columns.push({ ...value, x: timestamp });
           newState.index.push(timestamp);
         } else {
           const index = findIndex(newState.index, t => t >= timestamp, lastIndex);
           lastIndex = index;
           if (index === -1) {
-            newState.columns.push({ ...value, x: new Date(timestamp) });
+            newState.columns.push({ ...value, x: timestamp });
             newState.index.push(timestamp);
           } else if (newState.index[index] === timestamp) {
             // update
@@ -115,7 +121,7 @@ export function updateRangeData(viewSubState, action) {
             newState.index = concat(newState.index.slice(0, index),
               timestamp, newState.index.slice(index));
             newState.columns = concat(newState.columns.slice(0, index),
-              { ...value, x: new Date(timestamp) }, newState.columns.slice(index));
+              { ...value, x: timestamp }, newState.columns.slice(index));
           }
         }
       });

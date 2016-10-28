95684c8e4a75502f239cab480c572bfdf9ed5523
Olivier HUYARD <olivier.huyard@atos.net>
2016-10-28 14:48:09 +0000

    [FT:#3622] Creation of a launcher of GPCCHS for ISIS product line

---
+++ GPCCHS_E_VIS/src/launcher/src/main/py/GPCCHS_E_VIS/launcher/__init__.py
@@ -0,0 +1,24 @@
+# Produced by Acceleo Python Generator 1.1.0-R7S1-20151214
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""!
+Project   : ISIS
+Component : GPCCHS_E_VIS
+@file     : __init__.py
+@author   : isis
+@date     : 25/10/2016
+@brief    : /
+@type     : Package
+"""
+# ====================================================================
+# HISTORY
+#
+# VERSION  : 1.0  : Creation
+#
+# END-HISTORY
+# ====================================================================
+# Start of user code init
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)  
+# End of user code

8218e60ffafbccf4fb1227fd45238798436ae1d7
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-11-03 13:46:00 +0000

    [HL] . . . . .

---
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/common/data/map/missingRemoteIds.spec.js
@@ -0,0 +1,40 @@
+/* eslint no-unused-expressions: 0 */
+
+import '../../test';
+import missingRemoteIds from './missingRemoteIds';
+import { inspect } from 'util';
+
+describe.only('missingRemoteIds', () => {
+  const map = {
+    myRemoteId: {
+      structureType: 'range',
+      dataId: {
+        catalog: 'Reporting',
+        parameterName: 'TMMGT_BC_VIRTCHAN3',
+        comObject: 'ReportingParameter',
+        domainId: 27,
+        sessionId: 1,
+      },
+      filter: [
+        {
+          field: 'convertedValue',
+          operator: '!=',
+          operand: 0,
+        },
+      ],
+      localIds: {
+        myLocalId: {
+          field: 'groundDate',
+          timebarId: 'myTimebarId',
+          offset: 0,
+          expectedInterval: [10, 20],
+        },
+      },
+    },
+  };
+
+  it('works', () => {
+    console.log(inspect(missingRemoteIds(map), { depth: 5 }));
+    console.log(inspect(missingRemoteIds(map), { depth: 5 }));
+  });
+});

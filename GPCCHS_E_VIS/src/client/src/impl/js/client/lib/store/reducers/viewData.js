a6de51ae7c6a88de01de2c9a4b74a58b82d4b31a
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-11-04 13:30:58 +0000

    [FT:#3622] Fix and improve viewData injection

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/reducers/viewData.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/reducers/viewData.js
@@ -1,140 +1,200 @@
-import { each, findIndex, findLastIndex, concat, omit } from 'lodash';
-import * as types from '../types';
 import { constants as globalConstants } from 'common';
+import { findIndex, findLastIndex, concat } from 'lodash';
+import _each from 'lodash/each';
+import _get from 'lodash/get';
+import _reduce from 'lodash/reduce';
+import _last from 'lodash/last';
+import _keys from 'lodash/keys';
+import * as types from '../types';
 
-export default function viewData(stateViewData = {}, action) {
+export default function viewData(state = {}, action) {
   switch (action.type) {
     case types.DATA_IMPORT_VIEWDATA: {
-      const views = action.payload.viewData;
-      const newState = {};
-      each(views, (view, viewId) => {
+      /**
+       * {
+       *   viewId: {
+       *     // last structure type
+       *     structureType: 'last'
+       *     index: {
+       *       CLK_BC_PMONENA: 1420106460000,
+       *       GENE_AM_S23RTSWD: 1420106460000,
+       *     },
+       *     values: {
+       *       CLK_BC_PMONENA: 0.286974618986513,
+       *       GENE_AM_S23RTSWD: 0.286974618986513,
+       *     },
+       *   },
+       *   viewId: {
+       *     // range structure type
+       *     structureType: 'range',
+       *     remove: {
+       *       lower: 1420106460000,
+       *       upper: 1420106460000,
+       *     },
+       *     add: {
+       *       '1420106464000': {
+       *         CLK_BC_PMONENA: { x: 1420106464000, value: 0.286974618986513 },
+       *         GENE_AM_S23RTSWD: { x: 1420106464000, value: 0.25454618986513 },
+       *       },
+       *     },
+       *    }
+       *  }
+       */
+      if (!action.payload.viewData || Object.keys(action.payload.viewData).length < 1) {
+        return state;
+      }
+      return _reduce(action.payload.viewData, (newState, view, viewId) => {
         switch (view.structureType) {
-          case globalConstants.DATASTRUCTURETYPE_LAST: {
-            newState[viewId] = { ...stateViewData[viewId], ...omit(view, ['structureType']) };
-            break;
-          }
+          case globalConstants.DATASTRUCTURETYPE_LAST:
+            return viewLast(newState, viewId, view.index, view.values);
           case globalConstants.DATASTRUCTURETYPE_RANGE: {
-            newState[viewId] = {}; // index: [], columns: [] };
-            if (stateViewData[viewId] && view.remove) {
-              newState[viewId] = cleanRangeData(
-                stateViewData[viewId],
-                { type: 'DATA_IMPORT_VIEWDATA', payload: view.remove }
-                );
-            }
-            if (view.add) {
-              newState[viewId] = updateRangeData(
-                newState[viewId],
-                { type: 'DATA_IMPORT_VIEWDATA', payload: view.add }
-                );
-            }
-            break;
+            return viewRange(newState, viewId, view.remove, view.add);
           }
           default:
-            return;
+            return newState;
         }
-      });
-      return { ...stateViewData, ...newState };
+      }, state);
     }
     case types.DATA_REMOVE_ALL_VIEWDATA:
       return {};
     default:
-      return stateViewData;
+      return state;
   }
 }
-// Clean values outside the expected interval
-export function cleanRangeData(viewSubState = {}, action) {
-  if (!viewSubState || !viewSubState.index || viewSubState.index.length === 0) {
-    return viewSubState;
-  }
-  switch (action.type) {
-    case types.DATA_IMPORT_VIEWDATA: {
-      // master's timestamp (arbitrary determined from the first entryPoint)
-      const lower = action.payload.lower;
-      const upper = action.payload.upper;
-      const newState = { index: [], columns: [] };
 
-      const iLower = findIndex(viewSubState.index, t => t >= lower);
-      let iUpper = -1;
-      if (iLower >= 0) {
-        const l = viewSubState.index.length;
-        if (viewSubState.index[l - 1] > upper) {
-          iUpper = findLastIndex(viewSubState.index, t => t <= upper);
-        }
-      }
-      // All data to erase
-      if (iLower < 0) {
-        return newState;
-      }
-      // All data to keep
-      if (iLower === 0 && iUpper < 0) {
-        return viewSubState;
-      }
-      if (iUpper >= 0) {
-        newState.index = viewSubState.index.slice(iLower, iUpper + 1);
-        newState.columns = viewSubState.columns.slice(iLower, iUpper + 1);
-      } else {
-        newState.index = viewSubState.index.slice(iLower);
-        newState.columns = viewSubState.columns.slice(iLower);
-      }
-      return newState;
-    }
-    default:
-      return viewSubState;
+export function viewLast(state, viewId, index, values) {
+  if (!Object.keys(index).length && !Object.keys(values).length) {
+    return state;
   }
+
+  const stateIndex = _get(state, [viewId, 'index'], {});
+  const stateValues = _get(state, [viewId, 'values'], {});
+
+  // TODO : simplify code and bufferisation by passing all value for this param and take only closer
+  // to current time here
+
+  return Object.assign({}, state, {
+    [viewId]: {
+      index: { ...stateIndex, ...index },
+      values: { ...stateValues, ...values },
+    }
+  });
 }
-// viewParam = {
-// type: string,
-// entryPoints: {
-//   'name': {
-//     remoteId,
-//     fieldX: string,
-//     fieldY: string,
-//     offset: number,
-//     expectedInterval: [number, number],
-//   },
-// }}
-export function updateRangeData(viewSubState = {}, action) {
-  switch (action.type) {
-    case types.DATA_IMPORT_VIEWDATA: {
-      // Check presence of payloads
-      const timestamps = Object.keys(action.payload);
-      if (timestamps.length === 0) {
-        return viewSubState;
-      }
-      const newState = { ...viewSubState };
-      if (!newState.index) {
-        newState.index = [];
-        newState.columns = [];
-      }
-      let lastIndex = 0;
-      // newState[masterTime][epName] =
-      //   { x: value.payload[ep.fieldX], value: value.payload[ep.fieldY] };
-      each(timestamps, (time) => {
-        const timestamp = parseInt(time, 10);
-        const value = action.payload[time];
-        if (lastIndex === -1) {
-          newState.columns.push({ ...value, x: timestamp });
-          newState.index.push(timestamp);
-        } else {
-          const index = findIndex(newState.index, t => t >= timestamp, lastIndex);
-          lastIndex = index;
-          if (index === -1) {
-            newState.columns.push({ ...value, x: timestamp });
-            newState.index.push(timestamp);
-          } else if (newState.index[index] === timestamp) {
-            // update
-            newState.columns[index] = { ...newState.columns[index], ...value };
-          } else { // add
-            newState.index = concat(newState.index.slice(0, index),
-              timestamp, newState.index.slice(index));
-            newState.columns = concat(newState.columns.slice(0, index),
-              { ...value, x: timestamp }, newState.columns.slice(index));
-          }
-        }
-      });
-      return newState;
-    }
-    default:
-      return viewSubState;
+
+export function viewRange(state, viewId, add, remove) {
+  let viewState = state[viewId];
+  viewState = viewRangeRemove(viewState, remove.lower, remove.upper);
+  viewState = viewRangeAdd(viewState, add);
+
+  return (viewState === state[viewId])
+    ? state // no modification
+    : Object.assign({}, state, {
+      [viewId]: viewState,
+    });
+}
+
+/**
+ * Returns a modified state without values outside lower and upper limits
+ *
+ * @param state
+ * @param lower
+ * @param upper
+ * @return {*}
+ */
+export function viewRangeRemove(state, lower, upper) {
+  if (lower > upper) {
+    // unpredictable usage
+    return { index: [], columns: [] };
   }
+
+  // keep everything
+  if (!state || !state.index || state.index.length === 0) {
+    // state contains no data
+    return state;
+  }
+  if (state.index[0] >= lower && _last(state.index) <= upper) {
+    // all points are in visuWindow
+    return state;
+  }
+
+  // drop everything
+  if (state.index[0] >= upper || _last(state.index) <= lower) {
+    return { index: [], columns: [] };
+  }
+
+  // cut
+  const iLower = findIndex(state.index, t => t >= lower);
+  let iUpper = findLastIndex(state.index, t => t <= upper);
+  iUpper = (iUpper === -1) ? state.index.length - 1 : iUpper;
+
+  return {
+    index: state.index.slice(iLower, iUpper + 1),
+    columns: state.columns.slice(iLower, iUpper + 1),
+  };
+}
+
+export function viewRangeAdd(state, payloads) {
+  const keys = _keys(payloads);
+  if (!keys.length) {
+    // nothing to import contains no data
+    return state;
+  }
+
+  // from now we are sure we will mutate the state
+  const newState = { ...state };
+  if (!newState.index) {
+    newState.index = [];
+    newState.columns = [];
+  }
+
+  let lastIndex = 0;
+  let lastTime = 0;
+  // newState[masterTime][epName] =
+  //   { x: value.payload[ep.fieldX], value: value.payload[ep.fieldY] };
+  // TODO: use reduce and improve code understanding
+  _each(keys, (key) => {
+    const timestamp = parseInt(key, 10); // TODO : avoid by passing .index[] in payload
+    const value = payloads[key];
+
+    // if new value should be pushed at end (most common case in play mode)
+    if (lastIndex === -1 && timestamp > lastTime) {
+      newState.columns.push({ ...value, x: timestamp });
+      newState.index.push(timestamp);
+      lastTime = timestamp;
+      return;
+    }
+
+    if (timestamp < lastTime) {
+      lastIndex = 0; // fix the Object.keys() not always sorted behavior
+    }
+    lastTime = timestamp;
+
+    const index = findIndex(newState.index, t => t >= timestamp, lastIndex);
+    lastIndex = index;
+    if (index === -1) {
+      // add at end
+      newState.columns.push({ ...value, x: timestamp });
+      newState.index.push(timestamp);
+      return;
+    }
+
+    if (newState.index[index] === timestamp) {
+      // update an existing value
+      newState.columns[index] = { ...newState.columns[index], ...value };
+      return;
+    }
+
+    // add at index
+    newState.index = concat(
+      newState.index.slice(0, index),
+      timestamp,
+      newState.index.slice(index)
+    );
+    newState.columns = concat(
+      newState.columns.slice(0, index),
+      { ...value, x: timestamp },
+      newState.columns.slice(index)
+    );
+  });
+  return newState;
 }
--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/reducers/viewData.spec.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/reducers/viewData.spec.js
@@ -1,4 +1,6 @@
-import { cleanRangeData, updateRangeData } from './viewData';
+import { freezeMe } from '../../common/test';
+import reducer, { viewRangeRemove, viewRangeAdd, viewLast } from './viewData';
+import { importPayload, removeAllData } from '../actions/viewData';
 
 describe('reducers/viewData', () => {
   const payload = { rId1: {}, rId2: {} };
@@ -14,202 +16,258 @@ describe('reducers/viewData', () => {
     payload.rId2[j] = payload.rId1[j];
   }
 
-  const viewDataMap = {
-    plot1: {
-      type: 'PlotView',
-      entryPoints: {
-        ep1: {
-          remoteId: 'rId1',
-          fieldX: 'time',
-          fieldY: 'val1',
-          expectedInterval: [10, 15],
-          offset: 0,
-        }
-      },
-    },
-    plot2: {
-      type: 'PlotView',
-      entryPoints: {
-        ep2: {
-          remoteId: 'rId1',
-          fieldX: 'time',
-          fieldY: 'val2',
-          expectedInterval: [12, 16],
-          offset: 2,
-        },
-        ep3: {
-          remoteId: 'rId2',
-          fieldX: 'time',
-          fieldY: 'val2',
-          expectedInterval: [14, 18],
-          offset: 0,
-        }
-      },
-    },
-    plot3: {
-      type: 'PlotView',
-      entryPoints: {
-        ep1: {
-          remoteId: 'rId1',
-          fieldX: 'time',
-          fieldY: 'val1',
-          expectedInterval: [1001, 1005],
-          offset: -987,
-        }
-      },
-    },
-    text1: {
-      type: 'TextView',
-      entryPoints: {
-        ep4: {
-          remoteId: 'rId1',
-          field: 'val3',
-          expectedInterval: [10, 20],
-          offset: 0,
-        }
-      }
-    },
-    text2: {
-      type: 'TextView',
-      entryPoints: {
-        ep5: {
-          remoteId: 'rId1',
-          field: 'val3',
-          expectedInterval: [18, 20],
-          offset: 0,
-        },
-        ep6: {
-          remoteId: 'rId2',
-          field: 'val3',
-          expectedInterval: [12, 20],
-          offset: 0,
-        }
-      },
-    },
-  };
-  const viewMap2 = {
-    hist1: {
-      type: 'HitoryView',
-      entryPoints: {
-        ep1: {
-          remoteId: 'rId1',
-          fieldX: 'time',
-          fieldY: 'val1',
-          expectedInterval: [10, 15],
-          offset: 0,
-        }
-      },
-    },
-  };
-
-  const state = { viewData: {
-    plot2: {
-      index: [10, 14],
-      columns: [{ x: 10, ep5: { x: 10.2, value: 100 }, ep3: { x: 12.2, value: 25 } },
-                { x: 14, ep5: { x: 10.7, value: 100 }, ep3: { x: 12.7, value: 25 } }],
-    },
-    text2: {
-      index: { ep2: 18, ep3: 21 },
-      values: { ep2: 1, ep3: 3 },
-    },
-  } };
-
-
-  describe('cleanRangeData', () => {
-    // cleanRangeData(viewSubState, ep, epName)
-    it('empty state', () => {
-      const state1 = {};
-      const cleanedState = cleanRangeData(state1, viewDataMap.plot1.entryPoints.ep1, 'ep1');
-      cleanedState.should.equal(state1);
+  describe('DATA_IMPORT_VIEWDATA', () => {
+    it('should handle empty action', () => {
+      reducer(Object.freeze({}), importPayload({})).should.eql({});
+      reducer(Object.freeze({}), importPayload({ myViewId: {} })).should.eql({});
     });
-    it('no data to keep', () => {
-      const tmpState = { index: [0, 1, 2, 3],
-      columns: { 0: { ep1: { x: 0, value: 0.1 } },
-        1: { ep1: { x: 0, value: 0.1 } },
-        2: { ep1: { x: 0, value: 0.1 } },
-        3: { ep1: { x: 0, value: 0.1 } },
-        },
-      };
-      const action = { type: 'DATA_IMPORT_VIEWDATA', payload: { lower: 10, upper: 15 } };
-      const cleanedState = cleanRangeData(tmpState, action);
-      cleanedState.index.should.have.length(0);
-      cleanedState.columns.should.have.length(0);
+    describe('last structure', () => {
+      it('should add value', () => {
+        const frozen = Object.freeze({});
+        const r = reducer(frozen, importPayload({
+          myViewId: {
+            structureType: 'last',
+            index: { myEntryPoint: 15 },
+            values: { myEntryPoint: 300 },
+          }
+        }));
+        r.should.eql({
+          myViewId: {
+            index: { myEntryPoint: 15 },
+            values: { myEntryPoint: 300 },
+          }
+        });
+      });
+      it('should replace value', () => {
+        const frozen = Object.freeze({
+          myViewId: {
+            index: { myEntryPoint: 10 },
+            values: { myEntryPoint: 250 },
+          },
+        });
+        const r = reducer(frozen, importPayload({
+          myViewId: {
+            structureType: 'last',
+            index: { myEntryPoint: 15 },
+            values: { myEntryPoint: 300 },
+          }
+        }));
+        r.should.eql({
+          myViewId: {
+            index: { myEntryPoint: 15 },
+            values: { myEntryPoint: 300 },
+          }
+        });
+      });
+      it('should respect other values', () => {
+        const frozen = Object.freeze({
+          myViewId: {
+            index: { myEntryPoint: 10 },
+            values: { myEntryPoint: 250 },
+          },
+          myOtherViewId: {
+            index: { myEntryPoint: 5 },
+            values: { myEntryPoint: 125 },
+          },
+        });
+        const r = reducer(frozen, importPayload({
+          myViewId: {
+            structureType: 'last',
+            index: { myEntryPoint: 15 },
+            values: { myEntryPoint: 300 },
+          }
+        }));
+        r.should.eql({
+          myViewId: {
+            index: { myEntryPoint: 15 },
+            values: { myEntryPoint: 300 },
+          },
+          myOtherViewId: {
+            index: { myEntryPoint: 5 },
+            values: { myEntryPoint: 125 },
+          },
+        });
+      });
     });
-    it('interval to keep', () => {
-      const tmpState = { index: [8.5, 9.5, 10.5, 11.5],
-        columns: [
-          { x: 8.5, ep1: { x: 0, value: 0.1 } },
-          { x: 9.5, ep1: { x: 0, value: 0.1 } },
-          { x: 10.5, ep1: { x: 1, value: 0.1 } },
-          { x: 11.5, ep1: { x: 2, value: 0.1 } },
-        ],
-      };
-      const action = { type: 'DATA_IMPORT_VIEWDATA', payload: { lower: 10, upper: 15 } };
+    describe('range structure', () => {
 
-      const cleanedState = cleanRangeData(tmpState, action);
-      cleanedState.index.should.deep.equal([10.5, 11.5]);
-      cleanedState.columns[0].should.deep.equal({ x: 10.5, ep1: { x: 1, value: 0.1 } });
-      cleanedState.columns[1].should.deep.equal({ x: 11.5, ep1: { x: 2, value: 0.1 } });
-    });
-    it('whole interval to keep', () => {
-      const tmpState = { index: [10, 11, 14],
-      columns: [
-        { x: 10, ep1: { x: 1, value: 0.1 } },
-        { x: 11, ep1: { x: 2, value: 0.1 } },
-        { x: 14, ep1: { x: 3, value: 0.1 } },
-        ],
-      };
-      const action = { type: 'DATA_IMPORT_VIEWDATA', payload: { lower: 10, upper: 15 } };
-      const cleanedState = cleanRangeData(tmpState, action);
-      cleanedState.index.should.deep.equal([10, 11, 14]);
-      cleanedState.columns[0].should.deep.equal({ x: 10, ep1: { x: 1, value: 0.1 } });
-      cleanedState.columns[1].should.deep.equal({ x: 11, ep1: { x: 2, value: 0.1 } });
-      cleanedState.columns[2].should.deep.equal({ x: 14, ep1: { x: 3, value: 0.1 } });
     });
   });
-  describe('updateRangeData', () => {
-    it('no payload', () => {
-      const tmpState = {};
-      const action = { type: 'DATA_IMPORT_VIEWDATA', payload: {} };
-      const newState = updateRangeData(tmpState, action);
-      newState.should.equal(tmpState);
-    });
-    it('state empty', () => {
-      const action = { type: 'DATA_IMPORT_VIEWDATA',
-      payload: {
-        10: { ep1: { x: 1, value: 0.1 } },
-        11: { ep1: { x: 2, value: 0.1 } },
-      } };
-      // newState[masterTime][epName] =
-      //   { x: value.payload[ep.fieldX], value: value.payload[ep.fieldY] };
-      const newState = updateRangeData({}, action);
-      newState.index.should.deep.equal([10, 11]);
-      newState.columns[0].should.deep.equal({ x: 10, ep1: { x: 1, value: 0.1 } });
-      newState.columns[1].should.deep.equal({ x: 11, ep1: { x: 2, value: 0.1 } });
-    });
-    it('state not empty', () => {
-      const oldState = { index: [1, 101, 104],
-      columns: [
-        { x: 1, ep1: { x: 1, value: 0.1 } },
-        { x: 101, ep1: { x: 2, value: 0.1 } },
-        { x: 104, ep1: { x: 3, value: 0.1 } },
-        ],
-      };
+  describe('DATA_REMOVE_ALL_VIEWDATA', () => {
+    // TODO DATA_REMOVE_ALL_VIEWDATA
+  });
 
-      const action = { type: 'DATA_IMPORT_VIEWDATA',
-      payload: {
+  describe('viewLast', () => {
+    it('should ignore payloads', () => {
+      const state = Object.freeze({});
+      viewLast(state, 'myViewId', {}, {}).should.equal(state);
+    });
+    it('should add', () => {
+      const state = Object.freeze({});
+      viewLast(state, 'myViewId', { myEntryPoint: 10 }, {}).should.eql({
+        myViewId: {
+          index: { myEntryPoint: 10 },
+          values: {},
+        }
+      });
+      viewLast(state, 'myViewId', {}, { myEntryPoint: 10 }).should.eql({
+        myViewId: {
+          index: {},
+          values: { myEntryPoint: 10 },
+        }
+      });
+    });
+    it('should update', () => {
+      const state = Object.freeze({
+        myViewId: {
+          index: { myEntryPoint: 10 },
+          values: { myEntryPoint: 150 },
+        }
+      });
+      viewLast(state, 'myViewId', { myEntryPoint: 20 }, { myEntryPoint: 300 }).should.eql({
+        myViewId: {
+          index: { myEntryPoint: 20 },
+          values: { myEntryPoint: 300 },
+        }
+      });
+    });
+    it('should preserve other values', () => {
+      const state = Object.freeze({
+        myViewId: {
+          index: { myEntryPoint: 10, myOther: 20 },
+          values: { myEntryPoint: 150, myOther: 200 },
+        }
+      });
+      viewLast(state, 'myViewId', { myEntryPoint: 20 }, { myEntryPoint: 300 }).should.eql({
+        myViewId: {
+          index: { myEntryPoint: 20, myOther: 20 },
+          values: { myEntryPoint: 300, myOther: 200 },
+        }
+      });
+    });
+  });
+
+  const state = {
+    index: [0, 1, 2, 3],
+    columns: [
+      { 0: { x: 0, ep1: { x: 0, value: 100.1 }, ep2: { x: 0, value: 200.1 } } },
+      { 1: { x: 1, ep1: { x: 1, value: 100.2 } } },
+      { 2: { x: 2, ep1: { x: 2, value: 100.3 } } },
+      { 3: { x: 3, ep1: { x: 3, value: 100.4 } } },
+    ],
+  };
+
+  describe('viewRangeRemove', () => {
+    it('should support empty state', () => {
+      const frozen = Object.freeze({});
+      viewRangeRemove(frozen, 10, 20).should.equal(frozen);
+      const otherFrozen = Object.freeze({ index: [] });
+      viewRangeRemove(otherFrozen, 10, 20).should.equal(otherFrozen);
+    });
+    it('should support nothing to keep', () => {
+      viewRangeRemove(freezeMe(state), -2, 0).should.eql({ index: [], columns: [] });
+      viewRangeRemove(freezeMe(state), 3, 5).should.eql({ index: [], columns: [] });
+      viewRangeRemove(freezeMe(state), 2, 1).should.eql({ index: [], columns: [] });
+    });
+    it('should support partial keeping', () => {
+      viewRangeRemove(freezeMe(state), 1, 2).should.eql({
+        index: [1, 2],
+        columns: [
+          { 1: { x: 1, ep1: { x: 1, value: 100.2 } } },
+          { 2: { x: 2, ep1: { x: 2, value: 100.3 } } },
+        ],
+      });
+      viewRangeRemove(freezeMe(state), 0, 1).index.should.eql([0, 1]);
+      viewRangeRemove(freezeMe(state), -1, 1).index.should.eql([0, 1]);
+      viewRangeRemove(freezeMe(state), 1, 3).index.should.eql([1, 2, 3]);
+      viewRangeRemove(freezeMe(state), 1, 5).index.should.eql([1, 2, 3]);
+    });
+    it('should support keep everything', () => {
+      viewRangeRemove(freezeMe(state), 0, 3).index.should.eql([0, 1, 2, 3]);
+    });
+  });
+  describe.only('viewRangeAdd', () => {
+    it('should ignore empty payloads call', () => {
+      const previousState = freezeMe(state);
+      const newState = viewRangeAdd(previousState, {});
+      newState.should.equal(previousState);
+    });
+    it('should support empty state', () => {
+      viewRangeAdd(freezeMe({}), {
         10: { ep1: { x: 1, value: 0.1 } },
         11: { ep1: { x: 2, value: 0.1 } },
-      } };
-      // newState[masterTime][epName] =
-      //   { x: value.payload[ep.fieldX], value: value.payload[ep.fieldY] };
-      const newState = updateRangeData(oldState, action);
-      newState.index.should.deep.equal([1, 10, 11, 101, 104]);
-      newState.columns[0].should.deep.equal({ x: 1, ep1: { x: 1, value: 0.1 } });
-      newState.columns[1].should.deep.equal({ x: 10, ep1: { x: 1, value: 0.1 } });
-      newState.columns[2].should.deep.equal({ x: 11, ep1: { x: 2, value: 0.1 } });
-      newState.columns[3].should.deep.equal({ x: 101, ep1: { x: 2, value: 0.1 } });
-      newState.columns[4].should.deep.equal({ x: 104, ep1: { x: 3, value: 0.1 } });
+      }).should.eql({
+        index: [10, 11],
+        columns: [
+          { x: 10, ep1: { x: 1, value: 0.1 } },
+          { x: 11, ep1: { x: 2, value: 0.1 } },
+        ],
+      });
+    });
+    describe.only('should add points', () => {
+      it('one point in middle', () => {
+        viewRangeAdd(Object.freeze({
+          index: [1, 4],
+          columns: [
+            { x: 1, ep1: { x: 1, value: 101 } },
+            { x: 4, ep1: { x: 4, value: 104 } },
+          ],
+        }), {
+          3: { ep1: { x: 3, value: 103 } },
+        }).should.eql({
+          index: [1, 3, 4],
+          columns: [
+            { x: 1, ep1: { x: 1, value: 101 } },
+            { x: 3, ep1: { x: 3, value: 103 } },
+            { x: 4, ep1: { x: 4, value: 104 } },
+          ],
+        });
+      });
+      it('update existing', () => {
+        viewRangeAdd(Object.freeze({
+          index: [1, 4],
+          columns: [
+            { x: 1, ep1: { x: 1, value: 101 }, ep2: { x: 1, value: 201 } },
+            { x: 4, ep1: { x: 4, value: 104 } },
+          ],
+        }), {
+          1: { ep2: { x: 1, value: 2002 } },
+          4: { ep1: { x: 4, value: 1004 } },
+        }).should.eql({
+          index: [1, 4],
+          columns: [
+            { x: 1, ep1: { x: 1, value: 101 }, ep2: { x: 1, value: 2002 } },
+            { x: 4, ep1: { x: 4, value: 1004 } },
+          ],
+        });
+      });
+      it('points everywhere', () => {
+        const r = viewRangeAdd(Object.freeze({
+          index: [1, 4, 8, 10],
+          columns: [
+            { x: 1, ep1: { x: 1, value: 100.1 }, ep2: { x: 1, value: 200.1 } },
+            { x: 4, ep1: { x: 4, value: 100.4 } },
+            { x: 8, ep1: { x: 8, value: 100.8 } },
+            { x: 10, ep1: { x: 10, value: 100.10 } },
+          ],
+        }), {
+          0: { ep1: { x: 0, value: 104 } },
+          1: { ep2: { x: 1, value: 204 } },
+          8: { ep1: { x: 8, value: 108 } },
+          10: { ep1: { x: 10, value: 110 } },
+          11: { ep1: { x: 11, value: 111 } },
+        }).should.eql({
+          index: [0, 1, 4, 8, 10, 11],
+          columns: [
+            { x: 0, ep1: { x: 0, value: 104 } },
+            { x: 1, ep1: { x: 1, value: 100.1 }, ep2: { x: 1, value: 204 } },
+            { x: 4, ep1: { x: 4, value: 100.4 } },
+            { x: 8, ep1: { x: 8, value: 108 } },
+            { x: 10, ep1: { x: 10, value: 110 } },
+            { x: 11, ep1: { x: 11, value: 111 } },
+          ],
+        });
+      });
     });
   });
 });

ed0f9fa69aa3bf453963e526944326a9d1632520
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-11-07 16:56:12 +0000

    [FT:#3622] Fix interval requesting duplication with for last sturcture type

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/common/data/structures/last/addInterval.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/common/data/structures/last/addInterval.js
@@ -1,3 +1,11 @@
+import _find from 'lodash/find';
+
 export default function addInterval(knownIntervals, interval) {
+  // TODO factorize or replace with existing function (rperrot)
+  const existing = _find(knownIntervals, i => i[0] === interval[0] && i[1] === interval[1]);
+  if (existing) {
+    return knownIntervals;
+  }
+
   return [...knownIntervals, interval];
 }

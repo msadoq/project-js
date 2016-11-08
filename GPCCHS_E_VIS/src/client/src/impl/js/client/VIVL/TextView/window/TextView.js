b85e331f866ad32e839ebb651bae33fbd0f9dab5
Julien Renaux <jrenaux.external@atos.net>
2016-11-08 11:23:29 +0000

    [HL] TextView perf enhancement. New dependency html-to-react

--- GPCCHS_E_VIS/src/client/src/impl/js/client/VIVL/TextView/window/TextView.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/VIVL/TextView/window/TextView.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
-import Mustache from 'mustache';
-
+import { Parser, ProcessNodeDefinitions } from 'html-to-react';
+import _get from 'lodash/get';
 import debug from '../../../lib/common/debug/windowDebug';
 
 const logger = debug('view:text');
@@ -22,27 +22,53 @@ export default class TextView extends Component {
     },
   };
 
-  constructor(props, context) {
-    super(props, context);
-
-    // recreate the html string from an array
-    this.state = {
-      template: props.configuration.content.join(''),
-    };
+  componentWillMount() {
+    this.template = this.props.configuration.content.join('');
   }
 
-  getMarkup = () => Mustache.render(
-    this.state.template,
-    this.props.data.values || {} // TODO : temp fix when no value = props is set but empty
-  );
+  getComponent() {
+    // console.time('view:text parse template');
+    const processingInstructions = [
+      {
+        shouldProcessNode: node => node.data && node.data.match(/{{\s*([^}]+)\s*}}/g),
+        processNode: (node, children, index) => {
+          const matches = node.data.match(/{{\s*([^}]+)\s*}}/g);
+          const nodes = [];
+          for (let i = 0, len = matches.length; i < len; i += 1) {
+            const match = matches[i];
+            const value = match.substring(2, match.length - 2);
+            nodes.push(React.createElement('span', {
+              key: `${index}-${i}`
+            }, _get(this.props, `data.values[${value}]`)));
+          }
+          return nodes;
+        }
+      },
+      {
+        shouldProcessNode: () => true,
+        processNode: this.processNodeDefinitions.processDefaultNode,
+      },
+    ];
+    const component = this.htmlToReactParser.parseWithInstructions(
+      `<div>${this.template}</div>`,
+      () => true,
+      processingInstructions
+    );
+    // console.timeEnd('view:text parse template');
+    return component;
+  }
+
+  htmlToReactParser = new Parser();
+  processNodeDefinitions = new ProcessNodeDefinitions(React);
 
   render() {
-    logger.debug('render');
-    const innerHTML = this.getMarkup();
+    const { configuration: { title } } = this.props;
+    const component = this.getComponent();
+    logger.debug(`render ${title}`);
+
     return (
       <div>
-        {/* eslint-disable-next-line react/no-danger */}
-        <div dangerouslySetInnerHTML={{ __html: innerHTML }} />
+        {component}
       </div>
     );
   }

b180d39590591748228906fa99606e27ca4aa62a
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-11-10 16:37:08 +0000

    [HL] . . . . .

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/actions/hss.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/actions/hss.js
@@ -1,8 +1,5 @@
 import simple from '../simpleActionCreator';
 import * as types from '../types';
 
-/**
- * Simple actions
- */
 export const remove = simple(types.HSS_WS_REMOVE, 'identity');
 export const updateStatus = simple(types.HSS_WS_UPDATE_STATUS, 'identity', 'status', 'error');

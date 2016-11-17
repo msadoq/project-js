1f2bdfcc58d7f06f24b0486dc5a42cb83e3325df
Audrey LEAL <audrey.leal.external@atos.net>
2016-11-17 16:37:39 +0000

    [FT:#3622] Add filePicker to choose file to open

---
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/mainProcess/filePicker.js
@@ -0,0 +1,28 @@
+import { dialog } from 'electron';
+import debug from '../common/debug/mainDebug';
+
+const logger = debug('mainProcess:filePicker');
+/**
+ * show a file picker
+ * @param folder : base folder of file picker
+ * @param type of component to open : 'workspace', 'page' or 'view'
+ * @return filepath or undefined si cancel
+ */
+
+function openFileDialog(folder, type) {
+  return dialog.showOpenDialog({
+    title: `Select a ${type}`,
+    defaultPath: folder,
+    buttonLabel: 'Open',
+    filters: [{ name: 'data files', extensions: ['json'] }],
+    properties: ['openFile']
+  });
+}
+
+export default function getPathByFilePicker(folder, type) {
+  const filePath = openFileDialog(folder, type);
+  if (filePath) {
+    return filePath[0];
+  }
+  return undefined;
+}

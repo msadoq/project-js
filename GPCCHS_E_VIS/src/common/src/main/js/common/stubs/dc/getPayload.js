949df53b48747b8724ebf2271438303988a67c63
Renaud PERROT <renaud.perrot@atos.net>
2016-11-18 16:04:34 +0000

    [HL] fix the dc stub for gpau

--- GPCCHS_E_VIS/src/common/src/main/js/common/stubs/dc/getPayload.js
+++ GPCCHS_E_VIS/src/common/src/main/js/common/stubs/dc/getPayload.js
@@ -5,8 +5,12 @@ function getValue(timestamp) {
 }
 
 module.exports = function getPayload(timestamp, epName) {
+  let localEpName = epName;
+  if (!localEpName) {
+    localEpName = 'todo';
+  }
   let epNumber = 0;
-  Buffer.from(epName).forEach((val) => { epNumber += val; });
+  Buffer.from(localEpName).forEach((val) => { epNumber += val; });
   const value = getValue(timestamp) + (epNumber / 10);
   return {
     timestamp: stubData.getTimestampProtobuf({ ms: timestamp }),

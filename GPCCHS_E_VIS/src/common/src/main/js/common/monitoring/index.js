256ac7fe42bc19ff6380e48fdb636758bfbe0743
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-11-21 09:27:58 +0000

    [HL] Add current date to monitoring output

--- GPCCHS_E_VIS/src/common/src/main/js/common/monitoring/index.js
+++ GPCCHS_E_VIS/src/common/src/main/js/common/monitoring/index.js
@@ -16,17 +16,26 @@ let memUsage;
 let monitorTimeout;
 const TIMESTEP = 5000;
 
+const leftPad = number => ((number < 10) ? `0${number}` : number);
+const getTime = () => {
+  const now = new Date();
+  let time = `${now.getFullYear()}-${leftPad(now.getMonth() + 1)}-${leftPad(now.getDate())}`;
+  time += ` ${leftPad(now.getHours())}:${leftPad(now.getMinutes())}:${leftPad(now.getSeconds())}`;
+  return time;
+};
+
 const countTicks = () => setImmediate(() => {
   ticks += 1;
   countTicks();
 });
 
 const monitor = () => {
+  const time = getTime();
   const diffTime = process.hrtime(currentTime);
   avgTime = _round((diffTime[0] / (ticks * 1e-3)) + (diffTime[1] / (ticks * 1e6)), 6);
   ticks = 0;
   memUsage = process.memoryUsage();
-  display(`= monitoring ======== ${(new Date())}`);
+  display(`= monitoring ======== (${time})`);
   display('average time consumption by loop', avgTime, 'ms');
   display('memory consumption');
   display('  rss', bytesToString(memUsage.rss));

fa7ad47e5d16bce7dc427874533922eabd0212dc
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-11-24 13:55:55 +0000

    [FT:#3622] Fix primus client build generation by adding require('ws')

--- GPCCHS_E_VIS/src/server/src/impl/js/server/bin/buildPrimusClient.js
+++ GPCCHS_E_VIS/src/server/src/impl/js/server/bin/buildPrimusClient.js
@@ -1,6 +1,6 @@
 require('dotenv-safe').load();
+const { writeFileSync } = require('fs');
 const debug = require('../lib/io/debug')('buildPrimusClient');
-const exit = require('exit'); // eslint-disable-line import/no-extraneous-dependencies
 const http = require('http');
 const app = require('../lib/express');
 const { join } = require('path');
@@ -10,11 +10,9 @@ const server = http.createServer(app);
 const instance = getNewInstance(server);
 const output = join(__dirname, '../../../../../../common/src/main/js/common/websocket/primus.js');
 
-instance.save(output, (err) => {
-  if (err) {
-    return debug.error(err);
-  }
+// add ws loading to fix a packaging issue
+const library = `const WebSocket = require('ws');\n${instance.library()}`;
 
-  debug.info(`primus client generated in ${output}`);
-  return exit(0);
-});
+// save file
+writeFileSync(output, library);
+debug.info(`primus client generated in ${output}`);

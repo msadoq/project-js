c6db6929ad3103c811de040ea15687270b4184f1
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-11-28 13:32:21 +0000

    [FT:#3622] Re-implement a simple domains selector module

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/selectors/domains.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/selectors/domains.js
@@ -1,12 +1,2 @@
-// import _find from 'lodash/find';
-// import _eq from 'lodash/eq';
-//
-// /**
-//  * Selectors
-//  */
-// // TODO test
-// // TODO rename as getDomainByName (or remove)
-// // TODO implement a simple getDomain (by uuid)
-// export default function getDomain(state, key) {
-//   return _find(state.domains, o => _eq(o.name, key));
-// }
+// eslint-disable-next-line import/prefer-default-export
+export const getDomains = state => state.domains;

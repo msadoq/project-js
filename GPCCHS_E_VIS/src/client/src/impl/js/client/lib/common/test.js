59b08310d0e8323258af0f2915a9ab5de43380dc
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-12-01 08:15:05 +0000

    [HL] . . . . .

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/common/test.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/common/test.js
@@ -1,4 +1,4 @@
-import chai from 'chai'; // eslint-disable-line import/no-extraneous-dependencies
+import chai from 'chai';
 import properties from 'chai-properties';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
@@ -6,19 +6,14 @@ import _cloneDeep from 'lodash/cloneDeep';
 import deepFreeze from 'deep-freeze';
 import reducer from '../store/reducers/index';
 
+process.env.DEBUG = '';
+process.env.LEVEL = 'ERROR';
 process.env.PROFILING = 'off';
 process.env.MONITORING = 'off';
 process.env.FMD_ROOT =
   '/data/work/gitRepositories/LPISIS/GPCCHS/GPCCHS_E_VIS/src/client/src/impl/js/client/lib/documentsManager/examples/';
-require('dotenv-safe').load();
-// import 'babel-polyfill';
-// import { jsdom } from 'jsdom';
 
 chai.use(properties);
-
-// global.document = jsdom('<!doctype html><html><body></body></html>');
-// global.window = document.defaultView;
-// global.navigator = global.window.navigator;
 
 function getStore(initialState) {
   return createStore(

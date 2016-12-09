baa47ef6d16808a7489d5789e4347a1e59fbd717
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2016-12-09 09:54:59 +0000

    [FT:#3622] Remove unused obsolete debug module files

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/common/debug/mainDebug.js
+++
@@ -1,5 +0,0 @@
-import deprecate from 'depd';
-import getLogger from 'common/log';
-
-// TODO deprecated
-module.exports = deprecate('common/debug/mainDebug').function(getLogger);
--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/common/debug/windowDebug.js
+++
@@ -1,5 +0,0 @@
-import debug from 'debug/browser';
-import debugHelper from 'common/debug';
-
-// return debug helper with browser implementation
-module.exports = debugHelper(debug);

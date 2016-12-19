664a2c470c54cf89fc886ddc0664fc756bbb9473
Raphael FABRE <raphael.fabre.external@atos.net>
2016-12-19 14:10:16 +0000

    [FT:#3622] New Timebar Help modal in the Control area.

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/windowProcess/Timebar/ControlsRight.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/windowProcess/Timebar/ControlsRight.js
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import classnames from 'classnames';
+import Help from './Help';
+import Modal from '../common/Modal';
 import styles from './Controls.css';
 
 export default class TimebarControlsRight extends Component {
@@ -13,6 +15,17 @@ export default class TimebarControlsRight extends Component {
     timebarMode: PropTypes.string.isRequired,
     timebarId: PropTypes.string.isRequired,
     currentSession: PropTypes.object,
+  }
+
+  state = {
+    showHelpModal: false,
+  }
+
+  toggleHelpModal = (e) => {
+    e.preventDefault();
+    this.setState({
+      showHelpModal: !this.state.showHelpModal,
+    });
   }
 
   switchMode = (e) => {
@@ -50,72 +63,94 @@ export default class TimebarControlsRight extends Component {
       currentSession
     } = this.props;
 
+    const {
+      showHelpModal,
+    } = this.state;
+
     const allButtonsKlasses = classnames('btn', 'btn-xs', 'btn-control');
 
     return (
-      <ul className={classnames(styles.controlsUl, styles.controlsUlRight)}>
-        <li className={styles.controlsLi}>
-          <button
-            mode="Normal"
-            className={classnames(
-              allButtonsKlasses,
-              {
-                [styles.controlButtonActive]: (timebarMode === 'Normal')
-              }
-            )}
-            onClick={this.switchMode}
-            title="Normal mode"
-          >
-            Normal
-          </button>
-        </li>
-        <li className={styles.controlsLi}>
-          <button
-            mode="Extensible"
-            className={classnames(
-              allButtonsKlasses,
-              {
-                [styles.controlButtonActive]: (timebarMode === 'Extensible')
-              }
-            )}
-            onClick={this.switchMode}
-            title="Extensible mode"
-          >
-            Extensible
-          </button>
-        </li>
-        <li className={styles.controlsLi}>
-          <button
-            mode="Fixed"
-            className={classnames(
-              allButtonsKlasses,
-              {
-                [styles.controlButtonActive]: (timebarMode === 'Fixed')
-              }
-            )}
-            onClick={this.switchMode}
-            title="Fixed mode"
-          >
-            Fixed
-          </button>
-        </li>
-        <li className={styles.controlsLi}>
-          <button
-            mode="Realtime"
-            className={classnames(
-              allButtonsKlasses,
-              {
-                [styles.controlButtonActive]: (timebarMode === 'Realtime')
-              }
-            )}
-            onClick={this.switchMode}
-            title={currentSession ? 'Real time mode' : "No master track is set, can't go realtime"}
-            disabled={!currentSession}
-          >
-            Real time
-          </button>
-        </li>
-      </ul>
+      <div>
+        <ul className={classnames(styles.controlsUl, styles.controlsUlRight)}>
+          <li className={styles.controlsLi}>
+            <button
+              mode="Normal"
+              className={classnames(
+                allButtonsKlasses,
+                {
+                  [styles.controlButtonActive]: (timebarMode === 'Normal')
+                }
+              )}
+              onClick={this.switchMode}
+              title="Normal mode"
+            >
+              Normal
+            </button>
+          </li>
+          <li className={styles.controlsLi}>
+            <button
+              mode="Extensible"
+              className={classnames(
+                allButtonsKlasses,
+                {
+                  [styles.controlButtonActive]: (timebarMode === 'Extensible')
+                }
+              )}
+              onClick={this.switchMode}
+              title="Extensible mode"
+            >
+              Extensible
+            </button>
+          </li>
+          <li className={styles.controlsLi}>
+            <button
+              mode="Fixed"
+              className={classnames(
+                allButtonsKlasses,
+                {
+                  [styles.controlButtonActive]: (timebarMode === 'Fixed')
+                }
+              )}
+              onClick={this.switchMode}
+              title="Fixed mode"
+            >
+              Fixed
+            </button>
+          </li>
+          <li className={styles.controlsLi}>
+            <button
+              mode="Realtime"
+              className={classnames(
+                allButtonsKlasses,
+                {
+                  [styles.controlButtonActive]: (timebarMode === 'Realtime')
+                }
+              )}
+              onClick={this.switchMode}
+              title={currentSession ? 'Real time mode' : "No master track is set, can't go realtime"}
+              disabled={!currentSession}
+            >
+              Real time
+            </button>
+          </li>
+          <li className={styles.controlsLi}>
+            <button
+              className={allButtonsKlasses}
+              onClick={this.toggleHelpModal}
+              title="Help modal"
+            >
+              ?
+            </button>
+          </li>
+        </ul>
+        <Modal
+          title="Timebar main commands"
+          onClose={this.toggleHelpModal}
+          isOpened={showHelpModal}
+        >
+          <Help />
+        </Modal>
+      </div>
     );
   }
 }

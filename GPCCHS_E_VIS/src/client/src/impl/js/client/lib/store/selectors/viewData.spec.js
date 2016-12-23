3c6b2d628b9c3188a0a7f95c293b9dd424c8b812
Paul Musso <paul.musso.external@atos.net>
2016-12-23 10:18:40 +0000

    [FT:#3622] Add tests on selectors viewData

---
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/selectors/viewData.spec.js
@@ -0,0 +1,13 @@
+import {
+  getViewData,
+} from './viewData';
+
+describe('store:viewData:selectors', () => {
+  it('getViewData', () => {
+    getViewData({
+      viewData: {
+        view1: {}
+      }
+    }, 'view1').should.be.an('object');
+  });
+});

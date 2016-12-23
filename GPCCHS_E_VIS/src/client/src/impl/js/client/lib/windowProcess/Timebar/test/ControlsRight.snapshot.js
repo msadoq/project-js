88bb989fa42f611ff9726aff2611d1cc00342c3d
Raphael FABRE <raphael.fabre.external@atos.net>
2016-12-23 09:17:30 +0000

    [FT:#3622] Snapshot Controls split in two snapshots : Left and Right .

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/windowProcess/Timebar/test/ControlsRight.snapshot.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/windowProcess/Timebar/test/ControlsRight.snapshot.js
@@ -1,33 +1,22 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import TimebarFixture from '../../../../test/__mocks__/Timebar';
-import Controls from '../Controls';
+import ControlsRight from '../ControlsRight';
 
 const propsStub = {
-  updateSpeed: () => null,
-  updateCursors: () => null,
-  updateMode: () => null,
-  toggleTimesetter: () => null,
-  pause: () => null,
   play: () => null,
   switchToFixedMode: () => null,
   switchToExtensibleMode: () => null,
   switchToRealtimeMode: () => null,
   switchToNormalMode: () => null,
-  restoreWidth: () => null,
-  goNow: () => null,
-  jump: () => null,
-  isPlaying: true,
   currentSessionExists: true,
-  timebarSpeed: TimebarFixture.speed,
   timebarMode: TimebarFixture.mode,
   timebarId: TimebarFixture.timebarId,
-  messages: [],
 };
 
 test('Controls renders correctly', () => {
   const tree = renderer.create(
-    <Controls {...propsStub} />
+    <ControlsRight {...propsStub} />
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });

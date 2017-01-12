b2772484e3ef407060bf40df5320819d7855f5a1
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2017-01-12 13:09:10 +0000

    [FT:#3622] Remove unused files from project

--- GPCCHS_E_VIS/src/common/src/main/js/common/stubs/dc.js
+++ GPCCHS_E_VIS/src/common/src/main/js/common/stubs/dc.js
@@ -1,6 +1,5 @@
 const _each = require('lodash/each');
 const _omit = require('lodash/omit');
-const { bind: bindMainIpc } = require('../ipc');
 const logger = require('../log')('stubs:dc');
 const zmq = require('../zmq');
 const globalConstants = require('../constants');
@@ -166,10 +165,9 @@ zmq.open(
       return;
     }
 
-    // bind IPC channel with main process
-    bindMainIpc();
-
     logger.info('sockets opened');
+    process.send('ready');
+
     nextDcCall();
   }
 );

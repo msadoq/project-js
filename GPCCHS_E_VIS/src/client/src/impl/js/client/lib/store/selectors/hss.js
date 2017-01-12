7cba341050d17ecb9c8a977baa5d2c4050248074
Damien Brugne <dbrugne@cnes-isis.toulouse.atos.net>
2017-01-12 16:46:27 +0000

    [FT:#3622] Remove obsolete selector file from project

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/selectors/hss.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export function getStatus(state, identity) {
-  const identityObject = state.hss[identity];
-  if (!identityObject) {
-    return { status: 'disconnected' };
-  }
-  return identityObject;
-}

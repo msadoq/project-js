fd3cafb9f53a4eb59cadbf7ad8575b3b3d44dc85
Guillaume ARM <guillaume.arm.external@atos.net>
2017-01-19 13:03:40 +0000

    [FT:#3622] Fix common/test, add LOG_FOLDER environment variable

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/common/test.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/common/test.js
@@ -12,6 +12,7 @@ process.env.DEBUG = '';
 process.env.LEVEL = 'ERROR';
 process.env.PROFILING = 'off';
 process.env.MONITORING = 'off';
+process.env.LOG_FOLDER = 'log';
 
 process.env.FMD_ROOT_DIR = path.resolve(__dirname, '../documentManager/fixtures');
 

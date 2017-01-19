c29f0c08b42ef8cae7e376d9787aa8ad04dbb6fe
Audrey LEAL <audrey.leal.external@atos.net>
2017-01-19 16:15:43 +0000

    [FT:#3622] Amend : Update view and editor for dynamic view

---
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/windowProcess/Editor/Components/Dynamic/DynamicTab.js
@@ -0,0 +1,33 @@
+import React, { PropTypes } from 'react';
+import {
+  Panel
+} from 'react-bootstrap';
+import ViewParamsContainer from '../ViewParamsContainer';
+
+export default class DynamicTab extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+    handleTextTitle: PropTypes.func,
+    titleStyle: PropTypes.object,
+    handleTextTitleStyle: PropTypes.func
+  }
+  static contextTypes = {
+    viewId: React.PropTypes.string
+  };
+  state = {
+    isTitleOpen: false
+  };
+
+  openTitle = () => this.setState({ isTitleOpen: true });
+  closeTitle = () => this.setState({ isTitleOpen: false });
+
+  render() {
+    const { viewId } = this.context;
+
+    return (
+      <Panel>
+        <ViewParamsContainer viewId={viewId} />
+      </Panel>
+    );
+  }
+}

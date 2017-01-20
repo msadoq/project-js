9b35301a2fd51372516bcb8a1b5682cd4dab6f30
Paul Musso <paul.musso.external@atos.net>
2017-01-20 13:26:13 +0000

    [FT:#3622] productLog add lockfile and refacto async code

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/mainProcess/index.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/mainProcess/index.js
@@ -28,7 +28,6 @@ export function start() {
     execPath: parameters.get('NODE_PATH'),
     env: {
       LOG: parameters.get('LOG'),
-      LOG_DIR: parameters.get('LOG_DIR'),
       MONITORING: parameters.get('MONITORING'),
       PROFILING: parameters.get('PROFILING'),
       ZMQ_GPCCDC_PUSH: parameters.get('ZMQ_GPCCDC_PUSH'),

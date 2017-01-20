d356cd6a9b9b4d424bf5f6b16e513b19c2e0b76c
Audrey LEAL <audrey.leal.external@atos.net>
2017-01-20 15:43:16 +0000

    [FT:#3622] Add a tooltip on page tab to show document properties

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/windowProcess/Navigation/Tabs.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/windowProcess/Navigation/Tabs.js
@@ -1,9 +1,32 @@
 import React, { Component, PropTypes } from 'react';
-import { Nav, NavItem, Button, Glyphicon } from 'react-bootstrap';
+import { Nav, NavItem, Button, Glyphicon, Popover, OverlayTrigger, Table } from 'react-bootstrap';
 import getLogger from 'common/log';
 import styles from './Tabs.css';
 
 const logger = getLogger('Tabs');
+
+function popoverHoverFocus(page) {
+  // TODO to be completed with document information
+  return (
+    <Popover id="popover-trigger-hover-focus" title="Document properties">
+      <Table>
+        <tbody>
+          <tr><td>Version</td><td>Unknown</td></tr>
+          {page.oId
+            ? <tr>
+              <td>OId</td>
+              <td>{page.oId}</td>
+            </tr>
+            : <tr>
+              <td>Path</td>
+              <td>{page.path}</td>
+            </tr>
+          }
+        </tbody>
+      </Table>
+    </Popover>
+  );
+}
 
 export default class Tabs extends Component {
   static propTypes = {
@@ -32,27 +55,35 @@ export default class Tabs extends Component {
   render() {
     logger.debug('render');
     const { pages, focusedPageId } = this.props;
+
     return (
       <Nav bsStyle="tabs" activeKey={focusedPageId} onSelect={this.handleSelect}>
         {pages.map(page =>
-          <NavItem
+          <OverlayTrigger
+            trigger={['hover', 'focus']}
+            placement="bottom"
+            overlay={popoverHoverFocus(page)}
             key={page.pageId}
-            eventKey={page.pageId}
           >
-            <div className={styles.title}>
-              {page.isModified ? page.title.concat(' *') : page.title}
-              <Button
-                bsStyle="link"
-                onClick={e => this.handleClose(e, page.pageId)}
-                className={styles.button}
-              >
-                <Glyphicon
-                  glyph="remove-circle"
-                  className="text-danger"
-                />
-              </Button>
-            </div>
-          </NavItem>
+            <NavItem
+              key={page.pageId}
+              eventKey={page.pageId}
+            >
+              <div className={styles.title}>
+                {page.isModified ? page.title.concat(' *') : page.title}
+                <Button
+                  bsStyle="link"
+                  onClick={e => this.handleClose(e, page.pageId)}
+                  className={styles.button}
+                >
+                  <Glyphicon
+                    glyph="remove-circle"
+                    className="text-danger"
+                  />
+                </Button>
+              </div>
+            </NavItem>
+          </OverlayTrigger>
         )}
         <NavItem eventKey="new">
           New page

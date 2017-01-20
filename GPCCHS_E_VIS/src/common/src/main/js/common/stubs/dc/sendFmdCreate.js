b555d38d0f582ae98d9e2e0cccae77b66f06f8c1
Guillaume ARM <guillaume.arm.external@atos.net>
2017-01-20 10:47:21 +0000

    [FT:#3622] Fix stubs dc sendFmdCreate about oid

--- GPCCHS_E_VIS/src/common/src/main/js/common/stubs/dc/sendFmdCreate.js
+++ GPCCHS_E_VIS/src/common/src/main/js/common/stubs/dc/sendFmdCreate.js
@@ -12,15 +12,43 @@ module.exports = function sendFmdCreate(queryId, { name, path }, zmq) {
     null,
   ];
 
-  const createPath = join(get('FMD_ROOT_DIR'), path, name);
+  const fullPath = join(path, name);
+  const createPath = join(get('FMD_ROOT_DIR'), fullPath);
   if (existsSync(createPath)) {
     buffer[3] = stubData.getErrorStatusProtobuf();
     buffer[4] = stubData.getStringProtobuf(`this file already exists: ${createPath}`);
   } else {
     writeFileSync(createPath, '', 'utf8');
     buffer[3] = stubData.getSuccessStatusProtobuf();
-    buffer[4] = stubData.getFMDFileInfoProtobuf();
+    buffer[4] = stubData.getFMDFileInfoProtobuf({
+      serializedOid: `oid:${fullPath}`,
+    });
   }
 
   zmq.push('stubData', buffer);
 };
+//
+// module.exports = function sendFmdGet(queryId, oid, zmq) {
+//   const buffer = [
+//     null,
+//     stubData.getFmdGetDataHeaderProtobuf(),
+//     stubData.getStringProtobuf(queryId),
+//     null,
+//     null,
+//   ];
+//
+//   const path = oid.replace('oid:', '');
+//   if (!existsSync(join(get('FMD_ROOT_DIR'), path))) {
+//     buffer[3] = stubData.getErrorStatusProtobuf();
+//     buffer[4] = stubData.getStringProtobuf(`this file doesn't exist ${path}`);
+//   } else {
+//     buffer[3] = stubData.getSuccessStatusProtobuf();
+//     buffer[4] = stubData.getFMDFileInfoProtobuf();
+//     buffer[5] = stubData.getDocumentProtobuf({
+//       dirname: dirname(path),
+//       basename: basename(path),
+//     });
+//   }
+//
+//   zmq.push('stubData', buffer);
+// };

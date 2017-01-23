b08b8df2a826dff281099599e3c9be03eef592d9
Guillaume ARM <guillaume.arm.external@atos.net>
2017-01-23 09:22:21 +0000

    [FT:#3622] Add isFmd test (fmd api)

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/common/fmd.spec.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/common/fmd.spec.js
@@ -1,120 +1,32 @@
-import { join } from 'path';
-import {
-  mkdirSync,
-  writeFileSync,
-  rmdirSync,
-  unlinkSync,
-  accessSync,
-  constants,
-  chmodSync
-} from 'fs';
+// import { join } from 'path';
+// import {
+//   mkdirSync,
+//   writeFileSync,
+//   rmdirSync,
+//   unlinkSync,
+//   accessSync,
+//   constants,
+//   chmodSync
+// } from 'fs';
 
 import { get } from 'common/parameters';
+import { getRootDir, isFmd } from './fmd';
 
-import { should, expect, getTmpPath } from '../common/test';
-import fmd from './fmd';
-
-describe('common/fmd', () => {
-  const folder = getTmpPath();
-  const txtFile = join(folder, '/foo.txt');
-  const json = join(folder, '/foo.json');
-  const unreadable = join(folder, '/unreadable.txt');
-  const notExists = join(folder, '/not-exists.txt');
-  before(() => {
-    try {
-      accessSync(folder, constants.F_OK);
-    } catch (e) {
-      mkdirSync(folder);
-    }
-    try {
-      accessSync(txtFile, constants.F_OK);
-    } catch (e) {
-      writeFileSync(txtFile, 'my content');
-    }
-    try {
-      accessSync(json, constants.F_OK);
-    } catch (e) {
-      writeFileSync(json, '{"foo":"bar"}');
-    }
-    try {
-      accessSync(unreadable, constants.F_OK);
-    } catch (e) {
-      writeFileSync(unreadable, 'my content');
-      chmodSync(unreadable, 0);
-    }
-  });
-  after(() => {
-    try {
-      unlinkSync(txtFile);
-      unlinkSync(json);
-      chmodSync(unreadable, 777);
-      unlinkSync(unreadable);
-      rmdirSync(folder);
-    } catch (e) {
-      console.warn(e); // eslint-disable-line no-console
-    }
-  });
-
+describe.only('common/fmd', () => {
   describe('getRootDir', () => {
     it('should return same thing than parameters.get', () => {
-      fmd.getRootDir().should.eql(get('FMD_ROOT_DIR'));
+      getRootDir().should.eql(get('FMD_ROOT_DIR'));
     });
   });
 
-  // describe('readJson', () => {
-  //   it('works with relativePath and folder', (done) => {
-  //     fmd.readJson(folder, 'foo.json', undefined, undefined, (err, content) => {
-  //       should.not.exist(err);
-  //       content.should.eql({ foo: 'bar' });
-  //       done();
-  //     });
-  //   });
-  //   it('works with only relativePath', (done) => {
-  //     // you can found '/text1.json' in lib/documentManager/fixtures
-  //     fmd.readJson(undefined, '/text1.json', undefined, undefined, (err, content) => {
-  //       should.exist(content);
-  //       should.not.exist(err);
-  //       done();
-  //     });
-  //   });
-  //   it('works with absolutePath', (done) => {
-  //     fmd.readJson(undefined, undefined, undefined, json, (err, content) => {
-  //       should.not.exist(err);
-  //       content.should.eql({ foo: 'bar' });
-  //       done();
-  //     });
-  //   });
-  //   it('works with oid', (done) => {
-  //     const oid = `oid:${json}`;
-  //     fmd.readJson(undefined, undefined, oid, undefined, (err, content) => {
-  //       should.not.exist(err);
-  //       content.should.eql({ foo: 'bar' });
-  //       done();
-  //     });
-  //   });
-  //   it('fail with bad file format', (done) => {
-  //     fmd.readJson(undefined, undefined, undefined, txtFile, (err, content) => {
-  //       expect(content).to.be.an('undefined');
-  //       should.exist(err);
-  //       done();
-  //     });
-  //   });
-  //   it('fail with unreadable file', (done) => {
-  //     fmd.readJson(undefined, undefined, undefined, unreadable, (err, content) => {
-  //       expect(content).to.be.an('undefined');
-  //       should.exist(err);
-  //       done();
-  //     });
-  //   });
-  //   it('fail with absent file', (done) => {
-  //     fmd.readJson(undefined, undefined, undefined, notExists, (err, content) => {
-  //       expect(content).to.be.an('undefined');
-  //       should.exist(err);
-  //       done();
-  //     });
-  //   });
-  // });
-  // describe('writeJson', (done) => {
-  //   done();
-  // });
+  describe('isFmd', () => {
+    // isFmd do not check fs for existing path, it's just a pure function
+    it('should be true', () => {
+      isFmd(`${getRootDir()}/missing`).should.be.eql(true);
+      isFmd(getRootDir()).should.be.eql(true);
+    });
+    it('should be false', () => {
+      isFmd('/').should.be.eql(false);
+    });
+  });
 });

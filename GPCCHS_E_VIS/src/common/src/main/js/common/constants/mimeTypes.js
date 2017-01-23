81933f188720b82a48beac222a611c3c0a7bcfcc
Audrey LEAL <audrey.leal.external@atos.net>
2017-01-23 11:04:14 +0000

    [FT:#3622] Create a model from a view

--- GPCCHS_E_VIS/src/common/src/main/js/common/constants/mimeTypes.js
+++ GPCCHS_E_VIS/src/common/src/main/js/common/constants/mimeTypes.js
@@ -3,6 +3,7 @@ module.exports = {
   Page: 'PageDoc',
   TextView: 'TextViewDoc',
   PlotView: 'PlotViewDoc',
+  DynamicView: 'DynamicViewDoc',
   MimicView: 'MimicViewDoc',
   PacketView: 'PacketViewDoc',
   HistoryView: 'HistoryViewDoc',

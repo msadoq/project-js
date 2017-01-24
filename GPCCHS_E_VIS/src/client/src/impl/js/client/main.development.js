bd3d06ec77fbb5a5c84aa083d5240b6f8078aad6
Paul Musso <paul.musso.external@atos.net>
2017-01-24 07:05:08 +0000

    [FT:#3622] remove comment in main.developpment.js .

--- GPCCHS_E_VIS/src/client/src/impl/js/client/main.development.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/main.development.js
@@ -5,16 +5,15 @@ import {
   LOG_APPLICATION_STOP,
   LOG_APPLICATION_ERROR,
 } from 'common/constants';
-import { server } from './lib/mainProcess/ipc';
 import { start, stop, onWindowsClose } from './lib/mainProcess'; // eslint-disable-line import/first
+import { server } from './lib/mainProcess/ipc';
 
 const logger = getLogger('main');
+
 init(__dirname, true);
 
 // avoid using host proxy configuration and perturbing local HTTP access (e.g.: index.html)
 app.commandLine.appendSwitch('no-proxy-server');
-
-// const distLogger = getLogger('Application', ['dist']);
 
 const errorHandler = (err) => {
   console.error(err); // eslint-disable-line no-console

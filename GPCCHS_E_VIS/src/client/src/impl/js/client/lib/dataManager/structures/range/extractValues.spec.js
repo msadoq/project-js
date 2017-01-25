81e8bd257593aa7eab2a3dde281fecebf5971642
Audrey LEAL <audrey.leal.external@atos.net>
2017-01-25 08:58:03 +0000

    [FT:#3622] Fix unit tests for using enum data

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/dataManager/structures/range/extractValues.spec.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/dataManager/structures/range/extractValues.spec.js
@@ -9,6 +9,7 @@ describe('data/map/extractValues', () => {
       val1: { type: 'uinteger', value: (j * 10) + 1 },
       val2: { type: 'uinteger', value: (j * 10) + 2 },
       val3: { type: 'uinteger', value: (j * 10) + 3 },
+      val4: { type: 'enum', value: j - 10, symbol: 'val'.concat(j - 10) },
       referenceTimestamp: { type: 'time', value: j },
       time: { type: 'time', value: j + 0.2 },
       monitoringState: { type: 'uinteger', value: 'ok' },
@@ -27,7 +28,7 @@ describe('data/map/extractValues', () => {
           fieldY: 'val1',
           expectedInterval: [10, 15],
           offset: 0,
-        }
+        },
       },
     },
     plot2: {
@@ -43,7 +44,7 @@ describe('data/map/extractValues', () => {
         ep3: {
           remoteId: 'rId2',
           fieldX: 'time',
-          fieldY: 'val2',
+          fieldY: 'val4',
           expectedInterval: [14, 18],
           offset: 0,
         }
@@ -68,22 +69,23 @@ describe('data/map/extractValues', () => {
     it('state undefined', () => {
       const newState = select(payload.rId1, viewDataMap.plot1.entryPoints.ep1,
         'ep1', undefined, count);
-      newState['10'].ep1.should.deep.equal({ x: 10.2, value: 101, monit: 'ok' });
-      newState['15'].ep1.should.deep.equal({ x: 15.2, value: 151, monit: 'ok' });
+      newState['10'].ep1.should.eql({ x: 10.2, value: 101, monit: 'ok', symbol: undefined });
+      newState['15'].ep1.should.eql({ x: 15.2, value: 151, monit: 'ok', symbol: undefined });
     });
     it('state empty', () => {
       const newState = select(payload.rId1, viewDataMap.plot1.entryPoints.ep1,
         'ep1', {}, count);
-      newState['10'].should.deep.equal({ ep1: { x: 10.2, value: 101, monit: 'ok' } });
-      newState['15'].should.deep.equal({ ep1: { x: 15.2, value: 151, monit: 'ok' } });
+      newState['10'].should.eql({ ep1: { x: 10.2, value: 101, monit: 'ok', symbol: undefined } });
+      newState['15'].should.eql({ ep1: { x: 15.2, value: 151, monit: 'ok', symbol: undefined } });
     });
     it('state not empty', () => {
       const oldState = { 10: { ep10: { x: 11.5, col1: 101 } },
         12.5: { ep10: { x: 12.5, value: 102 } } };
       const newState = select(payload.rId1, viewDataMap.plot1.entryPoints.ep1,
         'ep1', oldState, count);
-      newState['10'].should.deep.equal({ ep1: { x: 10.2, value: 101, monit: 'ok' }, ep10: { x: 11.5, col1: 101 } });
-      newState['15'].should.deep.equal({ ep1: { x: 15.2, value: 151, monit: 'ok' } });
+      newState['10'].should.eql({ ep1: { x: 10.2, value: 101, monit: 'ok', symbol: undefined },
+        ep10: { x: 11.5, col1: 101 } });
+      newState['15'].should.eql({ ep1: { x: 15.2, value: 151, monit: 'ok', symbol: undefined } });
     });
     it('no change', () => {
       const oldState = { 10: { ep1: [{ x: 1001.5, col1: 101 }, { x: 1002.5, value: 102 }] } };
@@ -101,8 +103,8 @@ describe('data/map/extractValues', () => {
       viewData.remove.lower.should.equal(ep.expectedInterval[0] + ep.offset);
       viewData.remove.upper.should.equal(ep.expectedInterval[1] + ep.offset);
       viewData.structureType.should.equal(globalConstants.DATASTRUCTURETYPE_RANGE);
-      viewData.add['10'].should.deep.equal({ ep1: { x: 10.2, value: 101, monit: 'ok' } });
-      viewData.add['15'].should.deep.equal({ ep1: { x: 15.2, value: 151, monit: 'ok' } });
+      viewData.add['10'].should.eql({ ep1: { x: 10.2, value: 101, monit: 'ok', symbol: undefined } });
+      viewData.add['15'].should.eql({ ep1: { x: 15.2, value: 151, monit: 'ok', symbol: undefined } });
       Object.keys(viewData.add).should.have.length(6);
     });
     it('multiple entry point', () => {
@@ -113,8 +115,10 @@ describe('data/map/extractValues', () => {
       viewData.remove.lower.should.equal(ep.expectedInterval[0] + ep.offset);
       viewData.remove.upper.should.equal(ep.expectedInterval[1] + ep.offset);
       viewData.structureType.should.equal(globalConstants.DATASTRUCTURETYPE_RANGE);
-      viewData.add['14'].should.deep.equal({ ep2: { x: 12.2, value: 122, monit: 'ok' }, ep3: { x: 14.2, value: 142, monit: 'ok' } });
-      viewData.add['18'].should.deep.equal({ ep2: { x: 16.2, value: 162, monit: 'ok' }, ep3: { x: 18.2, value: 182, monit: 'ok' } });
+      viewData.add['14'].should.eql({ ep2: { x: 12.2, value: 122, monit: 'ok', symbol: undefined },
+        ep3: { x: 14.2, symbol: 'val4', value: 4, monit: 'ok' } });
+      viewData.add['18'].should.eql({ ep2: { x: 16.2, value: 162, monit: 'ok', symbol: undefined },
+        ep3: { x: 18.2, symbol: 'val8', value: 8, monit: 'ok' } });
       Object.keys(viewData.add).should.have.length(5);
     });
   });

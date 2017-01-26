06b7e7c9d6d8c7afd3435ec2b768431cc85efa51
Damien BRUGNE <damien.brugne.external@atos.net>
2017-01-26 13:48:55 +0000

    [FT:#3622] Fix linting in PlotView component

--- GPCCHS_E_VIS/src/client/src/impl/js/client/VIVL/PlotView/window/PlotView.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/VIVL/PlotView/window/PlotView.js
@@ -779,68 +779,6 @@ export class PlotView extends PureComponent {
               bgheight={50}
             />
           </ChartCanvas>
-          { /* @TODO Uncomment when implementing markers!
-            do the same for the Editor PlotTab part.
-          <PlotMenu
-            isOpened={isMenuOpened}
-            openOnLeft={menuOpenOnLeft}
-            openOnTop={menuOpenOnTop}
-            mousePosition={menuPosition}
-          >
-            {disconnected &&
-              <Button
-                bsStyle="danger"
-                bsSize="xs"
-                className={styles.disconnectedButton}
-                onClick={this.reconnect}
-              >Reconnect</Button>
-            }
-            <ChartCanvas
-              plotFull={false}
-              ratio={1}
-              width={containerWidth}
-              height={containerHeight}
-              margin={marginChart}
-              pointsPerPxThreshold={4}
-              seriesName="PlotView"
-              data={columns}
-              type="hybrid"
-              xAccessor={this.xAccessor}
-              xScale={scaleTime()}
-              yAxisZoom={(id, domain) => console.log('zoom', id, domain)}
-              zoomEvent={!disableZoom}
-              xExtents={[new Date(lower), new Date(upper)]}
-            >
-              {this.getCharts()}
-              <CrossHairCursor opacity={1} />
-              <HoverTooltip
-                tooltipContent={this.handleTooltipContent}
-                opacity={1}
-                fill="#FFFFFF"
-                stroke="#F0F0F0"
-                bgwidth={160}
-                bgheight={50}
-                tooltipCanvas={this.tooltipCanvas}
-                backgroundShapeCanvas={this.backgroundShapeCanvas.bind(this)}
-              />
-            </ChartCanvas>
-
-            { /* @TODO Uncomment when implementing markers!
-              do the same for the Editor PlotTab part.
-            <PlotMenu
-              isOpened={isMenuOpened}
-              openOnLeft={menuOpenOnLeft}
-              openOnTop={menuOpenOnTop}
-              mousePosition={menuPosition}
-            >
-              <MenuItem header>Markers</MenuItem>
-              <MenuItem divider />
-              <MenuItem>Add a Text</MenuItem>
-              <MenuItem>Add an horizontal line</MenuItem>
-              <MenuItem>Add an Vertical line</MenuItem>
-            </PlotMenu>
-            */
-          }
         </div>
       </DroppableContainer>
     );
@@ -850,3 +788,62 @@ export class PlotView extends PureComponent {
 const SizeablePlotView = Dimensions()(PlotView);
 
 export default SizeablePlotView;
+
+/** @TODO Uncomment when implementing markers! do the same for the Editor PlotTab part.
+ <PlotMenu
+ isOpened={isMenuOpened}
+ openOnLeft={menuOpenOnLeft}
+ openOnTop={menuOpenOnTop}
+ mousePosition={menuPosition}
+ >
+ {disconnected &&
+ <Button
+ bsStyle="danger"
+ bsSize="xs"
+ className={styles.disconnectedButton}
+ onClick={this.reconnect}
+ >Reconnect</Button>
+ }
+ <ChartCanvas
+ plotFull={false}
+ ratio={1}
+ width={containerWidth}
+ height={containerHeight}
+ margin={marginChart}
+ pointsPerPxThreshold={4}
+ seriesName="PlotView"
+ data={columns}
+ type="hybrid"
+ xAccessor={this.xAccessor}
+ xScale={scaleTime()}
+ yAxisZoom={(id, domain) => console.log('zoom', id, domain)}
+ zoomEvent={!disableZoom}
+ xExtents={[new Date(lower), new Date(upper)]}
+ >
+ {this.getCharts()}
+ <CrossHairCursor opacity={1} />
+ <HoverTooltip
+ tooltipContent={this.handleTooltipContent}
+ opacity={1}
+ fill="#FFFFFF"
+ stroke="#F0F0F0"
+ bgwidth={160}
+ bgheight={50}
+ tooltipCanvas={this.tooltipCanvas}
+ backgroundShapeCanvas={this.backgroundShapeCanvas.bind(this)}
+ />
+ </ChartCanvas>
+ do the same for the Editor PlotTab part.
+ <PlotMenu
+ isOpened={isMenuOpened}
+ openOnLeft={menuOpenOnLeft}
+ openOnTop={menuOpenOnTop}
+ mousePosition={menuPosition}
+ >
+ <MenuItem header>Markers</MenuItem>
+ <MenuItem divider />
+ <MenuItem>Add a Text</MenuItem>
+ <MenuItem>Add an horizontal line</MenuItem>
+ <MenuItem>Add an Vertical line</MenuItem>
+ </PlotMenu>
+*/

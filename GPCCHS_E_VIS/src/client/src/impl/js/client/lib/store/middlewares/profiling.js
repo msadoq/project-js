a7cd0f88caf3d04a544d7a5da645bdc7b9fd522f
Guillaume ARM <guillaume.arm.external@atos.net>
2017-01-27 13:14:16 +0000

    [FT:#3622] Add profiling middleware and withProfiling higher order action creator

---
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/middlewares/profiling.js
@@ -0,0 +1,23 @@
+import executionMonitor from 'common/log/execution';
+import has from 'lodash/fp/has';
+import getOr from 'lodash/fp/getOr';
+
+const hasProfiling = has('meta.profiling');
+const getProfiling = getOr({}, 'meta.profiling');
+
+// you can use withProfiling higher order action creator to use execution profiling on the store
+const profilingMiddleware = ({ getState }) => next => (action) => {
+  const { predicate, namespace, key, msg } = getProfiling(action);
+  if (!hasProfiling(action) || !predicate(getState(), action)) {
+    return next(action);
+  }
+  const execution = executionMonitor(namespace);
+
+  execution.start(key); // start profiling
+  const returnedAction = next(action); // action pass in reducer
+  execution.stop(key, msg); // stop profiling
+
+  return returnedAction;
+};
+
+export default profilingMiddleware;

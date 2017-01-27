438d3c09b81c6892470b7d63235424ceb04498fd
Guillaume ARM <guillaume.arm.external@atos.net>
2017-01-27 13:57:23 +0000

    [FT:#3622] Enable profiling middleware on mainStore only on DEBUG

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/mainStore.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/mainStore.js
@@ -1,13 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { electronEnhancer } from 'redux-electron-store';
 import thunk from 'redux-thunk';
+
+import { get } from 'common/parameters';
 import reducer from './reducers';
+import profiling from './middlewares/profiling';
 
 let store;
 
 export function initStore(initialState) {
+  const middlewares = get('DEBUG') ? [profiling, thunk] : [thunk];
   const enhancer = compose(
-    applyMiddleware(thunk),
+    applyMiddleware(...middlewares),
     electronEnhancer()
   );
 

008b50217a34f09e4743b52c36b92e5a4364ccd9
Walid ETTAIEB <walid.ettaieb@atos.net>
2017-01-27 12:20:16 +0000

    [FT:#3622] Fix: add callback when saving page failed

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/mainProcess/menu/pageSave.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/mainProcess/menu/pageSave.js
@@ -26,59 +26,59 @@ function pageSave(focusedWindow) {
   const page = state.pages[pageId];
   const store = getStore();
   if (!page.oId && !page.absolutePath) {
-    updatePagePath(pageId, store, store.dispatch, (errUp, pId) => {
-      if (errUp) {
-        return getStore().dispatch(
-          addMessage(getWindowFocusedPageId(getStore().getState(), focusedWindow.windowId),
-            'danger',
-            'Saving Failed')
-          );
-      }
-      saveFile(pId, store);
+    getPathByFilePicker(state.hsc.folder, 'Page', 'save', (err, newPagePath) => {
+      getStore().dispatch(updateAbsolutePath(pageId, newPagePath));
+      return saveFile(pageId, store, (errSaving) => {
+        if (errSaving) {
+          return store.dispatch(addMessage(pageId, 'danger', errSaving));
+        }
+        return store.dispatch(addMessage(pageId, 'success', 'Page saved'));
+      });
     });
   } else {
-    saveFile(pageId, store);
+    return saveFile(pageId, getStore(), (errSaving) => {
+      if (errSaving) {
+        return store.dispatch(addMessage(pageId, 'danger', errSaving));
+      }
+      return getStore().dispatch(addMessage(pageId, 'success', 'Page saved'));
+    });
   }
 }
 
 function pageSaveAs(focusedWindow) {
-  const store = getStore();
-  const pageId = store.getState().windows[focusedWindow.windowId].focusedPage;
-  if (getPageModifiedViewsIds(store.getState(), pageId).length) {
+  const pageId = getStore().getState().windows[focusedWindow.windowId].focusedPage;
+  if (getPageModifiedViewsIds(getStore().getState(), pageId).length) {
     return getStore().dispatch(
       addMessage(getWindowFocusedPageId(getStore().getState(), focusedWindow.windowId,
         'danger',
         'Please, save the views of this page')
       ));
   }
-  updatePagePath(pageId, store, store.dispatch, (errUp, pId) => {
-    if (errUp) {
-      return store.dispatch(addMessage(pageId, 'danger', errUp));
-    }
-    saveFile(pId, store);
+  const state = getStore().getState();
+  const page = state.pages[pageId];
+  getPathByFilePicker(state.hsc.folder, 'Page', 'save', (err, newPagePath) => {
+    getStore().dispatch(updateAbsolutePath(pageId, newPagePath));
+    return saveFile(pageId, getStore(), (errSaving) => {
+      if (errSaving) {
+        getStore().dispatch(updateAbsolutePath(pageId, page.absolutePath));
+        getStore().dispatch(setModified(pageId, page.isModified));
+        getStore().dispatch(addMessage(pageId, 'danger', errSaving));
+        return;
+      }
+      return getStore().dispatch(addMessage(pageId, 'success', 'Page saved'));
+    });
   });
 }
 
-function saveFile(pageId, store) {
+function saveFile(pageId, store, callback) {
   savePage(store.getState(), pageId, false, (err, oid) => {
     if (err) {
-      return store.dispatch(addMessage(pageId, 'danger', err));
+      return callback(err);
     }
     if (oid) {
       store.dispatch(setPageOid(pageId, oid));
     }
-    store.dispatch(addMessage(pageId, 'success', 'View saved'));
     store.dispatch(setModified(pageId, false));
-  });
-}
-
-function updatePagePath(pageId, store, dispatch, callback) {
-  const state = store.getState();
-  getPathByFilePicker(state.hsc.folder, 'Page', 'save', (err, newPagePath) => {
-    if (err) {
-      return callback(err, null);
-    }
-    dispatch(updateAbsolutePath(pageId, newPagePath));
-    callback(null, pageId);
+    callback(null);
   });
 }

41f7d57aecc993df58b1b50ca3b50604996a9001
Guillaume ARM <guillaume.arm.external@atos.net>
2017-01-30 06:40:24 +0000

    [FT:#3622] Change profiling() middleware into createProfiling()
    profilingMiddleware can take a custom profiler (executionMonitor as default)

--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/mainStore.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/mainStore.js
@@ -4,12 +4,12 @@ import thunk from 'redux-thunk';
 
 import { get } from 'common/parameters';
 import reducer from './reducers';
-import profiling from './middlewares/profiling';
+import createProfilingMiddleware from './middlewares/profiling';
 
 let store;
 
 export function initStore(initialState) {
-  const middlewares = get('DEBUG') ? [profiling, thunk] : [thunk];
+  const middlewares = get('DEBUG') ? [createProfilingMiddleware(), thunk] : [thunk];
   const enhancer = compose(
     applyMiddleware(...middlewares),
     electronEnhancer()
--- GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/middlewares/profiling.js
+++ GPCCHS_E_VIS/src/client/src/impl/js/client/lib/store/middlewares/profiling.js
@@ -7,19 +7,21 @@ const hasProfiling = has('meta.profiling');
 const getProfiling = getOr({}, 'meta.profiling');
 
 // you can use withProfiling higher order action creator to use execution profiling on the store
-const profilingMiddleware = ({ getState }) => next => (action) => {
-  const { predicate = always(true), namespace, key, msg } = getProfiling(action);
-  if (!hasProfiling(action) || !predicate(getState(), action)) {
-    return next(action);
+const createProfilingMiddleware = (profiler = executionMonitor) => (
+  ({ getState }) => next => (action) => {
+    const { predicate = always(true), namespace, key, msg } = getProfiling(action);
+    if (!hasProfiling(action) || !predicate(getState(), action)) {
+      return next(action);
+    }
+    const execution = profiler(namespace);
+
+    execution.start(key); // start profiling
+    const returnedAction = next(action); // action pass in reducer
+    execution.stop(key, msg); // stop profiling
+    execution.print();
+
+    return returnedAction;
   }
-  const execution = executionMonitor(namespace);
+);
 
-  execution.start(key); // start profiling
-  const returnedAction = next(action); // action pass in reducer
-  execution.stop(key, msg); // stop profiling
-  execution.print();
-
-  return returnedAction;
-};
-
-export default profilingMiddleware;
+export default createProfilingMiddleware;

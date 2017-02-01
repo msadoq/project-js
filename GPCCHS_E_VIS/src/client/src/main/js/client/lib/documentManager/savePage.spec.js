731e0bd54a701f80635d642f11cd3f83de1f3ae4
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-01 13:06:13 +0000

    [FT:#3622] Improve savePage tests in documentManager

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/documentManager/savePage.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/documentManager/savePage.spec.js
@@ -1,9 +1,9 @@
-import { exec } from 'child_process';
+/* eslint-disable no-unused-expressions */
+import rimraf from 'rimraf';
 
-import { should, getTmpPath, freezeMe } from '../common/test';
+import { should, expect, getTmpPath, freezeMe } from '../common/test';
 import { applyDependencyToApi } from '../common/utils';
 
-import validation from './validation';
 import fmdApi from '../common/fmd';
 import fs from '../common/fs';
 import SavePage from './savePage';
@@ -11,104 +11,114 @@ import SavePage from './savePage';
 const { savePage, savePageAs } = applyDependencyToApi(SavePage, fmdApi);
 
 describe('mainProcess/documents/savePage', () => {
-  const state = {
-    pages: {
-      page1: {
-        editor: {},
-        layout: [
-          {
-            h: 1,
-            i: 'plot1',
-            w: 2,
-            x: 5,
-            y: 6
-          }, {
-            h: 1,
-            i: 'text1',
-            w: 2,
-            x: 5,
-            y: 6
-          },
-        ],
-        title: 'page1',
-        hideBorders: false,
-        timebarUuid: 'TB1',
-        views: ['plot1', 'text1'],
-        path: './pages/page1.json',
-        absolutePath: getTmpPath('testAs/pages/page1.json'),
-      }
-    },
-    views: {
-      plot1: {
-        type: 'PlotView',
-        configuration: {
-          type: 'PlotView',
-          entryPoints: [
+  let state;
+  beforeEach(() => {
+    state = {
+      pages: {
+        page1: {
+          editor: {},
+          layout: [
             {
-              name: 'ATT_BC_REVTCOUNT4',
-              connectedDataX: {},
-              connectedDataY: {},
+              h: 1,
+              i: 'plot1',
+              w: 2,
+              x: 5,
+              y: 6
+            }, {
+              h: 1,
+              i: 'text1',
+              w: 2,
+              x: 5,
+              y: 6
             },
           ],
-          axes: [],
-          grids: [],
-          title: 'Plotview 4 parameters',
-          titleStyle: {},
-        },
-        path: 'plotview4.json',
-        absolutePath: getTmpPath('testAs/views/plotview4.json')
+          title: 'page1',
+          hideBorders: false,
+          timebarUuid: 'TB1',
+          views: ['plot1', 'text1'],
+          path: './pages/page1.json',
+          absolutePath: getTmpPath('testAs/pages/page1.json'),
+        }
       },
-      text1: {
-        type: 'TextView',
-        configuration: {
-          title: 'TextView One parameter',
-          type: 'TextView',
-          entryPoints: [],
-          content: ''
+      views: {
+        plot1: {
+          type: 'PlotView',
+          configuration: {
+            type: 'PlotView',
+            entryPoints: [
+              {
+                name: 'ATT_BC_REVTCOUNT4',
+                connectedDataX: {},
+                connectedDataY: {},
+              },
+            ],
+            axes: [],
+            grids: [],
+            title: 'Plotview 4 parameters',
+            titleStyle: {},
+          },
+          path: 'plotview4.json',
+          absolutePath: getTmpPath('testAs/views/plotview4.json')
         },
-        path: 'textOne.json',
-        absolutePath: getTmpPath('testAs/views/textOne.json'),
+        text1: {
+          type: 'TextView',
+          configuration: {
+            title: 'TextView One parameter',
+            type: 'TextView',
+            entryPoints: [],
+            content: ''
+          },
+          path: 'textOne.json',
+          absolutePath: getTmpPath('testAs/views/textOne.json'),
+        }
+      },
+      workspace: {
+        folder: getTmpPath('testPage'),
+        file: './pages/page1.json',
       }
-    },
-    workspace: {
-      folder: getTmpPath('testPage'),
-      file: './pages/page1.json',
-    }
-  };
+    };
+  });
 
-  after((done) => {
-    exec('rm -r '.concat(getTmpPath()), () => {
-      // your callback goes here
-      done();
+  afterEach(done => (
+    rimraf(getTmpPath(), done)
+  ));
+  it('save ok', (done) => {
+    savePage(freezeMe(state), 'page1', false, (err) => {
+      should.not.exist(err);
+      fs.isExists(state.pages.page1.absolutePath, (exist) => {
+        exist.should.be.true;
+        done();
+      });
     });
   });
   it('saveAs ok', (done) => {
     savePageAs(freezeMe(state), 'page1', state.pages.page1.absolutePath, false, (err) => {
       should.not.exist(err);
-      done();
+      fs.isExists(state.pages.page1.absolutePath, (exist) => {
+        exist.should.be.true;
+        done();
+      });
     });
   });
-  it('check validity of saved as page', (done) => {
-    fs.readJsonFromAbsPath(state.pages.page1.absolutePath, (err, pageContent) => {
-      should.not.exist(err);
-      const validationError = validation('page', pageContent);
-      should.not.exist(validationError);
-      done();
-    });
-  });
-  it('save ok', (done) => {
-    state.pages.page1.isModified = true;
+
+  it('save should fail when page is invalid', (done) => {
+    state.pages.page1.title = undefined;
     savePage(freezeMe(state), 'page1', false, (err) => {
-      should.not.exist(err);
-      done();
+      expect(err).to.be.an('error');
+      fs.isExists(state.pages.page1.absolutePath, (exist) => {
+        exist.should.be.false;
+        done();
+      });
     });
   });
-  it('check validity of saved page', (done) => {
-    fs.readJsonFromAbsPath(state.pages.page1.absolutePath, (err, pageContent) => {
-      should.not.exist(err);
-      const validationError = validation('page', pageContent);
-      should.not.exist(validationError);
-      done();
+  it('saveAs should fail when page is invalid', (done) => {
+    state.pages.page1.title = undefined;
+    savePageAs(freezeMe(state), 'page1', state.pages.page1.absolutePath, false, (err) => {
+      expect(err).to.be.an('error');
+      fs.isExists(state.pages.page1.absolutePath, (exist) => {
+        exist.should.be.false;
+        done();
+      });
     });
   });
 });

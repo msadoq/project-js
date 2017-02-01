230abb2c9bde587b0c06431add243f878d05233e
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-01 09:08:00 +0000

    [FT:#3622] Refacto extractWindows tests in documentManager

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/documentManager/extractWindows.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/documentManager/extractWindows.spec.js
@@ -4,18 +4,37 @@ import { should } from '../common/test';
 
 describe('documents/lib', () => {
   describe('extractWindows ', () => {
+    it('ok', () => {
+      const content = { __original: { windows: [{
+        type: 'documentWindow',
+        val: 'val1'
+      }, {
+        type: 'sideWindow'
+      }, {
+        type: 'otherWindow'
+      }, {
+        type: 'documentWindow',
+        val: 'val2'
+      }] } };
+      extractWindows(content, (err, w) => {
+        should.not.exist(err);
+        w.should.have.keys('__original', 'windows');
+        Object.keys(w.windows).should.have.length(2);
+        _.each(w.windows, (v, k) => {
+          v.uuid.should.equal(k);
+        });
+      });
+    });
     it('not Array', () => {
-      const win = { __original: { windows: {
-        type: 'documentWindow'
-      } } };
-      extractWindows(win, (err, w) => {
+      const content = { __original: { windows: 'not an array' } };
+      extractWindows(content, (err, w) => {
         should.not.exist(err);
         w.should.have.keys('__original', 'windows');
         Object.keys(w.windows).should.have.length(0);
       });
     });
     it('no documentWindow', () => {
-      const win = { __original: { windows: [{
+      const content = { __original: { windows: [{
         type: 'sideWindow',
         val: 'val1'
       }, {
@@ -26,31 +45,10 @@ describe('documents/lib', () => {
         type: 'otherWindow',
         val: 'val2'
       }] } };
-      extractWindows(win, (err, w) => {
+      extractWindows(content, (err, w) => {
         should.not.exist(err);
         w.should.have.keys('__original', 'windows');
         Object.keys(w.windows).should.have.length(0);
-      });
-    });
-    it('ok', () => {
-      const win = { __original: { windows: [{
-        type: 'documentWindow',
-        val: 'val1'
-      }, {
-        type: 'sideWindow'
-      }, {
-        type: 'otherWindow'
-      }, {
-        type: 'documentWindow',
-        val: 'val2'
-      }] } };
-      extractWindows(win, (err, w) => {
-        should.not.exist(err);
-        w.should.have.keys('__original', 'windows');
-        Object.keys(w.windows).should.have.length(2);
-        _.each(w.windows, (v, k) => {
-          v.uuid.should.equal(k);
-        });
       });
     });
   });

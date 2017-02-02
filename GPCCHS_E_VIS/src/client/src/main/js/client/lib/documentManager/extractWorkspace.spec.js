a0e6e1f5e22832723e7afd220a6c7a6fd80d1ed6
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-02 09:06:00 +0000

    [FT:#3622] Add extractWorkspace tests in documentManager

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/documentManager/extractWorkspace.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/documentManager/extractWorkspace.spec.js
@@ -1,12 +1,56 @@
 // import path from 'path';
+import { compose, prop, split, mapValues, keys, each } from 'lodash/fp';
 
-import { } from '../common/test';
-// import { readWorkspace } from './extractWorkspace';
+import { should, expect } from '../common/test';
+import fmdApi from '../common/fmd';
+import ExtractWorkspace from './extractWorkspace';
+import { applyDependencyToApi } from '../common/utils';
+
+const getPathFromOid = compose(prop(1), split(':'));
+const mockFmdApi = fmd => ({
+  ...fmd,
+  resolveDocument: (oid, cb) => cb(null, getPathFromOid(oid)),
+});
+
+const { readWorkspace } = applyDependencyToApi(ExtractWorkspace, mockFmdApi(fmdApi));
 
 describe('documents/workspace', () => {
+  const rootDir = fmdApi.getRootDir();
   describe('readWorkspace', () => {
-    it('valid', (done) => {
-      done();
+    it('exists', () => {
+      readWorkspace.should.be.a('function');
+    });
+    it('valid workspace', (done) => {
+      const assertAllIsString = each(each(x => x.should.be.a('string')));
+      readWorkspace(rootDir, 'small.workspace.json', (err, res) => {
+        setImmediate(() => {
+          const workspace = mapValues(keys, res);
+          expect(err).not.be.an('error');
+          should.exist(workspace);
+          workspace.should.be.an('object').with.keys('timebars', 'timelines', 'windows', 'pages', 'views');
+          workspace.timebars.should.have.length(1);
+          workspace.timelines.should.have.length(1);
+          workspace.windows.should.have.length(1);
+          workspace.pages.should.have.length(2);
+          workspace.views.should.have.length(4);
+          assertAllIsString(workspace);
+          done();
+        });
+      });
+    });
+    it('invalid workspace', (done) => {
+      readWorkspace(rootDir, 'pages/page1.json', (err, res) => {
+        err.should.be.an('error');
+        should.not.exist(res);
+        done();
+      });
+    });
+    it('unknown workspace', (done) => {
+      readWorkspace(rootDir, 'unknown.workspace.json', (err, res) => {
+        err.should.be.an('error');
+        should.not.exist(res);
+        done();
+      });
     });
   });
 });

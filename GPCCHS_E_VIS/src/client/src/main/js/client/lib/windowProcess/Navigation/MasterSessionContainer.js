a46bd1ed6f62113be80f09b892997e57ce574117
Damien BRUGNE <damien.brugne.external@atos.net>
2017-02-03 10:39:16 +0000

    [FT:#3622] Add robustness code when LPISIS send invalid data (master session ID that doesn't correspond to any known session)

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Navigation/MasterSessionContainer.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Navigation/MasterSessionContainer.js
@@ -1,10 +1,13 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
+import _get from 'lodash/get';
 import { getMasterSession } from '../../store/selectors/masterSession';
 
 const MasterSession = ({ masterSession }) => (
-  <Button bsSize="sm">Master session: {masterSession.name}</Button>
+  <Button bsSize="sm">
+    Master session: {_get(masterSession, ['name'], 'unknown')}
+  </Button>
 );
 
 MasterSession.propTypes = {

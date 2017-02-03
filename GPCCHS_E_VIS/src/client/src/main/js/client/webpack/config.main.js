58a5a0b3a3dfff52e92133ab7dcc44c146b9e5f2
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-03 07:38:07 +0000

    [FT:#3622] Add webpack bundle analyzer  .
    - new dev-dependencies
    - new script 'stats:main' in package.json
    - new script 'stats:renderer' in package.json
    - add explanation in README.md

--- GPCCHS_E_VIS/src/client/src/main/js/client/webpack/config.main.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/webpack/config.main.js
@@ -1,7 +1,19 @@
+import noop from 'lodash/noop';
 import { join } from 'path';
 import webpack from 'webpack';
 import merge from 'webpack-merge';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import baseConfig from './config.base';
+
+const { BUNDLE_STATS } = process.env;
+const analyzer = new BundleAnalyzerPlugin({
+  analyzerPort: process.env.ANALYZER_PORT || 8888,
+  openAnalyzer: false,
+});
+
+if (BUNDLE_STATS) {
+  console.info('building and analyzing main bundle...'); // eslint-disable-line no-console
+}
 
 export default merge(baseConfig, {
   devtool: 'source-map',
@@ -24,6 +36,7 @@ export default merge(baseConfig, {
     //     warnings: false
     //   }
     // }),
+    BUNDLE_STATS ? analyzer : noop,
     new webpack.BannerPlugin(
       'require("source-map-support").install();',
       { raw: true, entryOnly: false }

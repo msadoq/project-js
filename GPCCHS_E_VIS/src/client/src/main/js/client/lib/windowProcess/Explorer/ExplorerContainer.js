6199ee8e98b18ad84e8e188f41a912b4d0e3f80d
Audrey LEAL <audrey.leal.external@atos.net>
2017-02-03 09:11:23 +0000

    [FT:#3622] Update of explorer container .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Explorer/ExplorerContainer.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Explorer/ExplorerContainer.js
@@ -1,6 +1,5 @@
 import { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import _get from 'lodash/get';
 import Explorer from './Explorer';
 import {
   getLastPubSubTimestamp,
@@ -8,25 +7,53 @@ import {
   getHssStatus
 } from '../../store/selectors/health';
 import dataMapGenerator from '../../dataManager/map';
+import { getExplorerTabName } from '../../store/selectors/windows';
+import { getViews } from '../../store/selectors/views';
+import { currentExplorer } from '../../store/actions/windows';
+import { main } from '../../windowProcess/ipc';
+import parseFormula from '../../dataManager/structures/common/formula';
 
 
-const mapStateToProps = (state) => {
-  const dcStatus = getDcStatus(state);
-  const hssStatus = getHssStatus(state);
-  const lastPubSubTime = getLastPubSubTimestamp(state);
-  const dataMap = dataMapGenerator(state);
-  const viewData = _get(state, ['viewData']);
-
-  return {
-    dcStatus,
-    hssStatus,
-    lastPubSubTime,
-    ...dataMap,
-    viewData,
-  };
+const mapStateToProps = (state, { windowId }) => {
+  const tabName = getExplorerTabName(state, windowId);
+  if (!tabName || tabName === 'perRemoteId') {
+    const dataMap = dataMapGenerator(state);
+    return {
+      perRemoteId: dataMap.perRemoteId,
+      currentTab: tabName || 'perRemoteId',
+      parseFormula,
+    };
+  } else if (tabName === 'perViewId') {
+    const dataMap = dataMapGenerator(state);
+    return {
+      perView: dataMap.perView,
+      currentTab: tabName,
+      parseFormula,
+      views: getViews(state),
+    };
+  } else if (tabName === 'health') {
+    const dcStatus = getDcStatus(state);
+    const hssStatus = getHssStatus(state);
+    const lastPubSubTime = getLastPubSubTimestamp(state);
+    return {
+      dcStatus,
+      hssStatus,
+      lastPubSubTime,
+      currentTab: tabName,
+    };
+  } else if (tabName === 'server') {
+    let server;
+    main.serverDebug((debug) => {
+      server = debug;
+    });
+    return {
+      server,
+      currentTab: tabName,
+    };
+  }
 };
 
-const ExplorerContainer = connect(mapStateToProps)(Explorer);
+const ExplorerContainer = connect(mapStateToProps, { currentExplorer })(Explorer);
 
 ExplorerContainer.propTypes = {
   windowId: PropTypes.string.isRequired,

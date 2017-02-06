b7204a9cc9d294f48c22cf0672d6dc668a7b7488
Damien BRUGNE <damien.brugne.external@atos.net>
2017-02-06 16:33:11 +0000

    [FT:#3622] Remove redux store profiling actions middleware

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/actions/enhancers/index.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/actions/enhancers/index.js
@@ -1,9 +1,7 @@
 import ifPathChanged from './ifPathChanged';
 import addUuids from './addUuids';
-import withProfiling from './withProfiling';
 
 export default {
   ifPathChanged,
   addUuids,
-  withProfiling,
 };
--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/mainStore.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/mainStore.js
@@ -1,17 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { electronEnhancer } from 'redux-electron-store';
 import thunk from 'redux-thunk';
-
-import { get } from 'common/parameters';
 import reducer from './reducers';
-import createProfilingMiddleware from './middlewares/profiling';
 
 let store;
 
 export function initStore(initialState) {
-  const middlewares = get('PROFILING') === 'on' ? [createProfilingMiddleware(), thunk] : [thunk];
   const enhancer = compose(
-    applyMiddleware(...middlewares),
+    applyMiddleware(thunk),
     electronEnhancer()
   );
 

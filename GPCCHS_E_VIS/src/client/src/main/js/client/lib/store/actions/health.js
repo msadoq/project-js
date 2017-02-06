5f441d82cc29e9f9c144ccf7fd59a80e5399b2c1
Damien BRUGNE <damien.brugne.external@atos.net>
2017-02-06 17:59:14 +0000

    [FT:#3622] Remove unused dispatch in updateHealth action creator

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/actions/health.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/actions/health.js
@@ -5,38 +5,28 @@ import * as types from '../types';
 import {
   getDcStatus,
   getHssStatus,
-  getMainStatus,
   getLastPubSubTimestamp,
 } from '../selectors/health';
 
-/**
- * Health status
- */
 export const updateDcStatus = simple(types.HSS_UPDATE_DC_STATUS, 'status');
 export const updateHssStatus = simple(types.HSS_UPDATE_HEALTH_STATUS, 'status');
 export const updateMainStatus = simple(types.HSS_UPDATE_MAIN_STATUS, 'status');
 export const updateWindowStatus = simple(types.HSS_UPDATE_WINDOW_STATUS, 'windowId', 'status');
-
-/**
- * Identify last received timestamp from pubsub data
- */
 export const updateLastPubSubTimestamp = simple(types.HSS_UPDATE_LAST_PUBSUB_TIMESTAMP, 'timestamp');
+
 const updateLastPubSubTimestampThrottled = _throttle(
   (dispatch, timestamp) => dispatch(updateLastPubSubTimestamp(timestamp)),
   HSC_PUBSUB_MONITORING_FREQUENCY
 );
 
 // TODO rperrot test
-export const updateHealth = ({ dcStatus, hssStatus, mainStatus, lastPubSubTimestamp }) =>
+export const updateHealth = ({ dcStatus, hssStatus, lastPubSubTimestamp }) =>
   (dispatch, getState) => {
     if (getDcStatus(getState()) !== dcStatus) {
       dispatch(updateDcStatus(dcStatus));
     }
     if (getHssStatus(getState()) !== hssStatus) {
       dispatch(updateHssStatus(hssStatus));
-    }
-    if (getMainStatus(getState()) !== mainStatus) {
-      dispatch(updateMainStatus(mainStatus));
     }
     if (getLastPubSubTimestamp(getState()) !== lastPubSubTimestamp) {
       updateLastPubSubTimestampThrottled(dispatch, lastPubSubTimestamp);

c0e8602d5cf833311da8ac59e5df083a5ad2d6ab
Alexis BESSON <alexis.besson@external.atos.net>
2017-02-06 09:42:07 +0000

    [FT:#3622] Lint windowProcess/Editor/Components/Navbar/Navbar.js . . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Editor/Components/Navbar/Navbar.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Editor/Components/Navbar/Navbar.js
@@ -4,10 +4,10 @@ import styles from './Navbar.css';
 
 export default class EditorNavbar extends React.Component {
   static propTypes = {
-    currentDisplay: PropTypes.number,
-    changeCurrentDisplay: PropTypes.func,
-    items: PropTypes.array,
-    closeEditor: PropTypes.func,
+    currentDisplay: PropTypes.number.isRequired,
+    changeCurrentDisplay: PropTypes.func.isRequired,
+    items: PropTypes.arrayOf(PropTypes.string).isRequired,
+    closeEditor: PropTypes.func.isRequired,
   }
 
   componentWillMount() {

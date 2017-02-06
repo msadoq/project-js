e4ca7a41f85e533b5a1dadc1ad098a0d6446ce5f
Alexis BESSON <alexis.besson@external.atos.net>
2017-02-06 13:55:12 +0000

    [FT:#3622] Lint debug error in SelectButtonField

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Editor/Components/Fields/SelectFontField.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Editor/Components/Fields/SelectFontField.js
@@ -2,20 +2,19 @@ import React, { PropTypes } from 'react';
 import { FormControl } from 'react-bootstrap';
 
 const SelectFontField = ({ input }) => {
-    const { value, onChange } = input;
-    return (
-      <FormControl
-        className="form-control input-sm"
-        componentClass="select"
-        onChange={onChange}
-        value={value}
-      >
-        <option value="Arial">Arial</option>
-        <option value="Helvetica">Helvetica</option>
-      </FormControl>
-    );
-  }
-}
+  const { value, onChange } = input;
+  return (
+    <FormControl
+      className="form-control input-sm"
+      componentClass="select"
+      onChange={onChange}
+      value={value}
+    >
+      <option value="Arial">Arial</option>
+      <option value="Helvetica">Helvetica</option>
+    </FormControl>
+  );
+};
 
 SelectFontField.propTypes = {
   input: PropTypes.shape({ value: PropTypes.string, onChange: PropTypes.func }).isRequired,

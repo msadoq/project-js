ad4648a5c1659971aeda0e51c2819183fbd2d117
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-07 14:00:22 +0000

    [FT:#5316] lint VIVL/main file in client

--- GPCCHS_E_VIS/src/client/src/main/js/client/VIVL/main.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/VIVL/main.js
@@ -1,15 +1,16 @@
+import _get from 'lodash/get';
+import _isFunction from 'lodash/isFunction';
+
 import TextView from './TextView/main';
 import PlotView from './PlotView/main';
 import DynamicView from './DynamicView/main';
-import _get from 'lodash/get';
-import _isFunction from 'lodash/isFunction';
 
 const supportedView = { PlotView, TextView, DynamicView };
 
 export default (viewType, functionName) => {
   const f = _get(supportedView, [viewType, functionName]);
   if (!f || !_isFunction(f)) {
-    throw new Error(`invalid function ${functionName} for view type ${viewType}`)
+    throw new Error(`invalid function ${functionName} for view type ${viewType}`);
   }
 
   return f;

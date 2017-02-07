7f5a4ce60be3bfae8ad2e08daa02b42054e36865
Alexis BESSON <alexis.besson@external.atos.net>
2017-02-07 12:49:22 +0000

    [FT:#5316] Lint DynamicEditorContainer.js DynamicEditorForm.js EntryPointActions.js  . . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Editor/Components/Dynamic/DynamicEditorForm.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Editor/Components/Dynamic/DynamicEditorForm.js
@@ -14,78 +14,78 @@ import {
   Composant react-select :
   https://github.com/JedWatson/react-select
 */
-class EntryPointConnectedData extends React.Component {
-  static propTypes = {
-    /* eslint-disable react/no-unused-prop-types */
-    initialValues: PropTypes.shape({
-      formula: PropTypes.string,
-      domain: PropTypes.string,
-      timeline: PropTypes.string,
-    }).isRequired,
-    timelines: PropTypes.array,
-    handleSubmit: PropTypes.func,
-    pristine: PropTypes.bool,
-    reset: PropTypes.func,
-    submitting: PropTypes.bool,
-    valid: PropTypes.bool,
-  }
+const EntryPointConnectedData = (props) => {
+  const {
+    handleSubmit,
+    pristine,
+    reset,
+    submitting,
+    valid,
+    timelines,
+  } = props;
+  // const { formula, domain, timeline } = initialValues;
 
-  render() {
-    const {
-      handleSubmit,
-      pristine,
-      reset,
-      submitting,
-      valid,
-      timelines,
-    } = this.props;
-    // const { formula, domain, timeline } = initialValues;
-
-    return (
-      <Form horizontal onSubmit={handleSubmit}>
-        <HorizontalFormGroup label="Formula">
-          <Field
-            name="formula"
-            component={TextareaField}
-            className="form-control input-sm"
-          />
-        </HorizontalFormGroup>
-        <HorizontalFormGroup label="Domain">
-          <Field
-            name="domain"
-            component={InputField}
-            type="text"
-            className="form-control input-sm"
-          />
-        </HorizontalFormGroup>
-
-        <HorizontalFormGroup label="Timeline">
-          <Field
-            name="timeline"
-            clearable={false}
-            component={ReactSelectField}
-            free
-            options={timelines.map(t =>
-              ({
-                label: t.id,
-                value: t.id,
-              })
-            ).concat({
-              label: '*',
-              value: '*',
-            })}
-          />
-        </HorizontalFormGroup>
-        <ClearSubmitButtons
-          pristine={pristine}
-          submitting={submitting}
-          reset={reset}
-          valid={valid}
+  return (
+    <Form horizontal onSubmit={handleSubmit}>
+      <HorizontalFormGroup label="Formula">
+        <Field
+          name="formula"
+          component={TextareaField}
+          className="form-control input-sm"
         />
-      </Form>
-    );
-  }
-}
+      </HorizontalFormGroup>
+      <HorizontalFormGroup label="Domain">
+        <Field
+          name="domain"
+          component={InputField}
+          type="text"
+          className="form-control input-sm"
+        />
+      </HorizontalFormGroup>
+
+      <HorizontalFormGroup label="Timeline">
+        <Field
+          name="timeline"
+          clearable={false}
+          component={ReactSelectField}
+          free
+          options={timelines.map(t =>
+            ({
+              label: t.id,
+              value: t.id,
+            })
+          ).concat({
+            label: '*',
+            value: '*',
+          })}
+        />
+      </HorizontalFormGroup>
+      <ClearSubmitButtons
+        pristine={pristine}
+        submitting={submitting}
+        reset={reset}
+        valid={valid}
+      />
+    </Form>
+  );
+};
+
+EntryPointConnectedData.propTypes = {
+  /* eslint-disable react/no-unused-prop-types */
+  initialValues: PropTypes.shape({
+    formula: PropTypes.string,
+    domain: PropTypes.string,
+    timeline: PropTypes.string,
+  }).isRequired,
+  timelines: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+  })).isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  pristine: PropTypes.bool.isRequired,
+  reset: PropTypes.func.isRequired,
+  submitting: PropTypes.bool.isRequired,
+  valid: PropTypes.bool.isRequired,
+};
 
 const requiredFields = ['formula', 'domain', 'timeline'];
 const validate = (values = {}) => {

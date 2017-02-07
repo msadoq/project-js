104f29d2cab350515d2ed98439754e51aacbe434
Raphael FABRE <raphael.fabre@external.atos.net>
2017-02-07 15:30:49 +0000

    [FT:#5317] Simulating resize event when view type PlotView mounted.

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/View/View.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/View/View.js
@@ -76,6 +76,12 @@ export default class View extends PureComponent {
 
   componentDidMount() {
     document.addEventListener('keydown', this.toggleCollapse);
+    if (this.props.type === 'PlotView') {
+      setTimeout(() => {
+        // used to avoid 0 height when first mouting plotView
+        window.dispatchEvent(new Event('resize'));
+      }, 40);
+    }
   }
 
   componentWillUnmount() {

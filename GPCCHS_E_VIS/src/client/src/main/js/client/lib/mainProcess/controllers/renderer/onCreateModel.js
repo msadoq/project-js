9bfa81f678bb8bc0a6c6e14108544fc10e326ff3
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-08 13:53:21 +0000

    [FT:#5316] Remove some eslint-disable in onCreateModel controller
    in mainProcess controllers.
    + refacto

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/mainProcess/controllers/renderer/onCreateModel.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/mainProcess/controllers/renderer/onCreateModel.js
@@ -1,4 +1,8 @@
-import _cloneDeep from 'lodash/cloneDeep';
+import identity from 'lodash/fp/identity';
+import compose from 'lodash/fp/compose';
+import update from 'lodash/fp/update';
+import map from 'lodash/fp/map';
+import constant from 'lodash/fp/constant';
 import { dirname } from 'path';
 import { getStore } from '../../../store/mainStore';
 import { getView } from '../../../store/selectors/views';
@@ -8,29 +12,28 @@ import { saveViewAs } from '../../../common/documentManager';
 import vivl from '../../../../VIVL/main';
 import { getRootDir } from '../../../common/fmd';
 
-
 const root = getRootDir();
+
+const clearFormula = (structureType) => {
+  const clear = key => update(key, constant(''));
+  if (structureType === 'last') {
+    return clear('connectedData.formula');
+  } else if (structureType === 'range') {
+    return compose(clear('connectedDataX.formula'), clear('connectedDataY.formula'));
+  }
+  return identity;
+};
 
 export default function ({ viewId }) {
   const { getState, dispatch } = getStore();
   const { type, configuration, absolutePath } = getView(getState(), viewId);
 
+  const structureType = vivl(type, 'structureType')();
+  const clearEntrypointsFormulas = update('entryPoints', map(clearFormula(structureType)));
+  const modelConfig = clearEntrypointsFormulas(configuration);
+
   const folder = absolutePath ? dirname(absolutePath) : root;
   return getPathByFilePicker(folder, 'model', 'save', (err, path) => {
-    // Create a twin view without formula in entry Points
-    const modelConfig = _cloneDeep(configuration);
-    const structureType = vivl(type, 'structureType')();
-    if (structureType === 'last') {
-      modelConfig.entryPoints.forEach((ep) => {
-        ep.connectedData.formula = ''; // eslint-disable-line no-param-reassign
-      });
-    } else if (structureType === 'range') {
-      modelConfig.entryPoints.forEach((ep) => {
-        ep.connectedDataX.formula = ''; // eslint-disable-line no-param-reassign
-        ep.connectedDataY.formula = ''; // eslint-disable-line no-param-reassign
-      });
-    }
-
     saveViewAs(modelConfig, type, path, (errSaving) => {
       if (errSaving) {
         dispatch(add(viewId, 'danger', `Model unsaved ${errSaving}`));

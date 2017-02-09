fcdecc754540ae5cb8b60ace10544b0145ef4490
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-09 14:57:26 +0000

    [FT:#3622] Write addAxis tests in reducers/views/axis

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/views/axis.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/views/axis.spec.js
@@ -1,22 +1,23 @@
 /* eslint no-unused-expressions: 0 */
 import { freezeMe } from '../../../common/test';
-import { updateAxis } from './axis';
+import { updateAxis, addAxis } from './axis';
 
 describe('store:views:axes', () => {
-  describe('updateAxis', () => {
-    const state = freezeMe({
-      view1: {
-        type: 'PlotView',
-        configuration: {
-          axes: {
-            axis1: null,
-          },
+  const state = freezeMe({
+    view1: {
+      type: 'PlotView',
+      configuration: {
+        axes: {
+          axis_1: { label: 'AXIS1', id: 'axis_1' },
+          axis_1_1: {},
         },
       },
-      view2: {
-        type: 'Unknown',
-      },
-    });
+    },
+    view2: {
+      type: 'Unknown',
+    },
+  });
+  describe('updateAxis', () => {
     it('does nothing when received an unknown viewId', () => {
       const newState = updateAxis(state, freezeMe({ payload: { viewId: 'unknown' } }));
       newState.should.be.eql(state);
@@ -30,15 +31,44 @@ describe('store:views:axes', () => {
       newState.should.be.eql(state);
     });
     it('does nothing when view is not a PlotView', () => {
-      const newState = updateAxis(state, freezeMe({ payload: { viewId: 'view2', axisId: 'axis1', axis: true } }));
+      const newState = updateAxis(state, freezeMe({ payload: { viewId: 'view2', axisId: 'axis_1', axis: true } }));
       newState.should.be.eql(state);
     });
     it('updates axis', () => {
-      const newState = updateAxis(state, freezeMe({ payload: { viewId: 'view1', axisId: 'axis1', axis: true } }));
+      const newState = updateAxis(state, freezeMe({ payload: { viewId: 'view1', axisId: 'axis_1', axis: true } }));
       newState.should.not.be.eql(state);
       newState.view2.should.be.eql(state.view2);
       newState.view1.isModified.should.be.true;
-      newState.view1.configuration.axes.axis1.should.be.true;
+      newState.view1.configuration.axes.axis_1.should.be.true;
+    });
+  });
+
+  describe('addAxis', () => {
+    it('does nothing when received an unknown viewId', () => {
+      const newState = addAxis(state, { payload: { viewId: 'unknown' } });
+      newState.should.be.eql(state);
+    });
+    it('does nothing when received an unknown axis', () => {
+      const newState = addAxis(state, { payload: { viewId: 'view1' } });
+      newState.should.be.eql(state);
+    });
+    it('does nothing when received an axis without label', () => {
+      const newState = addAxis(state, { payload: { viewId: 'view1', axis: {} } });
+      newState.should.be.eql(state);
+    });
+    it('adds axis with given id', () => {
+      const newState = addAxis(state, { payload: { viewId: 'view1', axis: { label: 'yolo', id: 'axis42' } } });
+      newState.should.not.be.eql(state);
+      newState.view1.isModified.should.be.true;
+      newState.view1.configuration.axes.axis42.should.eql({ label: 'yolo', id: 'axis42' });
+      newState.view1.configuration.axes.axis_1.should.eql(state.view1.configuration.axes.axis_1);
+    });
+    it('adds axis generating new id from label', () => {
+      const newState = addAxis(state, { payload: { viewId: 'view1', axis: { label: 'axis_1' } } });
+      newState.should.not.be.eql(state);
+      newState.view1.isModified.should.be.true;
+      newState.view1.configuration.axes.axis_1_2.should.eql({ label: 'axis_1', id: 'axis_1_2' });
+      newState.view1.configuration.axes.axis_1.should.eql(state.view1.configuration.axes.axis_1);
     });
   });
 });

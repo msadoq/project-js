a7153217239b7c6ca3876c218a787c2e1e0cad0d
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-15 06:36:11 +0000

    [FT:#3622] Add documentation about composeReducers (store)

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/composeReducers.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/composeReducers.js
@@ -2,9 +2,12 @@ import __ from 'lodash/fp';
 
 const injectAction = action => reducer => state => reducer(state, action);
 
-const composeReducers = (...reducers) => (state, action) => {
+/* ---------------------------- composeReducers ----------------------------- */
+// Purpose :           take some reducers and return a single reducers.
+// example :           composeReducers(r1, r2, r3)
+// is equivalent to :  (state, action) => r1(r2(r3(state, action), action), action)
+
+export default (...reducers) => (state, action) => {
   const reducersWithAction = __.map(injectAction(action), reducers);
   return __.compose(...reducersWithAction)(state, action);
 };
-
-export default composeReducers;

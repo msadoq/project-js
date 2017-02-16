006c3e676adc2336d8d2c7f9704e5c0958cc02af
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-16 08:43:18 +0000

    [FT:#3622] Add freezeReducer test utils + edit freezeMe to not clone object

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/common/test.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/common/test.js
@@ -5,7 +5,6 @@ import properties from 'chai-properties';
 import sinon from 'sinon';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import _cloneDeep from 'lodash/cloneDeep';
 import deepFreeze from 'deep-freeze';
 import reducer from '../store/reducers/index';
 
@@ -26,11 +25,14 @@ function getStore(initialState) {
   );
 }
 
+const freezeMe = o => o && deepFreeze(o);
+
 module.exports = {
   should: chai.should(),
   expect: chai.expect,
   sinon,
   getStore,
-  freezeMe: o => deepFreeze(_cloneDeep(o)),
+  freezeMe,
+  freezeReducer: r => (state, action) => r(freezeMe(state), freezeMe(action)),
   getTmpPath: (...args) => path.resolve(tmpdir(), 'vima-tests', ...args),
 };

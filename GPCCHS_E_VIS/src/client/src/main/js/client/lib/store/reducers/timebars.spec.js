a8588e174fbfc17c7e2259337ad2f1e9b2b0b945
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-16 08:45:59 +0000

    [FT:#3622] Timebars tests are now suitable

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/timebars.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/timebars.spec.js
@@ -1,18 +1,20 @@
 /* eslint no-unused-expressions: 0 */
+import __ from 'lodash/fp';
 import _omit from 'lodash/omit';
-import _each from 'lodash/each';
-import { should, getStore } from '../../common/test';
+import { freezeReducer } from '../../common/test';
 import * as actions from '../actions/timebars';
-import reducer from './timebars';
+import timebarsReducer from './timebars';
 import * as types from '../types';
+
+const reducer = freezeReducer(timebarsReducer);
 
 describe('store:timebars:reducer', () => {
   it('initial state', () => {
     reducer(undefined, {}).should.be.an('object').that.is.empty;
   });
   it('unknown action', () => {
-    reducer({ myTimebarId: { id: 'Id' } }, {})
-      .should.eql({ myTimebarId: { id: 'Id' } });
+    reducer({ tb1: { id: 'tb1' } }, { payload: { timebarUuid: 'tb1' } })
+      .should.eql({ tb1: { id: 'tb1' } });
   });
   describe('add', () => {
     it('add', () => {
@@ -76,342 +78,165 @@ describe('store:timebars:reducer', () => {
     });
   });
   describe('update', () => {
-    let state;
-    let dispatch;
-    let getState;
-    before(() => {
-      const store = getStore({
-        timebars: {
-          myTimebarId: {
-            id: 'Id',
-            visuWindow: {
-              lower: 200,
-              upper: 400,
-              current: 300,
-              defaultWidth: 600,
-            },
-            slideWindow: { lower: 250, upper: 350 },
-            rulerResolution: 100,
-            speed: 10,
-            realTime: false,
-            mode: 'Normal',
-            masterId: 'OtherId',
-            timelines: ['myTimelineId', 'myTimelineId3'],
-          },
+    const tbState = {
+      tb1: {
+        id: 'tb1',
+        visuWindow: {
+          lower: 200,
+          upper: 400,
+          current: 300,
+          defaultWidth: 200,
         },
-      });
-      getState = store.getState;
-      state = getState();
-      dispatch = store.dispatch;
-    });
+        slideWindow: { lower: 250, upper: 350 },
+        rulerStart: 0,
+        rulerResolution: 100,
+        speed: 10,
+        realTime: false,
+        mode: 'Normal',
+        masterId: 'OtherId',
+        timelines: ['myTimelineId', 'myTimelineId3'],
+      },
+    };
     it('id', () => {
-      const newState = reducer(state.timebars, actions.updateId('myTimebarId', 'newId'));
-      newState.should.have.property('myTimebarId');
-      newState.myTimebarId.should.have.property('id');
-      newState.myTimebarId.id.should.equal('newId');
+      const newState = reducer(tbState, actions.updateId('tb1', 'newId'));
+      newState.should.have.property('tb1');
+      newState.tb1.should.have.property('id');
+      newState.tb1.id.should.equal('newId');
+    });
+    it('slideWindow', () => {
+      const action = {
+        type: types.WS_TIMEBAR_UPDATE_CURSORS,
+        payload: {
+          slideWindow: { lower: 250, upper: 42 },
+          timebarUuid: 'tb1',
+        },
+      };
+      const newState = reducer(tbState, action);
+
+      // visuWindow
+      newState.tb1.visuWindow.should.be.eql(tbState.tb1.visuWindow);
+
+      // slideWindow
+      newState.tb1.slideWindow.lower.should.be.eql(250);
+      newState.tb1.slideWindow.upper.should.be.eql(42);
     });
     it('visuWindow', () => {
-      dispatch(actions.updateCursors(
-        'myTimebarId',
-        {
-          lower: 198,
-          upper: 403,
-          current: 306,
+      const action = {
+        type: types.WS_TIMEBAR_UPDATE_CURSORS,
+        payload: {
+          visuWindow: { lower: 1, upper: 2, current: 3 },
+          timebarUuid: 'tb1',
         },
-        null
-      ));
-      const timebars = getState().timebars;
-      //  5, 40, 30));
-      timebars.should.have.property('myTimebarId');
-      timebars.myTimebarId.should.have.property('visuWindow');
-      timebars.myTimebarId.visuWindow.should.have.property('lower');
-      timebars.myTimebarId.visuWindow.lower.should.equal(198);
-      timebars.myTimebarId.visuWindow.should.have.property('upper');
-      timebars.myTimebarId.visuWindow.upper.should.equal(403);
-      timebars.myTimebarId.visuWindow.should.have.property('current');
-      timebars.myTimebarId.visuWindow.current.should.equal(306);
+      };
+      const newState = reducer(tbState, action);
+
+
+      // visuWindow
+      newState.tb1.visuWindow.lower.should.be.eql(1);
+      newState.tb1.visuWindow.upper.should.be.eql(2);
+      newState.tb1.visuWindow.current.should.be.eql(3);
+
+      // slideWindow
+      newState.tb1.slideWindow.should.be.eql(tbState.tb1.slideWindow);
     });
-    it('visuWindow should fail', () => {
-      dispatch(actions.updateCursors('myTimebarId', { lower: 290 }, null));
-      const timebars = getState().timebars;
-      timebars.should.have.property('myTimebarId');
-      timebars.myTimebarId.should.have.property('visuWindow');
-      timebars.myTimebarId.visuWindow.should.have.property('lower');
-      timebars.myTimebarId.visuWindow.lower.should.equal(198);
-      getState().messages['timeSetter-myTimebarId'].length.should.equal(1);
-    });
-    it('slideWindow should fail', () => {
-      dispatch(actions.updateCursors('myTimebarId', null, { upper: 450 }));
-      const timebars = getState().timebars;
-      timebars.should.have.property('myTimebarId');
-      timebars.myTimebarId.should.have.property('slideWindow');
-      timebars.myTimebarId.slideWindow.should.have.property('upper');
-      timebars.myTimebarId.slideWindow.upper.should.equal(350);
-      getState().messages['timeSetter-myTimebarId'].length.should.equal(2);
+    it('empty visuWindow/slideWindow', () => {
+      const action = {
+        type: types.WS_TIMEBAR_UPDATE_CURSORS,
+        payload: {
+          visuWindow: { },
+          slideWindow: { },
+          timebarUuid: 'tb1',
+        },
+      };
+      reducer(tbState, action).should.deep.eql(tbState);
     });
     it('should be immutable with same data', () => {
-      dispatch(actions.updateCursors(
-        'myTimebarId',
-        {
-          lower: 5,
-          upper: 40,
-          current: 30,
-        }
-        )
-      );
-      const oldState = getState().timebars;
-      dispatch(actions.updateCursors(
-        'myTimebarId',
-        {
-          lower: 5,
-          upper: 40,
-          current: 30,
-        }
-        )
-      );
-      const newState = getState().timebars;
-      newState.should.equal(oldState);
+      const action = {
+        type: types.WS_TIMEBAR_UPDATE_CURSORS,
+        payload: {
+          timebarUuid: 'tb1',
+          visuWindow: { lower: 5, upper: 40, current: 30 },
+        },
+      };
+      const newState = reducer(tbState, action);
+      reducer(newState, action).should.equal(newState);
     });
     it('speed', () => {
-      dispatch(actions.updateSpeed('myTimebarId', 20));
-      const newState = getState().timebars;
-      newState.should.have.property('myTimebarId');
-      newState.myTimebarId.speed.should.equal(20);
+      const action = {
+        type: types.WS_TIMEBAR_SPEED_UPDATE,
+        payload: {
+          timebarUuid: 'tb1',
+          speed: 20,
+        },
+      };
+      const newState = reducer(tbState, action);
+      newState.tb1.speed.should.be.eql(20);
     });
     it('defaultWidth', () => {
-      const newState = reducer(state.timebars, actions.updateDefaultWidth('myTimebarId', 10005));
-      newState.should.have.property('myTimebarId');
-      newState.myTimebarId.should.have.property('visuWindow');
-      newState.myTimebarId.visuWindow.should.have.property('defaultWidth');
-      newState.myTimebarId.visuWindow.defaultWidth.should.equal(10005);
+      const newState = reducer(tbState, actions.updateDefaultWidth('tb1', 10005));
+      newState.should.have.property('tb1');
+      newState.tb1.should.have.property('visuWindow');
+      newState.tb1.visuWindow.should.have.property('defaultWidth');
+      newState.tb1.visuWindow.defaultWidth.should.equal(10005);
     });
     it('masterId', () => {
-      const newState = reducer(state.timebars, actions.updateMasterId('myTimebarId', 'myTlId'));
-      newState.should.have.property('myTimebarId');
-      newState.myTimebarId.should.have.property('masterId');
-      newState.myTimebarId.masterId.should.equal('myTlId');
+      const newState = reducer(tbState, actions.updateMasterId('tb1', 'myTlId'));
+      newState.should.have.property('tb1');
+      newState.tb1.should.have.property('masterId');
+      newState.tb1.masterId.should.equal('myTlId');
     });
     it('realTime', () => {
-      const newState = reducer(state.timebars, actions.setRealTime('myTimebarId', true));
-      newState.should.have.property('myTimebarId');
-      newState.myTimebarId.should.have.property('realTime');
-      newState.myTimebarId.realTime.should.equal(true);
-    });
-    it('jump', () => {
-      const visuWindow = getState().timebars.myTimebarId.visuWindow;
-      dispatch(actions.jump('myTimebarId', 2));
-      const timebars = getState().timebars;
-      timebars.should.have.property('myTimebarId');
-      timebars.myTimebarId.visuWindow.lower.should.equal(visuWindow.lower + 2);
-      timebars.myTimebarId.visuWindow.upper.should.equal(visuWindow.upper + 2);
-      timebars.myTimebarId.visuWindow.current.should.equal(visuWindow.current + 2);
-    });
-    it('restore Width', () => {
-      dispatch(actions.restoreWidth('myTimebarId'));
-      const timebars = getState().timebars;
-      timebars.should.have.property('myTimebarId');
-      (timebars.myTimebarId.visuWindow.upper - timebars.myTimebarId.visuWindow.lower).should.equal(
-        600
-      );
+      const newState = reducer(tbState, actions.setRealTime('tb1', true));
+      newState.should.have.property('tb1');
+      newState.tb1.should.have.property('realTime');
+      newState.tb1.realTime.should.equal(true);
     });
     it('mount timeline', () => {
-      const newState = reducer(state.timebars, actions.mountTimeline('myTimebarId', 'myTimelineId2'));
-      newState.should.have.property('myTimebarId');
-      newState.myTimebarId.should.have.property('timelines');
-      newState.myTimebarId.timelines.should.have.length(3);
-      newState.myTimebarId.timelines.should.deep.equal(['myTimelineId', 'myTimelineId3', 'myTimelineId2']);
+      const newState = reducer(tbState, actions.mountTimeline('tb1', 'myTimelineId2'));
+      newState.should.have.property('tb1');
+      newState.tb1.should.have.property('timelines');
+      newState.tb1.timelines.should.have.length(3);
+      newState.tb1.timelines.should.deep.equal(['myTimelineId', 'myTimelineId3', 'myTimelineId2']);
     });
     it('unmount timeline', () => {
-      const newState = reducer(state.timebars, actions.unmountTimeline('myTimebarId', 'myTimelineId'));
-      newState.should.have.property('myTimebarId');
-      newState.myTimebarId.should.have.property('timelines');
-      newState.myTimebarId.timelines.should.have.length(1);
-      newState.myTimebarId.timelines.should.deep.equal(['myTimelineId3']);
+      const newState = reducer(tbState, actions.unmountTimeline('tb1', 'myTimelineId'));
+      newState.should.have.property('tb1');
+      newState.tb1.should.have.property('timelines');
+      newState.tb1.timelines.should.have.length(1);
+      newState.tb1.timelines.should.deep.equal(['myTimelineId3']);
+    });
+    it('updates rulerStart and rulerResolution', () => {
+      const removeRulerProperties = __.update('tb1', __.omit(['rulerStart', 'rulerResolution']));
+      const newState = reducer(tbState, actions.updateViewport('tb1', 42, 42));
+      newState.tb1.rulerStart.should.be.eql(42);
+      newState.tb1.rulerResolution.should.be.eql(42);
+      removeRulerProperties(tbState).should.be.eql(removeRulerProperties(newState));
     });
   });
   describe('switch modes', () => {
-    let state;
-    let dispatch;
-    let getState;
-    before(() => {
-      const store = getStore({
-        timebars: {
-          myTimebarId: {
-            id: 'Id',
-            visuWindow: {
-              lower: 200,
-              upper: 400,
-              current: 300,
-              defauttWidth: 600,
-            },
-            slideWindow: { lower: 250, upper: 350 },
-          },
+    const state = {
+      tb1: {
+        id: 'tb1',
+        visuWindow: {
+          lower: 200,
+          upper: 400,
+          current: 300,
+          defauttWidth: 600,
         },
-      });
-      getState = store.getState;
-      state = getState();
-      dispatch = store.dispatch;
-    });
-    it('mode to Extensible', () => {
-      state.timebars.myTimebarId.slideWindow.upper.should.be.below(
-        state.timebars.myTimebarId.visuWindow.upper
-      );
-      dispatch(actions.switchToExtensibleMode('myTimebarId'));
-      const timebars = getState().timebars;
-      timebars.should.have.property('myTimebarId');
-      timebars.myTimebarId.mode.should.equal('Extensible');
-      timebars.myTimebarId.slideWindow.upper.should.be.above(
-        timebars.myTimebarId.visuWindow.upper
-      );
-    });
-    it('mode to Fixed', () => {
-      dispatch(actions.switchToFixedMode('myTimebarId'));
-      const timebars = getState().timebars;
-      timebars.should.have.property('myTimebarId');
-      timebars.myTimebarId.mode.should.equal('Fixed');
-      timebars.myTimebarId.slideWindow.upper.should.be.below(
-        timebars.myTimebarId.visuWindow.upper
-      );
-    });
-    it('mode to Normal', () => {
-      let timebars = getState().timebars;
-      const slideUpper = timebars.myTimebarId.slideWindow.upper;
-      dispatch(actions.switchToNormalMode('myTimebarId'));
-      timebars = getState().timebars;
-      timebars.should.have.property('myTimebarId');
-      timebars.myTimebarId.mode.should.equal('Normal');
-      timebars.myTimebarId.slideWindow.upper.should.equal(slideUpper);
+        slideWindow: { lower: 250, upper: 350 },
+      },
+    };
+    it('switch mode', () => {
+      const action = {
+        type: types.WS_TIMEBAR_MODE_UPDATE,
+        payload: { timebarUuid: 'tb1', mode: 'yolo' },
+      };
+      const newState = reducer(state, action);
+      newState.tb1.mode.should.be.eql('yolo');
     });
     it('close_workspace', () => {
-      const newState = reducer(state.timebars, { type: types.HSC_CLOSE_WORKSPACE });
+      const newState = reducer(state, { type: types.HSC_CLOSE_WORKSPACE });
       newState.should.be.an('object').that.is.empty;
-    });
-  });
-  describe('Compound actions', () => {
-    it('add and mount timeline', () => {
-      const { dispatch, getState } = getStore({
-        timebars: {
-          myTimebarId: {
-            id: 'Id',
-            visuWindow: { lower: 10, upper: 20, current: 15 },
-            slideWindow: { lower: 20, upper: 30 },
-            rulerResolution: 100,
-            speed: 10,
-            masterId: 'OtherId',
-            timelines: ['myTimelineId', 'myTimelineId3'],
-          } },
-        timelines: {
-          myTimelineId: { id: 'tl1' },
-          myTimelineId2: { id: 'tl2' },
-          myTimelineId3: { id: 'tl3' },
-        } });
-
-      const fixture = {
-        id: 'Id',
-        offset: 10,
-        kind: 'DataSet',
-        sessionId: 100,
-      };
-      dispatch(actions.addAndMountTimeline('myTimebarId', fixture));
-      getState().timebars.myTimebarId.timelines.should.be.an('array').with.length(3);
-      _each(getState().timebars.myTimebarId.timelines, (tlId) => {
-        should.exist(getState().timelines[tlId]);
-      });
-    });
-    it('remove and unmount timeline', () => {
-      const { dispatch, getState } = getStore({
-        timebars: {
-          myTimebarId: {
-            id: 'Id',
-            visuWindow: { lower: 10, upper: 20, current: 15 },
-            slideWindow: { lower: 20, upper: 30 },
-            rulerResolution: 100,
-            speed: 10,
-            masterId: 'OtherId',
-            timelines: ['myTimelineId', 'myTimelineId3'],
-          } },
-        timelines: {
-          myTimelineId: { id: 'tl1' },
-          myTimelineId2: { id: 'tl2' },
-          myTimelineId3: { id: 'tl3' },
-        } });
-      dispatch(actions.unmountAndRemoveTimeline('myTimebarId', 'myTimelineId'));
-      getState().timebars.myTimebarId.timelines.should.be.an('array').with.length(1);
-      getState().timelines.should.not.keys('myTimelineId');
-    });
-  });
-  describe('handlePlay action', () => {
-    let store;
-    let dispatch;
-    let getState;
-    const initStore = (playingTimebarId) => {
-      store = getStore({
-        hsc: {
-          playingTimebarId,
-        },
-        timebars: {
-          myTimebarId: {
-            id: 'Id',
-            mode: 'Normal',
-            visuWindow: {
-              lower: 1420106400000,
-              upper: 1420106700000,
-              current: 1420106500000,
-            },
-            slideWindow: {
-              lower: 1420106430000,
-              upper: 1420106700500, // visuWindow.upper + 500
-            },
-            speed: 1,
-          },
-        },
-      });
-      getState = store.getState;
-      dispatch = store.dispatch;
-    };
-
-    it('(Normal mode) - should move 377ms', () => {
-      initStore('myTimebarId');
-      const state = getState();
-      const offset = 377;
-      const vw = state.timebars.myTimebarId.visuWindow;
-      const newCurrent = vw.current + offset;
-      dispatch(actions.handlePlay(newCurrent - vw.current, 0));
-
-      const newState = getState();
-      newState.timebars.myTimebarId.visuWindow.should.eql({
-        lower: vw.lower,
-        upper: vw.upper,
-        current: newCurrent,
-      });
-      newState.timebars.myTimebarId.slideWindow.should.eql({
-        lower: Math.trunc((vw.lower + newCurrent) / 2),
-        upper: Math.trunc((vw.current + vw.upper + offset) / 2),
-      });
-    });
-    it('(Normal mode) - should fail moving 377ms because timebar not found', () => {
-      initStore('myTimebarId02');
-      const state = getState();
-      const offset = 377;
-      const vw = state.timebars.myTimebarId.visuWindow;
-      const sw = state.timebars.myTimebarId.slideWindow;
-      const newCurrent = vw.current + offset;
-      dispatch(actions.handlePlay(
-        vw.current,
-        newCurrent,
-        0
-      ));
-
-      const newState = getState();
-      newState.timebars.myTimebarId.visuWindow.should.eql({
-        lower: vw.lower,
-        upper: vw.upper,
-        current: vw.current,
-      });
-      newState.timebars.myTimebarId.slideWindow.should.eql({
-        lower: sw.lower,
-        upper: sw.upper,
-      });
     });
   });
 });

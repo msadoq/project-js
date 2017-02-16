764ddb1a91429a2cc973a97b8713f420bdca52e0
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-16 14:56:55 +0000

    [FT:#3622] Rename some points in ifPathChanged action creator enhancer

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/actions/enhancers/ifPathChanged.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/actions/enhancers/ifPathChanged.js
@@ -6,13 +6,13 @@ import { compose, pathOr } from 'lodash/fp';
 const ifPathChanged = (actionCreator, key = 'views', keyPath = 'path', id = 'viewId') => (
   (...args) => (dispatch, getState) => {
     const action = actionCreator(...args);
-    const getView = compose(pathOr({}, [key, action.payload[id]]), getState);
+    const getStateElement = compose(pathOr({}, [key, action.payload[id]]), getState);
 
     const newPath = action.payload.newPath;
-    const oldPath = getView()[keyPath];
-    if (!getView() || !newPath) {
+    if (!getStateElement() || !newPath) {
       return undefined;
     }
+    const oldPath = getStateElement()[keyPath];
     if ((newPath && oldPath && resolve(newPath) !== resolve(oldPath))) {
       return dispatch(action);
     }

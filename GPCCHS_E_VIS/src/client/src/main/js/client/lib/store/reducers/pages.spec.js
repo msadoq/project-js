409ed0942db9ee94ba1567fd8269d8a19603214d
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-16 12:47:25 +0000

    [FT:#3622] Remove dispatch uses in pages reducer tests

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/pages.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/pages.spec.js
@@ -1,8 +1,10 @@
 /* eslint no-unused-expressions: 0 */
-import { should, getStore, freezeMe } from '../../common/test';
+import { freezeReducer } from '../../common/test';
 import * as actions from '../actions/pages';
-import reducer from './pages';
+import pagesReducer from './pages';
 import * as types from '../types';
+
+const reducer = freezeReducer(pagesReducer);
 
 describe('store:page:reducer', () => {
   it('initial state', () => {
@@ -18,7 +20,6 @@ describe('store:page:reducer', () => {
   });
   describe('add', () => {
     it('add', () => {
-      // 'path', 'oId', 'absolutePath', 'isModified'
       const state = reducer(
         undefined,
         actions.add('myPageId', 'myTimebarId', 'Title', ['myViewId'], [
@@ -71,7 +72,7 @@ describe('store:page:reducer', () => {
       });
     });
   });
-  describe('remove', () => { // TODO changer car thunk
+  describe('remove', () => {
     it('removePage', () => {
       const state = reducer(
         { myPageId: { title: 'Title' } },
@@ -85,22 +86,6 @@ describe('store:page:reducer', () => {
         actions.removePage('foo')
       );
       state.should.have.property('myPageId');
-    });
-    it('remove', () => {
-      const { dispatch, getState } = getStore({ pages:
-        { myPageId: { title: 'Title', views: ['view1'] } },
-        views: { view1: {} } });
-      dispatch(actions.remove('myPageId'));
-      getState().pages.should.not.have.property('myPageId');
-      getState().views.should.not.have.property('view1');
-    });
-    it('remove unknown', () => {
-      const { dispatch, getState } = getStore({ pages:
-        { myPageId: { title: 'Title', views: ['view1'] } },
-        views: { view1: {} } });
-      dispatch(actions.remove('wrongPageId'));
-      getState().pages.should.have.property('myPageId');
-      getState().views.should.have.property('view1');
     });
   });
   describe('un/mount view', () => {
@@ -117,11 +102,9 @@ describe('store:page:reducer', () => {
       state.myPageId.views.should.eql(['myViewId', 'another']);
     });
     it('unmount', () => {
-      const { dispatch, getState } = getStore({ pages: { myPageId: { views: ['myViewId', 'another'], title: 'aa' } } });
-      dispatch(actions.unmountView('myPageId', 'myViewId'));
-      getState().pages.myPageId.views.should.eql(['another']);
-      dispatch(actions.unmountView('myPageId', 'another'));
-      getState().pages.myPageId.views.should.eql([]);
+      const state = { myPageId: { views: ['myViewId', 'another'], title: 'aa' } };
+      const nextState = reducer(state, actions.unmountView('myPageId', 'myViewId'));
+      nextState.myPageId.views.should.eql(['another']);
     });
   });
   describe('open/close editor', () => {
@@ -152,81 +135,7 @@ describe('store:page:reducer', () => {
         actions.updateLayoutSimple('myPageId', [{ key: '3' }, { key: '4' }])
       );
       state.myPageId.layout.should.eql([{ key: '3' }, { key: '4' }]);
-    });
-    it('update layout', () => {
-      const { dispatch, getState } = getStore({
-        pages:
-        { myPageId: { layout: [{ h: 2, i: 'v1' }, { h: 2, i: 'v2' }], title: 'aa' } },
-        views:
-        {
-          v1: { configuration: { collapsed: true } },
-          v2: { configuration: { collapsed: false } },
-        },
-      });
-      dispatch(actions.updateLayout('myPageId', [{ h: 3, i: 'v1' }, { h: 4, i: 'v2' }]));
-      getState().pages.should.eql(
-        { myPageId: { layout: [{ h: 3, i: 'v1' }, { h: 4, i: 'v2' }], title: 'aa', isModified: true } });
-      getState().views.should.eql(
-        {
-          v1: {
-            configuration: { collapsed: false },
-            isModified: true,
-          },
-          v2: { configuration: { collapsed: false } },
-        }
-      );
-    });
-  });
-  describe('addAndMount/unmountAndRemove', () => {
-    const { dispatch, getState } = getStore({ pages: { myPageId: { views: ['v1'], title: 'aa' } } });
-    let newViewId;
-    it('addAndMount', () => {
-      dispatch(actions.addAndMount('myPageId'));
-      getState().pages.myPageId.views.should.be.an('array').with.lengthOf(2);
-      newViewId = getState().pages.myPageId.views[1];
-    });
-    it('unmountAndRemove', () => {
-      dispatch(actions.unmountAndRemove('myPageId', newViewId));
-      getState().pages.myPageId.views.should.be.an('array').with.lengthOf(1);
-      should.not.exist(getState().views[newViewId]);
-    });
-  });
-  describe('moveViewToPage', () => {
-    let dispatch;
-    let getState;
-    beforeEach(() => {
-      const store = getStore({
-        pages: {
-          myPageId: { views: ['v1'], layout: [], title: 'aa' },
-          myOtherPage: { views: ['v2'], layout: [], title: 'bb' },
-        },
-      });
-      dispatch = store.dispatch;
-      getState = store.getState;
-    });
-    it('existing toPage ok', () => {
-      dispatch(actions.moveViewToPage('myWindowId', 'myPageId', 'myOtherPage', 'v1'));
-      getState().pages.should.deep.equal({
-        myPageId: { views: [], isModified: true, layout: [], title: 'aa' },
-        myOtherPage: { views: ['v2', 'v1'],
-          isModified: true,
-          layout: [{ h: 5, i: 'v1', w: 5, x: 0, y: 0 }],
-          title: 'bb',
-        },
-      });
-    });
-    it('toPage is a new page', () => {
-      dispatch(actions.moveViewToPage('myWindowId', 'myPageId', 'newId', 'v1'));
-      getState().pages.myPageId.should.deep.equal({ views: [], isModified: true, layout: [], title: 'aa' });
-      getState().pages.myOtherPage.should.deep.equal({ views: ['v2'], layout: [], title: 'bb' });
-      const pageId = Object.keys(getState().pages)[2];
-      getState().pages[pageId].views.should.deep.equal(['v1']);
-      getState().pages[pageId].isModified.should.deep.equal(true);
-    });
-    it('move to the same page', () => {
-      const oldState = getState();
-      dispatch(actions.moveViewToPage('myWindowId', 'myPageId', 'myPageId', 'v1'));
-      getState().should.equal(oldState);
+      state.myPageId.isModified.should.be.true;
     });
   });
   describe('updateAbsolutePath', () => {
@@ -236,7 +145,7 @@ describe('store:page:reducer', () => {
     });
     it('invalid page id', () => {
       const state = { page1: {} };
-      const newState = reducer(freezeMe(state), {
+      const newState = reducer(state, {
         type: types.WS_PAGE_UPDATE_ABSOLUTEPATH,
         payload: {
           pageId: 'myPage',
@@ -247,7 +156,7 @@ describe('store:page:reducer', () => {
     });
     it('valid page id', () => {
       const state = { page1: { absolutePath: 'path1' } };
-      const newState = reducer(freezeMe(state), {
+      const newState = reducer(state, {
         type: types.WS_PAGE_UPDATE_ABSOLUTEPATH,
         payload: {
           pageId: 'page1',
@@ -258,7 +167,7 @@ describe('store:page:reducer', () => {
     });
     it('update absolutePath for new page', () => {
       const state = { page1: { pageId: 'page1' } };
-      const newState = reducer(freezeMe(state), {
+      const newState = reducer(state, {
         type: types.WS_PAGE_UPDATE_ABSOLUTEPATH,
         payload: {
           pageId: 'page1',
@@ -281,7 +190,7 @@ describe('store:page:reducer', () => {
     });
     it('valid page id', () => {
       const state = { page1: { path: 'path1' } };
-      const newState = reducer(freezeMe(state), {
+      const newState = reducer(state, {
         type: types.WS_PAGE_UPDATEPATH,
         payload: {
           pageId: 'page1',
@@ -304,7 +213,7 @@ describe('store:page:reducer', () => {
     });
     it('sets oid', () => {
       const state = { page1: { } };
-      const newState = reducer(freezeMe(state), {
+      const newState = reducer(state, {
         type: types.WS_PAGE_SET_OID,
         payload: {
           pageId: 'page1',
@@ -315,7 +224,6 @@ describe('store:page:reducer', () => {
     });
   });
   describe('setModified', () => {
-    // export const setModified = simple(types.WS_PAGE_SETMODIFIED, 'pageId', 'flag');
     it('no change', () => {
       reducer({ myPage: { isModified: false } }, actions.setModified('myPage', false))
       .should.eql({ myPage: { isModified: false } });
@@ -332,20 +240,16 @@ describe('store:page:reducer', () => {
     });
     it('invalid view id', () => {
       const state = { myPage: { isModified: true } };
-      reducer(freezeMe(state), actions.setModified('noPage', false))
+      reducer(state, actions.setModified('noPage', false))
       .should.eql(state);
     });
   });
   describe('updateTimebarId', () => {
     it('ok', () => {
-      const { dispatch, getState } = getStore({ pages: { myPage: { timebarUuid: 't1' } } });
-      dispatch(actions.updateTimebarId('myPage', 'time1'));
-      getState().pages.myPage.should.deep.equal({ timebarUuid: 'time1' });
-    });
-    it('invalid view id', () => {
-      const { dispatch, getState } = getStore({ pages: { myPage: { timebarUuid: 't1' } } });
-      dispatch(actions.updateTimebarId('noPage', 'time1'));
-      getState().pages.myPage.should.deep.equal({ timebarUuid: 't1' });
+      const state = { myPage: { timebarUuid: 'tb1' } };
+      const action = { type: types.WS_PAGE_UPDATE_TIMEBARID, payload: { pageId: 'myPage', timebarUuid: 'newTb1' } };
+      const nextState = reducer(state, action);
+      nextState.myPage.timebarUuid.should.be.eql('newTb1');
     });
   });
   describe('updateTimebarHeight', () => {
@@ -364,13 +268,13 @@ describe('store:page:reducer', () => {
   });
   describe('close workspace', () => {
     it('close', () => {
-      reducer(freezeMe({ myPage: { timebarHeight: 5 } }), { type: types.HSC_CLOSE_WORKSPACE })
+      reducer({ myPage: { timebarHeight: 5 } }, { type: types.HSC_CLOSE_WORKSPACE })
       .should.be.an('object').that.is.empty;
     });
   });
   describe('collapse', () => {
     it('collapses timebar', () => {
-      const state = freezeMe({ myPage: { timebarHeight: 42 } });
+      const state = { myPage: { timebarHeight: 42 } };
       const action = actions.collapseTimebar('myPage', true);
       const newState = reducer(state, action);
       newState.myPage.should.be.eql({

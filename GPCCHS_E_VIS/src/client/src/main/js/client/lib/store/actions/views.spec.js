055e6e0567eb1d93b5cc4090c5773e7c584597a9
Guillaume ARM <guillaume.arm.external@atos.net>
2017-02-17 13:44:09 +0000

    [FT:#3622] Test all views thunks  .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/actions/views.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/actions/views.spec.js
@@ -6,18 +6,29 @@ import { freezeMe } from '../../common/test';
 
 describe('store:actions:views', () => {
   const state = freezeMe({
+    domains: [
+      { name: 'fr.cnes.isis' },
+    ],
     views: {
-      view1: {
+      textview: {
+        type: 'TextView',
         configuration: { collapsed: false },
         path: '/folder1/oldPath',
         absolutePath: '/folder1/oldPath',
+        pages: ['pageWithLayout'],
+      },
+      plotview: {
+        type: 'PlotView',
+        pages: ['emptyPage'],
       },
     },
     pages: {
-      page1: {
+      pageWithLayout: {
+        views: ['textview'],
+        timebarUuid: 'tb1',
         layout: [
           {
-            i: 'view1',
+            i: 'textview',
             maxH: 100,
             maxW: 100,
             x: 0,
@@ -36,8 +47,28 @@ describe('store:actions:views', () => {
           },
         ],
       },
+      emptyPage: {
+        views: ['plotview'],
+        timebarUuid: 'unknownTimbarId',
+      },
+    },
+    timebars: {
+      tb1: {
+        masterId: 'masterId',
+      },
     },
   });
+  const emptyEntryPoint = {
+    connectedData: {},
+    connectedDataX: {},
+    connectedDataY: {},
+  };
+  const entryPoint = {
+    connectedData: { timeline: 1, domain: 2 },
+    connectedDataX: { timeline: 3, domain: 4 },
+    connectedDataY: { timeline: 5, domain: 5 },
+  };
+
   let dispatch;
   const getState = () => state;
 
@@ -46,41 +77,56 @@ describe('store:actions:views', () => {
   });
 
   describe('update path', () => {
-    const doUpdatePathTests = (updatePathFn, type) => {
+    describe('updatePath', () => {
       it('should dispatch', () => {
-        updatePathFn('view1', '/folder1/newPath')(dispatch, getState);
+        actions.updatePath('textview', '/folder1/newPath')(dispatch, getState);
         dispatch.should.have.been.calledOnce;
         dispatch.getCall(0).calledWith({
-          type,
-          payload: { viewId: 'view1', newPath: '/folder1/newPath' },
+          type: types.WS_VIEW_UPDATEPATH,
+          payload: { viewId: 'textview', newPath: '/folder1/newPath' },
         });
       });
       it('should not dispatch when newPath is falsy', () => {
-        updatePathFn('view1', '')(dispatch, getState);
+        actions.updatePath('textview', '')(dispatch, getState);
         dispatch.should.have.not.been.calledOnce;
       });
       it('should not dispatch when view is unknow', () => {
-        updatePathFn('unknow_view', '/folder1/newPath')(dispatch, getState);
+        actions.updatePath('unknow_view', '/folder1/newPath')(dispatch, getState);
         dispatch.should.have.not.been.calledOnce;
       });
       it('should not dispatch when newPath and oldPath are the same', () => {
-        updatePathFn('view1', '/../folder1/oldPath')(dispatch, getState);
+        actions.updatePath('textview', '/../folder1/oldPath')(dispatch, getState);
         dispatch.should.have.not.been.calledOnce;
       });
-    };
-
-    describe('updatePath', () => {
-      doUpdatePathTests(actions.updatePath, types.WS_VIEW_UPDATEPATH);
     });
 
     describe('updateAbsolutePath', () => {
-      doUpdatePathTests(actions.updateAbsolutePath, types.WS_VIEW_UPDATE_ABSOLUTEPATH);
+      it('should dispatch', () => {
+        actions.updateAbsolutePath('textview', '/folder1/newPath')(dispatch, getState);
+        dispatch.should.have.been.calledOnce;
+        dispatch.getCall(0).calledWith({
+          type: types.WS_VIEW_UPDATE_ABSOLUTEPATH,
+          payload: { viewId: 'textview', newPath: '/folder1/newPath' },
+        });
+      });
+      it('should not dispatch when newPath is falsy', () => {
+        actions.updateAbsolutePath('textview', '')(dispatch, getState);
+        dispatch.should.have.not.been.calledOnce;
+      });
+      it('should not dispatch when view is unknow', () => {
+        actions.updateAbsolutePath('unknow_view', '/folder1/newPath')(dispatch, getState);
+        dispatch.should.have.not.been.calledOnce;
+      });
+      it('should not dispatch when newPath and oldPath are the same', () => {
+        actions.updateAbsolutePath('textview', '/../folder1/oldPath')(dispatch, getState);
+        dispatch.should.have.not.been.calledOnce;
+      });
     });
   });
 
   describe('setCollapsedAndUpdateLayout', () => {
     it('collapses', () => {
-      actions.setCollapsedAndUpdateLayout('page1', 'view1', true)(dispatch, getState);
+      actions.setCollapsedAndUpdateLayout('pageWithLayout', 'textview', true)(dispatch, getState);
 
       dispatch.should.have.been.callCount(3);
       dispatch.getCall(0).args[0].should.be.a('function');
@@ -89,11 +135,11 @@ describe('store:actions:views', () => {
 
       dispatch.getCall(1).should.have.been.calledWith({
         type: types.WS_VIEW_SETCOLLAPSED,
-        payload: { viewId: 'view1', flag: true },
+        payload: { viewId: 'textview', flag: true },
       });
       dispatch.getCall(2).should.have.been.calledWith({
         type: types.WS_VIEW_SETMODIFIED,
-        payload: { viewId: 'view1', flag: true },
+        payload: { viewId: 'textview', flag: true },
       });
 
       const dispatchedThunk = dispatch.getCall(0).args[0];
@@ -104,12 +150,12 @@ describe('store:actions:views', () => {
       dispatch.getCall(0).should.have.been.calledWith({
         type: types.WS_PAGE_UPDATE_LAYOUT,
         payload: {
-          pageId: 'page1',
+          pageId: 'pageWithLayout',
           layout: [
             {
               minW: 3,
               minH: 3,
-              i: 'view1',
+              i: 'textview',
               maxH: 3,
               maxW: 5,
               x: 0,
@@ -133,7 +179,7 @@ describe('store:actions:views', () => {
       });
     });
     it('uncollapses', () => {
-      actions.setCollapsedAndUpdateLayout('page1', 'view1', false)(dispatch, getState);
+      actions.setCollapsedAndUpdateLayout('pageWithLayout', 'textview', false)(dispatch, getState);
 
       dispatch.should.have.been.callCount(3);
       dispatch.getCall(0).args[0].should.be.a('function');
@@ -142,11 +188,11 @@ describe('store:actions:views', () => {
 
       dispatch.getCall(1).should.have.been.calledWith({
         type: types.WS_VIEW_SETCOLLAPSED,
-        payload: { viewId: 'view1', flag: false },
+        payload: { viewId: 'textview', flag: false },
       });
       dispatch.getCall(2).should.have.been.calledWith({
         type: types.WS_VIEW_SETMODIFIED,
-        payload: { viewId: 'view1', flag: true },
+        payload: { viewId: 'textview', flag: true },
       });
 
       const dispatchedThunk = dispatch.getCall(0).args[0];
@@ -157,12 +203,12 @@ describe('store:actions:views', () => {
       dispatch.getCall(0).should.have.been.calledWith({
         type: types.WS_PAGE_UPDATE_LAYOUT,
         payload: {
-          pageId: 'page1',
+          pageId: 'pageWithLayout',
           layout: [
             {
               minW: 3,
               minH: 3,
-              i: 'view1',
+              i: 'textview',
               maxH: undefined,
               maxW: undefined,
               x: 0,
@@ -182,6 +228,122 @@ describe('store:actions:views', () => {
               w: 5,
             },
           ],
+        },
+      });
+    });
+  });
+
+  describe('addEntryPoint', () => {
+    it('should works with a TexView, with empty entryPoint', () => {
+      actions.addEntryPoint('textview', emptyEntryPoint)(dispatch, getState);
+
+      dispatch.should.have.been.callCount(2);
+      dispatch.getCall(0).args[0].should.be.an('object');
+      dispatch.getCall(1).args[0].should.be.an('object');
+
+      dispatch.getCall(0).should.have.been.calledWith({
+        type: types.WS_PAGE_EDITOR_OPEN,
+        payload: {
+          pageId: 'pageWithLayout',
+          viewId: 'textview',
+          viewType: 'TextView',
+        },
+      });
+      dispatch.getCall(1).should.have.been.calledWith({
+        type: types.WS_VIEW_ADD_ENTRYPOINT,
+        payload: {
+          viewId: 'textview',
+          entryPoint: {
+            connectedDataX: {},
+            connectedDataY: {},
+            connectedData: { timeline: 'masterId', domain: 'fr.cnes.isis' },
+          },
+        },
+      });
+    });
+    it('should works with a TexView, with entryPoint', () => {
+      actions.addEntryPoint('textview', entryPoint)(dispatch, getState);
+
+      dispatch.should.have.been.callCount(2);
+      dispatch.getCall(0).args[0].should.be.an('object');
+      dispatch.getCall(1).args[0].should.be.an('object');
+
+      dispatch.getCall(0).should.have.been.calledWith({
+        type: types.WS_PAGE_EDITOR_OPEN,
+        payload: {
+          pageId: 'pageWithLayout',
+          viewId: 'textview',
+          viewType: 'TextView',
+        },
+      });
+
+      dispatch.getCall(1).should.have.been.calledWith({
+        type: types.WS_VIEW_ADD_ENTRYPOINT,
+        payload: {
+          viewId: 'textview',
+          entryPoint: {
+            connectedData: { timeline: 1, domain: 2 },
+            connectedDataX: { timeline: 3, domain: 4 },
+            connectedDataY: { timeline: 5, domain: 5 },
+          },
+        },
+      });
+    });
+
+    it('should works with a PlotView, with empty entryPoint', () => {
+      actions.addEntryPoint('plotview', emptyEntryPoint)(dispatch, getState);
+
+      dispatch.should.have.been.callCount(2);
+      dispatch.getCall(0).args[0].should.be.an('object');
+      dispatch.getCall(1).args[0].should.be.an('object');
+
+      dispatch.getCall(0).should.have.been.calledWith({
+        type: types.WS_PAGE_EDITOR_OPEN,
+        payload: {
+          pageId: 'emptyPage',
+          viewId: 'plotview',
+          viewType: 'PlotView',
+        },
+      });
+
+      dispatch.getCall(1).should.have.been.calledWith({
+        type: types.WS_VIEW_ADD_ENTRYPOINT,
+        payload: {
+          viewId: 'plotview',
+          entryPoint: {
+            connectedData: { },
+            connectedDataX: { timeline: '*', domain: 'fr.cnes.isis' },
+            connectedDataY: { timeline: '*', domain: 'fr.cnes.isis' },
+          },
+        },
+      });
+    });
+
+    it('should works with a TexView, with entryPoint', () => {
+      actions.addEntryPoint('plotview', entryPoint)(dispatch, getState);
+
+      dispatch.should.have.been.callCount(2);
+      dispatch.getCall(0).args[0].should.be.an('object');
+      dispatch.getCall(1).args[0].should.be.an('object');
+
+      dispatch.getCall(0).should.have.been.calledWith({
+        type: types.WS_PAGE_EDITOR_OPEN,
+        payload: {
+          pageId: 'emptyPage',
+          viewId: 'plotview',
+          viewType: 'PlotView',
+        },
+      });
+
+      dispatch.getCall(1).should.have.been.calledWith({
+        type: types.WS_VIEW_ADD_ENTRYPOINT,
+        payload: {
+          viewId: 'plotview',
+          entryPoint: {
+            connectedData: { timeline: 1, domain: 2 },
+            connectedDataX: { timeline: 3, domain: 4 },
+            connectedDataY: { timeline: 5, domain: 5 },
+          },
         },
       });
     });

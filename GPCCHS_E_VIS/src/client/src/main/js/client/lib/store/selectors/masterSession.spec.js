4ac14c9867ab2b77a570ee84bfca6a84330d60e6
Paul Musso <paul.musso.external@atos.net>
2017-02-21 10:51:21 +0000

    [FT:#3622] add tests on masterSession selectors

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/selectors/masterSession.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/selectors/masterSession.spec.js
@@ -1,5 +1,8 @@
 import { should } from '../../common/test';
-import { getMasterSessionId } from './masterSession';
+import {
+  getMasterSessionId,
+  getMasterSession,
+} from './masterSession';
 
 describe('store:masterSession:selectors', () => {
   it('should support empty state without error', () => {
@@ -8,5 +11,12 @@ describe('store:masterSession:selectors', () => {
   it('should returns master sessionId', () => {
     getMasterSessionId({ masterSession: { sessionId: 10 } })
       .should.equal(10);
+  });
+  it('should return master session', () => {
+    const state = {
+      masterSession: { sessionId: 10 },
+      sessions: [{ id: 10 }],
+    };
+    getMasterSession(state).should.eql({ id: 10 });
   });
 });

779b54dd8034b45a9533fa1c7bd2e01d66022caa
Guillaume ARM <guillaume.arm@redpelicans.com>
2017-03-08 21:41:52 +0100

    [FT:#3622] Fix unremoved views when close a page

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/index.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/index.js
@@ -1,5 +1,8 @@
+import _ from 'lodash/fp';
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
+import composeReducers from '../composeReducers';
+
 import hsc from './hsc';
 import timebars from './timebars';
 import timebarTimelines from './timebarTimelines';
@@ -15,7 +18,22 @@ import viewData from './viewData';
 import health from './health';
 import editor from './editor';
 
-const reducer = combineReducers({
+import * as types from '../types';
+
+const rootReducer = (state, action) => {
+  switch (action.type) {
+    case types.WS_PAGE_CLOSE: { // Remove views from closed page
+      const { pageId } = action.payload;
+      const viewIds = _.get(['pages', pageId, 'views'], state);
+      const removeViews = _.update('views', _.omit(viewIds));
+      return removeViews(state);
+    }
+    default:
+      return state;
+  }
+};
+
+const allReducer = combineReducers({
   form,
   hsc,
   timebars,
@@ -33,4 +51,4 @@ const reducer = combineReducers({
   editor,
 });
 
-export default reducer;
+export default composeReducers(allReducer, rootReducer);

449db10cfc3d3b58bb1e81e034c7c07a95f69d6e
Guillaume ARM <guillaume.arm.external@atos.net>
2017-03-09 10:15:06 +0000

    [FT:#3622] Change tests in reducers/pages  .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/pages.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/pages.spec.js
@@ -18,60 +18,6 @@ describe('store:page:reducer', () => {
     const state = { myPageId: { title: 'Title' } };
     reducer(state, { payload: { pageId: 'myPageId' } }).should.eql(state);
   });
-  describe('add', () => {
-    it('add', () => {
-      const state = reducer(
-        undefined,
-        actions._add('myPageId', 'myTimebarId', 'Title', ['myViewId'], [
-          { viewId: 'myViewId', x: 10, y: 10, w: 10, h: 10 },
-        ], '/path', '1234', '/absolutePath', true, [1, 2, 3, 4])
-      );
-      state.myPageId.should.deep.eql({
-        title: 'Title',
-        timebarHeight: 135,
-        timebarUuid: 'myTimebarId',
-        views: ['myViewId'],
-        layout: [
-          { viewId: 'myViewId', x: 10, y: 10, w: 10, h: 10 },
-        ],
-        editor: {
-          isOpened: false,
-          viewId: null,
-          viewType: null,
-        },
-        path: '/path',
-        oId: '1234',
-        absolutePath: '/absolutePath',
-        isModified: true,
-        properties: [1, 2, 3, 4],
-        timebarCollapsed: false,
-      });
-    });
-    it('add empty', () => {
-      const state = reducer(
-        undefined,
-        actions._add('myPageId')
-      );
-      state.myPageId.should.deep.eql({
-        title: 'Unknown',
-        timebarHeight: 135,
-        timebarUuid: null,
-        views: [],
-        layout: [],
-        editor: {
-          isOpened: false,
-          viewId: null,
-          viewType: null,
-        },
-        path: undefined,
-        oId: undefined,
-        absolutePath: undefined,
-        isModified: true,
-        properties: [],
-        timebarCollapsed: false,
-      });
-    });
-  });
   describe('remove', () => {
     it('removePage', () => {
       const state = reducer(

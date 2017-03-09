d665c378b0b0f3ea11639c64f7c32c21582c7783
Guillaume ARM <guillaume.arm.external@atos.net>
2017-03-09 08:57:43 +0000

    [FT:#3622] Rewrite reducers/timebarTimelines tests  . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/timebarTimelines.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/timebarTimelines.spec.js
@@ -1,51 +1,66 @@
 /* eslint no-unused-expressions: 0 */
-// import _omit from 'lodash/omit';
 import { freezeArgs } from '../../common/test';
-import * as actions from '../actions/timebarTimelines';
 import timebarTimelinesReducer from './timebarTimelines';
-// import * as types from '../types';
+import * as types from '../types';
 
 const reducer = freezeArgs(timebarTimelinesReducer);
 
 describe('store:timebarTimelines:reducer', () => {
-  it('add timebar: emptyState', () => {
-    reducer(undefined, actions.addTimebar('tb1')).should.eql({ tb1: [] });
+  it('does nothing with unknown action', () => {
+    const state = reducer(undefined, { type: 'DUMMY_ACTION' });
+    state.should.be.eql({});
   });
-  it('add timebar: State ok', () => {
-    reducer({ tb1: ['tl1'] }, actions.addTimebar('tb2')).should.eql({ tb1: ['tl1'], tb2: [] });
+  it('cleans timebarTimelines when close workspace', () => {
+    const state = reducer({ a: [1, 2, 3] }, { type: types.HSC_CLOSE_WORKSPACE });
+    state.should.be.eql({});
   });
-  it('add timebar: state with timebar', () => {
-    const state = { tb1: ['tl1'], tb2: [] };
-    reducer(state, actions.addTimebar('tb2')).should.equal(state);
+  it('add timebar and timelines ids when load a workspace', () => {
+    const action = {
+      type: types.WS_LOAD_DOCUMENTS,
+      payload: {
+        documents: {
+          timebars: {
+            tb1: {
+              uuid: 'tb1',
+              timelines: [1, 2, 3],
+            },
+            tb2: {
+              uuid: 'tb2',
+              timelines: [1, 2, 3],
+            },
+          },
+        },
+      },
+    };
+    const state = reducer({ a: [1, 2, 3] }, action);
+    state.should.be.eql({
+      a: [1, 2, 3],
+      tb1: [1, 2, 3],
+      tb2: [1, 2, 3],
+    });
   });
-  it('add timeline: emptyState', () => {
-    reducer(undefined, actions.mountTimeline('tb1', 'tl1')).should.eql({ tb1: ['tl1'] });
+  it('creates new timebar', () => {
+    const action = {
+      type: types.WS_TIMEBAR_CREATE_NEW,
+      payload: { timebarUuid: 'tb1' },
+    };
+    const state = reducer({ a: [1, 2, 3] }, action);
+    state.should.be.eql({ a: [1, 2, 3], tb1: [] });
   });
-  it('add timeline: State with TB', () => {
-    const newState = reducer({ tb1: ['tl1'] }, actions.mountTimeline('tb1', 'tl2'));
-    newState.should.eql({ tb1: ['tl1', 'tl2'] });
+  it('creates new timeline', () => {
+    const action = {
+      type: types.WS_TIMELINE_CREATE_NEW,
+      payload: { timebarUuid: 'tb1', timeline: { uuid: 'tl2' } },
+    };
+    const state = reducer({ tb1: ['tl1'] }, action);
+    state.should.be.eql({ tb1: ['tl1', 'tl2'] });
   });
-  it('add timeline: State with TL', () => {
-    reducer({ tb1: ['tl1'] }, actions.mountTimeline('tb1', 'tl1')).should.eql({ tb1: ['tl1'] });
-  });
-  it('remove timebar: emptyState', () => {
-    reducer(undefined, actions.removeTimebar('tb1')).should.eql({});
-  });
-  it('remove timebar: State ok', () => {
-    reducer({ tb1: ['tl1'], tb2: ['tl2'] }, actions.removeTimebar('tb1')).should.eql({ tb2: ['tl2'] });
-  });
-  it('remove timebar: state without timebar', () => {
-    const state = { tb1: ['tl1'], tb2: [] };
-    reducer(state, actions.removeTimebar('tb32')).should.equal(state);
-  });
-  it('remove timeline: emptyState', () => {
-    reducer(undefined, actions.unmountTimeline('tb1', 'tl1')).should.eql({ });
-  });
-  it('remove timeline: State with TB', () => {
-    reducer({ tb1: ['tl1', 'tl2'] }, actions.unmountTimeline('tb1', 'tl2')).should.eql({ tb1: ['tl1'] });
-  });
-  it('remove timeline: State without TL', () => {
-    const state = { tb1: ['tl1'], tb2: [] };
-    reducer(state, actions.unmountTimeline('tb1', 'tl2')).should.eql(state);
+  it('removes timeline', () => {
+    const action = {
+      type: types.WS_TIMELINE_REMOVE,
+      payload: { timelineUuid: 'tl1', timebarUuid: 'tb1' },
+    };
+    const state = reducer({ tb1: ['tl1', 'tl2'], tb2: [] }, action);
+    state.should.be.eql({ tb1: ['tl2'], tb2: [] });
   });
 });

ea146cf08012ef68df79810f07efc63edfe9094f
Guillaume ARM <guillaume.arm.external@atos.net>
2017-03-09 10:51:52 +0000

    [FT:#3622] Remove some tests in timelines.spec.js
    need to rewrite it

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/timelines.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/timelines.spec.js
@@ -1,6 +1,5 @@
 /* eslint no-unused-expressions: 0 */
 import { freezeArgs } from '../../common/test';
-import * as actions from '../actions/timelines';
 import timelinesReducer from './timelines';
 import * as types from '../types';
 
@@ -13,50 +12,6 @@ describe('store:timelines:reducer', () => {
   it('unknown action', () => {
     reducer({ myTimelineUuid: { id: 'myTimelineId' } }, { payload: { timelineId: 'myTimelineId' } })
       .should.eql({ myTimelineUuid: { id: 'myTimelineId' } });
-  });
-  describe('add', () => {
-    it('add', () => {
-      const fixture = {
-        id: 'Id',
-        offset: 10,
-        kind: 'DataSet',
-        sessionId: 100,
-      };
-      const state = reducer(
-        undefined,
-        actions.add('myTimelineUuid', fixture)
-      );
-      state.myTimelineUuid.should.have.properties(fixture);
-    });
-    it('add empty', () => {
-      const state = reducer(
-        undefined,
-        actions.add('myTimelineUuid')
-      );
-      state.myTimelineUuid.should.have.properties({
-        id: null,
-        offset: 0,
-        kind: 'Session',
-        sessionId: null,
-        color: '#31b0d5',
-      });
-    });
-  });
-  describe('remove', () => {
-    it('remove', () => {
-      const state = reducer(
-        { myTimelineUuid: { id: 'Id' } },
-        actions.remove('myTimelineUuid')
-      );
-      state.should.not.have.property('myTimelineUuid');
-    });
-    it('remove unknown', () => {
-      const state = reducer(
-        { myTimelineUuid: { id: 'Id' } },
-        actions.remove('foo')
-      );
-      state.should.have.property('myTimelineUuid');
-    });
   });
   describe('HSC workspace', () => {
     it('close', () => {

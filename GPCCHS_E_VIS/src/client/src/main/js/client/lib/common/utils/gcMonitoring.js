a0dd932d27ff0b091fe395b7519f53f72c279a72
Paul Musso <paul.musso.external@atos.net>
2017-03-13 10:54:24 +0000

    [FT:#3622] Remove use of console.time from gcMonitoring

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/common/utils/gcMonitoring.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/common/utils/gcMonitoring.js
@@ -6,6 +6,8 @@ const logger = getLogger('gcMonitoring');
 export default function () {
   let size;
   let time;
+  let gcStartTime;
+  let gcEndTime;
 
   const getHeapSize = () => Math.floor(process.memoryUsage().heapUsed / (1024 * 1024));
   const getTime = () => (new Date()).getTime();
@@ -13,16 +15,15 @@ export default function () {
   gcWatch.on('beforeGC', () => {
     // detect memory usage, push stats etc.
     size = getHeapSize();
-    // eslint-disable-next-line no-console
-    console.time('GC Duration');
+    gcStartTime = getTime();
   });
 
   gcWatch.on('afterGC', () => {
     // calculate memory freed by GC, etc.
     logger.info('-------------------------------------------------');
     logger.info(`GC passed, memory free ${size - getHeapSize()} mb (Total: ${getHeapSize()} mb)`);
-    // eslint-disable-next-line no-console
-    console.timeEnd('GC Duration');
+    gcEndTime = getTime();
+    logger.info(`GC duration: ${gcEndTime - gcStartTime} ms`);
     logger.info(`Since last GC tick : ${getTime() - (time || getTime())} ms`);
     logger.info('-------------------------------------------------');
     time = getTime();

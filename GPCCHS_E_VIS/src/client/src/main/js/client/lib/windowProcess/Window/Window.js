90b0686244b3be9480271543604efe17998a5f1b
Damien BRUGNE <damien.brugne.external@atos.net>
2017-03-15 16:16:10 +0000

    [FT:#5828] Control help, explorer and timebar from electron menu

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Window/Window.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Window/Window.js
@@ -3,8 +3,6 @@ import { ButtonToolbar } from 'react-bootstrap';
 import getLogger from 'common/log';
 import classnames from 'classnames';
 import DebugContainer from '../Navigation/DebugContainer';
-import Help from '../Navigation/Help';
-import Explorer from '../Navigation/Explorer';
 import HelpContent from '../Navigation/HelpContent';
 import MasterSessionContainer from '../Navigation/MasterSessionContainer';
 import HealthContainer from '../Navigation/HealthContainer';
@@ -12,9 +10,9 @@ import MessagesContainer from '../Navigation/MessagesContainer';
 import TabsContainer from '../Navigation/TabsContainer';
 import PageContainer from '../Page/PageContainer';
 import TimebarMasterContainer from '../Timebar/TimebarMasterContainer';
-import styles from './Window.css';
 import ExplorerContainer from '../Explorer/ExplorerContainer';
-// import DummyDrag from '../Navigation/DummyDrag';
+
+import styles from './Window.css';
 
 const logger = getLogger('Window');
 
@@ -26,7 +24,6 @@ export default class Window extends PureComponent {
     isExplorerOpened: PropTypes.bool,
     isHelpDisplayed: PropTypes.bool,
     setIsLoaded: PropTypes.func.isRequired,
-    displayExplorer: PropTypes.func.isRequired,
     displayHelp: PropTypes.func.isRequired,
   };
   static defaultProps = {
@@ -45,46 +42,19 @@ export default class Window extends PureComponent {
   }
 
   componentDidMount() {
-    document.addEventListener('keydown', this.toggleHelpShortCut);
-    document.addEventListener('keydown', this.toggleExplorerShortCut);
+    document.addEventListener('keydown', this.closeHelpShortCut);
 
     // set in store that this is window is fully loaded and ready to run
     const { setIsLoaded, windowId } = this.props;
     setTimeout(() => setIsLoaded(windowId), 0);
   }
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.toggleHelpShortCut);
-    document.removeEventListener('keydown', this.toggleExplorerShortCut);
-  }
-
-  toggleHelpShortCut = (e) => {
-    if (e.keyCode === 72 && e.ctrlKey) {
-      this.toggleHelp();
-    } else if (e.keyCode === 27 && this.props.isHelpDisplayed) {
-      this.toggleHelp();
-    }
-  }
-
-  toggleHelp = (e) => {
-    if (e) {
+  closeHelpShortCut = (e) => {
+    if (e.keyCode === 27 && this.props.isHelpDisplayed) {
       e.preventDefault();
+      const { displayHelp, windowId, isHelpDisplayed } = this.props;
+      displayHelp(windowId, !isHelpDisplayed);
     }
-
-    const { displayHelp, windowId, isHelpDisplayed } = this.props;
-    displayHelp(windowId, !isHelpDisplayed);
-  }
-
-  toggleExplorerShortCut = (e) => {
-    if (e.keyCode === 72 && e.ctrlKey) {
-      this.toggleExplorer();
-    } else if (e.keyCode === 27 && this.props.isExplorerOpened) {
-      this.toggleExplorer();
-    }
-  }
-
-  toggleExplorer = () => {
-    this.props.displayExplorer(this.props.windowId, !this.props.isExplorerOpened);
   }
 
   render() {
@@ -104,13 +74,10 @@ export default class Window extends PureComponent {
           <MasterSessionContainer />
           <HealthContainer windowId={windowId} />
           <MessagesContainer />
-          <Help toggleHelp={this.toggleHelp} />
-          <Explorer toggleExplorer={this.toggleExplorer} />
           <DebugContainer
             windowId={windowId}
             focusedPageId={focusedPageId}
           />
-          {/* <DummyDrag /> */}
         </ButtonToolbar>
         <div className={styles.divPagesTb}>
           <div className={styles.contentDiv}>
--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Window/WindowContainer.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Window/WindowContainer.js
@@ -4,21 +4,17 @@ import {
   getExplorerDisplay,
   getDisplayHelp,
 } from '../../store/selectors/windows';
-import { setIsLoaded, displayExplorer, displayHelp } from '../../store/actions/windows';
+import { setIsLoaded, displayHelp } from '../../store/actions/windows';
 
 import Window from './Window';
 
 const mapStateToProps = (state, { windowId }) => ({
   focusedPageId: getWindowFocusedPageId(state, { windowId }),
-  timelines: state.timelines,
+  timelines: state.timelines, // TODO boxmodel remove ?
   isModified: state.windows[windowId].isModified,
-  title: state.windows[windowId].title,
+  title: state.windows[windowId].title, // TODO boxmodel remove
   isExplorerOpened: getExplorerDisplay(state, { windowId }),
   isHelpDisplayed: getDisplayHelp(state, { windowId }),
 });
 
-export default connect(mapStateToProps, {
-  displayExplorer,
-  setIsLoaded,
-  displayHelp,
-})(Window);
+export default connect(mapStateToProps, { setIsLoaded, displayHelp })(Window);

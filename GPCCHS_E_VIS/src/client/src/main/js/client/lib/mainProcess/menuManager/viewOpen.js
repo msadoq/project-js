24a077dc7dc9ae6e875fb0a87292736d7d9292ca
Guillaume ARM <guillaume.arm.external@atos.net>
2017-03-24 07:58:42 +0000

    [FT:#5828] Add an empty page while adding a view if no page opened

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/mainProcess/menuManager/viewOpen.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/mainProcess/menuManager/viewOpen.js
@@ -8,6 +8,7 @@ import { server } from '../ipc';
 import { openView } from '../../documentManager';
 import { getPathByFilePicker } from '../dialog';
 import { addBlankView } from '../../store/actions/views';
+import { addBlankPage } from '../../store/actions/pages';
 import { getWindowFocusedPageId } from '../../store/reducers/windows';
 import { getStore } from '../../store/mainStore';
 import { getViewModule } from '../../viewManager';
@@ -26,19 +27,27 @@ function viewOpen(focusedWindow) {
 
 function viewOpenWithPath({ windowId, absolutePath }) {
   const { dispatch, getState } = getStore();
-  const pageUuid = getWindowFocusedPageId(getState(), { windowId });
+  const focusedPageId = getWindowFocusedPageId(getState(), { windowId });
+  const pageId = focusedPageId || v4();
   const viewInfo = { absolutePath };
-  dispatch(openView(viewInfo, pageUuid));
+  if (!focusedPageId) {
+    dispatch(addBlankPage(windowId, pageId));
+  }
+  dispatch(openView(viewInfo, pageId));
 }
 
 const viewAddBlank = (type, focusedWindow) => {
   const { dispatch, getState } = getStore();
-  const pageId = getState().windows[focusedWindow.windowId].focusedPage;
+  const focusedPageId = getState().windows[focusedWindow.windowId].focusedPage;
+  const pageId = focusedPageId || v4();
   const view = {
     type,
     configuration: getViewModule(type).prepareConfigurationForStore({}),
     uuid: v4(),
   };
+  if (!focusedPageId) {
+    dispatch(addBlankPage(focusedWindow.windowId, pageId));
+  }
   dispatch(addBlankView(view, pageId));
   server.sendProductLog(LOG_DOCUMENT_OPEN, 'view', `new ${_.get('type', view)}`);
 };

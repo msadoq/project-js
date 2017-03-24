1c0e35e630420691330eb7d7375a21e116adc2e8
Guillaume ARM <guillaume.arm.external@atos.net>
2017-03-24 12:28:56 +0000

    [FT:#5828] Fix bugs in PlotView/TextView editors
    due to the panels merge

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/PlotView/Components/Editor/EntryPointDetailsContainer.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/PlotView/Components/Editor/EntryPointDetailsContainer.js
@@ -2,16 +2,16 @@ import { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import EntryPointDetails from './EntryPointDetails';
 import { getView } from '../../../../store/reducers/views';
-import { getPage } from '../../../../store/reducers/pages';
+import { getFocusedPage } from '../../../../store/selectors/pages';
 import { getTimebarTimelinesSelector } from '../../../../store/selectors/timebars';
 import {
   updateEntryPoint,
   removeEntryPoint,
 } from '../../../../store/actions/views';
 
-const mapStateToProps = (state, { viewId, focusedPageId }) => {
+const mapStateToProps = (state, { viewId, windowId }) => {
   const view = getView(state, { viewId });
-  const { timebarUuid } = getPage(state, { pageId: focusedPageId });
+  const { timebarUuid } = getFocusedPage(state, { windowId });
   const timelines = getTimebarTimelinesSelector(state, { timebarUuid });
   return {
     axes: view.configuration.axes,

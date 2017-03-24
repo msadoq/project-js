9ad6fabf31cbc1c8376eebbf905947175b13b2ea
Alexis BESSON <alexis.besson@external.atos.net>
2017-03-24 10:47:33 +0000

    [FT:#5828] Maj airbnb version in package json

--- GPCCHS_E_VIS/src/client/src/main/js/client/tests.js
+++
@@ -1,6 +0,0 @@
-const _ = require('lodash/fp');
-
-let stateLayout = [{ collapsed: true, x: 1, y: 2, i: 'view1' }, { x: 1, y: 2, i: 'view2' }];
-let actionLayout = [{ x: 10, y: 20, i: 'view1' }, { x: 1, y: 2, i: 'view2' }];
-
-_.reduce((geo, newGeo) => { console.log(geo, newGeo); }, stateLayout, actionLayout)

3765c89a6c4a856919e8ecf0c561e3aa770fb4ab
Raphael FABRE <raphael.fabre@external.atos.net>
2017-03-31 15:02:32 +0000

    [FT:#5828] Mini syntax - improvements on timebar.

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Timebar/Timebar/Timebar.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Timebar/Timebar/Timebar.js
@@ -115,7 +115,6 @@ export default class Timebar extends PureComponent {
     e.stopPropagation();
     if (this.el.parentElement.querySelector(':hover')) {
       const {
-        visuWindow,
         timebarUuid,
         updateCursors,
         jump,
@@ -124,8 +123,12 @@ export default class Timebar extends PureComponent {
         play,
         pause,
         slideWindow,
+        visuWindow: {
+          lower,
+          upper,
+          current,
+        },
       } = this.props;
-      const { lower, upper, current } = visuWindow;
       const { cursorMs } = this.state;
 
       switch (e.keyCode) {
@@ -400,7 +403,7 @@ export default class Timebar extends PureComponent {
     const {
       resizeCursor,
     } = state;
-    let cursorPosMs = this.state.cursorMs;
+    let cursorPosMs = state.cursorMs;
 
     const lower = state.lower || visuWindow.lower;
     const upper = state.upper || visuWindow.upper;

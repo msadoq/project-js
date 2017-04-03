43414b1b3ee9ad20af38606a6bd1867fb85db335
Damien BRUGNE <damien.brugne.external@atos.net>
2017-04-03 14:00:26 +0000

    [FT:#6302] Add comment and fix coding convetions warning and un-needed relaxations

--- GPCCHS_E_VIS/src/common/src/main/js/common/protobuf/adapters/lpisis/cop1/clcwSegmentMask.js
+++ GPCCHS_E_VIS/src/common/src/main/js/common/protobuf/adapters/lpisis/cop1/clcwSegmentMask.js
@@ -1,4 +1,6 @@
 // Produced by Acceleo JavaScript Generator 1.1.0
+/* eslint-disable complexity, "DV6 TBC_CNES Generated code complexity is not avoidable" */
+
 const flagVal = require('./flagVal');
 const sequenceType = require('./sequenceType');
 
--- GPCCHS_E_VIS/src/common/src/main/js/common/protobuf/adapters/lpisis/types.js
+++ GPCCHS_E_VIS/src/common/src/main/js/common/protobuf/adapters/lpisis/types.js
@@ -4,8 +4,6 @@ const _isNumber = require('lodash/isNumber');
 const _isUndefined = require('lodash/isUndefined');
 const _isNull = require('lodash/isNull');
 const _isString = require('lodash/isString');
-
-// eslint-disable-next-line import/no-extraneous-dependencies
 const Long = require('long');
 const ByteBuffer = require('bytebuffer');
 
@@ -150,19 +148,19 @@ module.exports = {
         value = attribute;
         break;
       case 'object': {
-        // TODO : complete with real received type parsing from DC/LPISIS
         if (_isBuffer(attribute)) {
           type = '_blob';
           value = attribute;
-          break;
         }
+        break;
       }
-      default: // eslint-disable-line no-fallthrough
+      default:
         throw new Error(`Unknown data type ${mixedType}`);
     }
 
     return { [type]: { value } };
   },
+  // eslint-disable-next-line complexity, "DV6 TBC_CNES Un-avoidable complexity due to MAL sub-type"
   decodeAttribute: (attribute) => {
     if (attribute === null || typeof attribute === 'undefined') {
       return undefined;

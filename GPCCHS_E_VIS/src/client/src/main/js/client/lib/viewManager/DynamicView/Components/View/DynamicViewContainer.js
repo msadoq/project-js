f02bf3084230c6d9dbfce2bdadb2a44835d1102d
Guillaume ARM <guillaume.arm.external@atos.net>
2017-04-03 12:40:09 +0000

    [FT:#5828] Rewrite mapStateToProps in DynamicViewContainer  .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/DynamicView/Components/View/DynamicViewContainer.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/DynamicView/Components/View/DynamicViewContainer.js
@@ -1,16 +1,18 @@
 import { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import DynamicView from './DynamicView';
 import { getViewConfiguration } from '../../../../store/reducers/views';
 import { getViewEntryPoints } from '../../../../store/selectors/views';
 import { getFormula } from './selectors';
 import { getData } from '../../store/dataReducer';
 
-const mapStateToProps = (state, { viewId }) => ({
-  formula: getFormula(state, { viewId }),
-  configuration: getViewConfiguration(state, { viewId }),
-  entryPoints: getViewEntryPoints(state, { viewId }),
-  data: getData(state, { viewId }),
+const mapStateToProps = createStructuredSelector({
+  formula: getFormula,
+  configuration: getViewConfiguration,
+  entryPoints: getViewEntryPoints,
+  data: getData,
 });
 
 export const DynamicViewContainer = connect(mapStateToProps, null)(DynamicView);

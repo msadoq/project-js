207a2f98f1963cadc66179fac2f86c24e83a74f3
Damien BRUGNE <damien.brugne.external@atos.net>
2017-04-03 12:10:39 +0000

    [FT:#6302] Comment VIMA coding convention relaxation rules

--- GPCCHS_E_VIS/src/server/src/main/js/server/lib/utils/test.js
+++ GPCCHS_E_VIS/src/server/src/main/js/server/lib/utils/test.js
@@ -1,6 +1,7 @@
-/* eslint import/no-extraneous-dependencies:0 */
 const _each = require('lodash/each');
+// eslint-disable-next-line import/no-extraneous-dependencies, "DV6 TBC_CNES Only used by UT files"
 const chai = require('chai');
+// eslint-disable-next-line import/no-extraneous-dependencies, "DV6 TBC_CNES Only used by UT files"
 const properties = require('chai-properties');
 const database = require('../models/loki');
 

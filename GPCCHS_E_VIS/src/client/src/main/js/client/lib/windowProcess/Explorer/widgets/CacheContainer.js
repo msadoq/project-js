24a9ea7fc5eea28640b9339bc46a6c4f2a09566d
Audrey LEAL <audrey.leal.external@atos.net>
2017-04-07 15:34:23 +0000

    [FT:#5828] Complete performance tab in explorer

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Explorer/widgets/CacheContainer.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Explorer/widgets/CacheContainer.js
@@ -1,18 +1,11 @@
 import { connect } from 'react-redux';
 import Cache from './Cache';
-import { updateCacheInvalidation, play, pause } from '../../../store/actions/hsc';
+import { updateCacheInvalidation } from '../../../store/actions/hsc';
 import { getLastCacheInvalidation } from '../../../store/reducers/hsc';
-// import { getWindowFocusedPageId } from '../../../store/reducers/windows';
 
-const mapStateToProps = (state) => {
-  console.log('');
-  // const focusedPageId = getWindowFocusedPageId(state, windowId)
-  return {
-    lastCacheCleanUp: getLastCacheInvalidation(state),
-    play,
-    pause,
-  };
-};
+const mapStateToProps = state => ({
+  lastCacheCleanUp: getLastCacheInvalidation(state),
+});
 
 const CacheContainer = connect(mapStateToProps, { updateCacheInvalidation })(Cache);
 

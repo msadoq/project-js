a976df8eb1d59f0be579105a501c7c75e36f2ddd
Audrey LEAL <audrey.leal.external@atos.net>
2017-04-21 09:31:34 +0000

    [FT:#5828] Fix requesting data when the same view is in multiple page

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/dataManager/request.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/dataManager/request.js
@@ -3,6 +3,7 @@ import _each from 'lodash/each';
 import _map from 'lodash/map';
 import _get from 'lodash/get';
 import _isEqual from 'lodash/isEqual';
+import _intersection from 'lodash/intersection';
 import getLogger from 'common/log';
 import globalConstants from 'common/constants';
 
@@ -56,7 +57,8 @@ export function missingRemoteIds(dataMap, lastMap) {
       if (!needed.length) {
         // Check if there is not a new view requesting the data
         const lastViews = lastMap.perRemoteId[remoteId].views;
-        if (_isEqual(views, lastViews) || views.length < lastViews.length) {
+        // same views or view removed
+        if (_isEqual(views, lastViews) || _isEqual(_intersection(views, lastViews), views)) {
           return;
         }
         // If a new view has been opened, add a request on the whole expected interval
@@ -101,7 +103,6 @@ export default function request(dataMap, lastMap, send) {
 
   const n = Object.keys(dataQueries).length;
   logger.debug(`dataQueries was generated for ${n}`, dataQueries);
-
   if (dataQueries && _isObject(dataQueries) && Object.keys(dataQueries).length) {
     send(globalConstants.IPC_METHOD_TIMEBASED_QUERY, { queries: dataQueries });
   }

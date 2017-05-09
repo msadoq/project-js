f72f83eea10a1d209927e02eb706cd2164c8a494
Guillaume ARM <guillaume.arm.external@atos.net>
2017-05-09 15:52:16 +0000

    [FT:#5828] Rewrite common/fs exports  . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/common/fs.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/common/fs.js
@@ -1,60 +1,69 @@
 const fs = require('fs');
-const {
-  join,
-} = require('path');
+const { join } = require('path');
 const mkdirp = require('mkdirp');
 
-const self = {
-  resolve: (folder, relativePath) => join(folder, relativePath),
-  isExists: (path, callback) => fs.access(path, fs.constants.F_OK, err => callback(!err)),
-  isReadable: (path, callback) => fs.access(path, fs.constants.R_OK, err => callback(!err)),
-  read: (path, callback) => {
-    self.isExists(path, (exists) => {
-      if (!exists) {
-        return callback(new Error(`File '${path}' doesn't exist`));
+const resolve = (folder, relativePath) => join(folder, relativePath);
+const isExists = (path, callback) => (
+  fs.access(path, fs.constants.F_OK, err => callback(!err))
+);
+const isReadable = (path, callback) => (
+  fs.access(path, fs.constants.R_OK, err => callback(!err))
+);
+
+const read = (path, callback) => {
+  isExists(path, (exists) => {
+    if (!exists) {
+      return callback(new Error(`File '${path}' doesn't exist`));
+    }
+    return isReadable(path, (readable) => {
+      if (!readable) {
+        return callback(new Error(`File '${path}' isn't readable`));
       }
-      return self.isReadable(path, (readable) => {
-        if (!readable) {
-          return callback(new Error(`File '${path}' isn't readable`));
+      return fs.readFile(path, 'utf8', (err, content) => {
+        if (err) {
+          return callback(new Error(err));
         }
-        return fs.readFile(path, 'utf8', (err, content) => {
-          if (err) {
-            return callback(new Error(err));
-          }
-          return callback(null, content);
-        });
+        return callback(null, content);
       });
     });
-  },
-  parse: (content, callback) => {
-    try {
-      callback(null, JSON.parse(content));
-    } catch (e) {
-      callback(e);
-    }
-  },
-
-  /**
-   * Checks if folder exists and if not, creates it
-   *
-   * @param folder
-   * @param cb
-   */
-  createFolder: (folder, cb) => {
-    fs.access(folder, fs.constants.F_OK, (noAccess) => {
-      if (noAccess) {
-        mkdirp(folder, (err) => {
-          if (err) {
-            cb(new Error(`Unable to create folder ${folder} : ${err}`));
-          } else {
-            cb(null, true);
-          }
-        });
-      } else {
-        cb(null, true);
-      }
-    });
-  },
+  });
 };
 
-module.exports = self;
+const parse = (content, callback) => {
+  try {
+    callback(null, JSON.parse(content));
+  } catch (e) {
+    callback(e);
+  }
+};
+
+/**
+ * Checks if folder exists and if not, creates it
+ *
+ * @param folder
+ * @param cb
+ */
+const createFolder = (folder, cb) => {
+  isExists(folder, (exist) => {
+    if (!exist) {
+      mkdirp(folder, (err) => {
+        if (err) {
+          cb(new Error(`Unable to create folder ${folder} : ${err}`));
+        } else {
+          cb(null, true);
+        }
+      });
+    } else {
+      cb(null, true);
+    }
+  });
+};
+
+export default {
+  resolve,
+  read,
+  parse,
+  createFolder,
+  isExists,
+  isReadable,
+};

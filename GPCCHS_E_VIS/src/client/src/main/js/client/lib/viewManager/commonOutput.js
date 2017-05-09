fbd45d4d42716657ad421ad068e184923fb42d51
Guillaume ARM <guillaume.arm.external@atos.net>
2017-05-09 16:08:17 +0000

    [FT:#5828] Simplify saveViewAs function in documentManager

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/commonOutput.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/commonOutput.js
@@ -2,7 +2,14 @@ import _ from 'lodash/fp';
 import { VM_COMMON_PROPERTIES } from './constants';
 
 // used by all 'prepareViewForFile'
-export default view => ({
+const flattenConfiguration = view => ({
   ..._.pick(VM_COMMON_PROPERTIES, view),
   ...view.configuration,
 });
+
+const removeEntryPointsIds = _.update('entryPoints', _.map(_.omit('id')));
+
+export default _.pipe(
+  flattenConfiguration,
+  removeEntryPointsIds
+);

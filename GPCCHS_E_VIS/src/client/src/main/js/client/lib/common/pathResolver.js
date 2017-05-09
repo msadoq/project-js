03fc3211230d980427433ee81e99128a89828738
Guillaume ARM <guillaume.arm.external@atos.net>
2017-05-09 15:32:15 +0000

    [FT:#5828] Refacto common/pathResolver  . . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/common/pathResolver.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/common/pathResolver.js
@@ -7,33 +7,37 @@ import { isExists } from './fs';
 
 const resolveFmdPath = (path, cb) => {
   if (!_.startsWith('/', path)) {
-    return cb(new Error(`Invalid FMD path ${path}`));
+    return cb(new Error(`Invalid FMD path ${path}`), {});
   }
   const resolvedPath = join(fmd.getRootDir(), path);
   return isExists(resolvedPath, (exist) => {
     if (exist) {
-      return cb(null, resolvedPath);
+      return cb(null, { resolvedPath });
     }
-    return cb(null, path);
+    return cb(null, { resolvedPath: path });
   });
 };
+
+const resolveOid = (oId, cb) => (
+  fmd.resolveDocument(oId, (resErr, resolvedPath, properties) => {
+    if (resErr) {
+      return cb(resErr, {});
+    }
+    return resolveFmdPath(resolvedPath, (err, infoResolved) => {
+      if (err) {
+        return cb(err, {});
+      }
+      return cb(null, { ...infoResolved, properties });
+    });
+  })
+);
 
 export default ({ folder, relativePath, oId, absolutePath }, cb) => {
   if (absolutePath) {
     return cb(null, { resolvedPath: absolutePath });
   }
   if (oId) {
-    return fmd.resolveDocument(oId, (resErr, resolvedPath, properties) => {
-      if (resErr) {
-        return cb(resErr, {});
-      }
-      return resolveFmdPath(resolvedPath, (err, resolvedFmdPath) => {
-        if (err) {
-          return cb(err, {});
-        }
-        return cb(null, { resolvedPath: resolvedFmdPath, properties });
-      });
-    });
+    return resolveOid(oId, cb);
   }
   if (folder && (!startsWith('/', relativePath))) {
     return cb(null, { resolvedPath: join(folder, relativePath) });
@@ -41,10 +45,5 @@ export default ({ folder, relativePath, oId, absolutePath }, cb) => {
   if (!fmd.getRootDir()) {
     return cb(new Error(`Unable to load document ${relativePath} due to no fmd support`), {});
   }
-  return resolveFmdPath(relativePath, (err, resolvedFmdPath) => {
-    if (err) {
-      return cb(err, {});
-    }
-    return cb(null, { resolvedPath: resolvedFmdPath });
-  });
+  return resolveFmdPath(relativePath, cb);
 };

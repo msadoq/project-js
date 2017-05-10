7b2574fe461d5f4b4c5412f81bac7086164c8fc2
Damien BRUGNE <damien.brugne.external@atos.net>
2017-05-10 14:59:39 +0000

    [FT:#5828] Now transpile server with babel to allow ES6 module usage and store module loading in server process.

---
+++ GPCCHS_E_VIS/src/client/src/main/js/client/webpack/config.server.js
@@ -0,0 +1,38 @@
+import { join } from 'path';
+import webpack from 'webpack';
+import merge from 'webpack-merge';
+import baseConfig from './config.base';
+
+export default merge(baseConfig, {
+  devtool: 'source-map',
+
+  entry: ['babel-polyfill', '../../../../../server/src/main/js/server/index'],
+
+  output: {
+    path: join(__dirname, '..'),
+    filename: './server.js',
+  },
+  externals: [
+    'source-map-support',
+    'package.json',
+    'zmq',
+    'bindings',
+  ],
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.BannerPlugin(
+      'require("source-map-support").install();',
+      { raw: true, entryOnly: false }
+    ),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'), // import for bundled libs as React https://facebook.github.io/react/docs/optimizing-performance.html#use-the-production-build
+        IS_BUNDLED: JSON.stringify('on'),
+      },
+    }),
+  ],
+
+  target: 'node',
+
+});

a57b2269b6506bbe820ad090f6476e000d3c0e2f
Guillaume ARM <guillaume.arm.external@atos.net>
2017-06-13 16:38:32 +0000

    [FT:#6670] Fix packaging stubProcess . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/scripts/start.sh
+++ GPCCHS_E_VIS/src/client/src/main/js/client/scripts/start.sh
@@ -10,5 +10,8 @@ cp config.local.json $APP_FOLDER
 # Bootstrap a stub
 mkdir $APP_FOLDER/lib && cp -R ./lib/stubProcess $APP_FOLDER/lib
 
+# Because stub need constants.js, copy it
+cp ./lib/constants.js $APP_FOLDER/lib
+
 # Start packaged vima
 ./dist/lpisis_gpcchs_e_clt-linux-x64/lpisis_gpcchs_e_clt

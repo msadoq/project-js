ff4d4e3ee075f6df56e6c083301b59660d4add47
Guillaume ARM <guillaume.arm.external@atos.net>
2017-06-16 13:00:25 +0000

    [FT:#6670] Write reloadView, openPage and openBlankWorkspace tests

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/documentManager/actions.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/documentManager/actions.spec.js
@@ -1,6 +1,6 @@
+import _ from 'lodash/fp';
 import sinon from 'sinon';
-import * as types from '../store/types';
-import { mockStore } from '../common/jest';
+import { mockStore, freezeMe } from '../common/jest';
 import readView from './readView';
 import * as readPageApi from './readPage';
 import * as actions from './actions';
@@ -10,11 +10,56 @@ describe('documentManager:actions', () => {
   afterEach(() => {
     stub.restore();
   });
+
+  describe('reloadView', () => {
+    test('reload view', () => {
+      const store = mockStore();
+      stub = sinon.stub(readView, 'simpleReadView').callsFake((viewInfo, cb) => {
+        cb(null, { some: 'properties' });
+      });
+      store.dispatch(actions.reloadView('myViewId', '/absolute/path'));
+      expect(store.getActions()).toMatchObject([
+        {
+          type: 'WS_VIEW_RELOAD',
+          payload: { viewId: 'myViewId', view: { some: 'properties', uuid: 'myViewId' } },
+        },
+        {
+          type: 'WS_MESSAGE_ADD',
+          payload: {
+            containerId: 'myViewId',
+            type: 'success',
+            messages: [{ content: 'View reloaded' }],
+          },
+        },
+      ]);
+    });
+    test('invalid view file', () => {
+      const store = mockStore();
+      stub = sinon.stub(readView, 'simpleReadView').callsFake((viewInfo, cb) => {
+        cb(new Error());
+      });
+      store.dispatch(actions.reloadView('myViewId', '/absolute/path'));
+      expect(store.getActions()).toMatchObject([
+        {
+          type: 'WS_MESSAGE_ADD',
+          payload: {
+            containerId: 'myViewId',
+            type: 'danger',
+            messages: [
+              {
+                content: 'Invalid View file selected',
+              },
+            ],
+          },
+        },
+      ]);
+    });
+  });
+
   describe('openView', () => {
     test('dispatches a message in case of error', () => {
       const store = mockStore();
       stub = sinon.stub(readView, 'simpleReadView').callsFake((viewInfo, cb) => {
-        expect(viewInfo).toEqual('viewInfo');
         cb(null, { error: 'Error' });
       });
       store.dispatch(actions.openView('viewInfo'));
@@ -32,13 +77,12 @@ describe('documentManager:actions', () => {
     test('dispatches a WS_VIEW_OPEN when view is loaded', () => {
       const store = mockStore();
       stub = sinon.stub(readView, 'simpleReadView').callsFake((viewInfo, cb) => {
-        expect(viewInfo).toEqual('viewInfo');
         cb(null, { value: { title: 'my view' } });
       });
       store.dispatch(actions.openView('viewInfo', 'myPageId'));
       expect(store.getActions()).toEqual([
         {
-          type: types.WS_VIEW_OPEN,
+          type: 'WS_VIEW_OPEN',
           payload: {
             pageId: 'myPageId',
             view: { title: 'my view' },
@@ -47,21 +91,99 @@ describe('documentManager:actions', () => {
       ]);
     });
   });
+
   describe('openPage', () => {
     test('dispatches a global error message in case of error', () => {
       const store = mockStore();
       stub = sinon.stub(readPageApi, 'readPageAndViews').callsFake((pageInfo, cb) => {
-        expect(pageInfo).toEqual('page_info');
         cb(null, { views: [{ error: 'Error view' }], pages: [{ error: 'Error page' }] });
       });
       store.dispatch(actions.openPage('page_info'));
       expect(store.getActions()).toMatchObject([
         {
-          type: types.WS_MESSAGE_ADD,
+          type: 'WS_MESSAGE_ADD',
           payload: {
             containerId: 'global',
             type: 'danger',
-            messages: [{ content: 'Error view' }, { content: 'Error page' }],
+            messages: [
+              { content: 'Error view' },
+              { content: 'Error page' },
+            ],
+          },
+        },
+      ]);
+    });
+
+    test('opens Page', (done) => {
+      const store = mockStore(freezeMe({}));
+      stub = sinon.stub(readPageApi, 'readPageAndViews').callsFake((pageInfo, cb) => {
+        cb(null, {
+          views: [{ value: { type: 'TextView' } }],
+          pages: [{ value: { windowId: 'windowId', type: 'Page' } }],
+        });
+      });
+      store.dispatch(actions.openPage('page_info'));
+      expect(store.getActions()).toMatchSnapshot();
+      done();
+    });
+  });
+
+  describe('openPageOrView', () => {
+    test('');
+  });
+
+  describe('openWorkspace', () => {
+    test('open workspace');
+    test('open workspace with errors');
+    test('do not open workspace because error');
+  });
+
+  describe('openBlankWorkspace', () => {
+    test('close current workspace, then open a freshly new created workspace', () => {
+      const store = mockStore();
+      stub = { restore: _.noop };
+      store.dispatch(actions.openBlankWorkspace());
+      const workspaceOpenPayload = store.getActions()[1].payload;
+      const firstTimebar = workspaceOpenPayload.timebars[0];
+      expect(firstTimebar.visuWindow).toHaveKeys(['lower', 'upper', 'current']);
+      expect(firstTimebar.slideWindow).toHaveKeys(['lower', 'upper']);
+      expect(firstTimebar).toHaveKeys(['rulerStart']);
+      expect(workspaceOpenPayload.windows[0].pages[0]).toBeAnUuid();
+      expect(workspaceOpenPayload.windows[0].uuid).toBeAnUuid();
+      expect(workspaceOpenPayload.timebars[0].uuid).toBeAnUuid();
+      expect(workspaceOpenPayload.pages[0].uuid).toBeAnUuid();
+      expect(workspaceOpenPayload.pages[0].timebarUuid).toBeAnUuid();
+      expect(store.getActions()).toMatchObject([
+        { type: 'HSC_CLOSE_WORKSPACE', payload: {} },
+        {
+          type: 'WS_WORKSPACE_OPEN',
+          payload: {
+            windows: [
+              {
+                title: 'Unknown',
+                type: 'documentWindow',
+                geometry: { w: 1200, h: 900, x: 10, y: 10 },
+              },
+            ],
+            timebars: [
+              {
+                type: 'timeBarConfiguration',
+                id: 'TB1',
+                mode: 'Normal',
+                rulerResolution: 11250,
+                speed: 1,
+                offsetFromUTC: 0,
+                timelines: [],
+              },
+            ],
+            pages: [
+              {
+                type: 'Page',
+                title: 'Unknown',
+                hideBorders: false,
+                timebarId: 'TB1',
+              },
+            ],
           },
         },
       ]);

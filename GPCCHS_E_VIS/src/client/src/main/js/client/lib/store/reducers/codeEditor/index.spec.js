598e3d51cb15704eabb67be4a16af5687e6e0cec
Guillaume ARM <guillaume.arm.external@atos.net>
2017-06-27 16:22:21 +0000

    [FT:#6700] Fix codeEditor tests (reducer) .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/codeEditor/index.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/codeEditor/index.spec.js
@@ -3,47 +3,47 @@ import * as actions from '../../actions/editor';
 import * as types from '../../types';
 
 const state = {
-  textViewId: 'test',
+  viewId: 'test',
 };
 
 describe('store:editor:reducer', () => {
   // TODO abesson add test should preserve state if unknown action dispatched
   test('should returns initial state', () => {
     const newState = reducer(undefined, {});
-    expect(newState).toHaveProperty('textViewId', null);
+    expect(newState).toHaveProperty('viewId', null);
   });
   test('should update viewId', () => {
-    expect(reducer(undefined, actions.openHtmlEditor('test'))
-    .textViewId).toBe('test');
+    expect(reducer(undefined, actions.openCodeEditor('test'))
+    .viewId).toBe('test');
   });
   test('should reset viewId when close editor', () => {
-    const newState = reducer(undefined, actions.closeHtmlEditor());
-    expect(newState).toHaveProperty('textViewId', null);
+    const newState = reducer(undefined, actions.closeCodeEditor());
+    expect(newState).toHaveProperty('viewId', null);
   });
   test('should reset viewId when delete associate view', () => {
     const newState = reducer(state, {
       type: types.WS_VIEW_CLOSE,
       payload: { viewId: 'test' },
     });
-    expect(newState).toHaveProperty('textViewId', null);
+    expect(newState).toHaveProperty('viewId', null);
   });
   test('should does nothing', () => {
     const newState = reducer(state, {
       type: types.WS_VIEW_CLOSE,
       payload: { viewId: 'test2' },
     });
-    expect(newState).toHaveProperty('textViewId', 'test');
+    expect(newState).toHaveProperty('viewId', 'test');
   });
 });
 
 describe('store:editor:selector', () => {
   test('should return false', () => {
-    expect(getIsCodeEditorOpened({ editor: { textViewId: null } })).toEqual(false);
+    expect(getIsCodeEditorOpened({ codeEditor: { viewId: null } })).toEqual(false);
   });
   test('should return false', () => {
     expect(getIsCodeEditorOpened({})).toEqual(false);
   });
   test('should return true', () => {
-    expect(getIsCodeEditorOpened({ editor: { textViewId: 'idView' } })).toEqual(true);
+    expect(getIsCodeEditorOpened({ codeEditor: { viewId: 'idView' } })).toEqual(true);
   });
 });

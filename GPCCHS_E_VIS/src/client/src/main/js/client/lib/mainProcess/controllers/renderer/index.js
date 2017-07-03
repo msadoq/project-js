43d5f5f091e7e6207e35d5b932201acba5fde4c3
Guillaume ARM <guillaume.arm.external@atos.net>
2017-07-03 16:44:52 +0000

    [FT:#6069] Remove unused onSavePage ipc renderer to mainProcess controllers

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/mainProcess/controllers/renderer/index.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/mainProcess/controllers/renderer/index.js
@@ -9,7 +9,6 @@ import onGetSessionTime from './onGetSessionTime';
 import onReloadSessions from './onReloadSessions';
 import onReloadView from './onReloadView';
 import onSaveView from './onSaveView';
-import onSavePage from './onSavePage';
 import onCreateModel from './onCreateModel';
 import onServerDebug from './onServerDebug';
 import onOpenView from './onOpenView';
@@ -31,7 +30,6 @@ const controller = {
   [constants.IPC_METHOD_RELOAD_SESSIONS]: onReloadSessions,
   [constants.IPC_METHOD_RELOAD_VIEW]: onReloadView,
   [constants.IPC_METHOD_SAVE_VIEW]: onSaveView,
-  [constants.IPC_METHOD_SAVE_PAGE]: onSavePage,
   [constants.IPC_METHOD_CREATE_MODEL]: onCreateModel,
   [constants.IPC_METHOD_SERVER_DEBUG]: onServerDebug,
   [constants.IPC_METHOD_OPEN_VIEW]: onOpenView,

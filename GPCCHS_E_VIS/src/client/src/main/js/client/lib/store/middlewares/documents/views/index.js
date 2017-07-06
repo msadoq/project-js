62d7df9f5c64839dfa712c506dce45f76e624661
Guillaume ARM <guillaume.arm.external@atos.net>
2017-07-06 11:23:19 +0000

    [FT:#6069] Lint documents middleware . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/documents/views/index.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/documents/views/index.js
@@ -1,8 +1,6 @@
-import * as types from '../../../types';
-import { openDialog } from '../../../actions/ui';
+// import * as types from '../../../types';
+// import { openDialog } from '../../../actions/ui';
 
-export default function createViewsMiddleware(documentManager) {
-  return ({ dispatch }) => next => (action) => {
-    return next(action);
-  };
-}
+const createViewsMiddleware = () => () => next => action => next(action);
+
+export default createViewsMiddleware;
--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/documents/workspace/index.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/documents/workspace/index.js
@@ -1,8 +1,6 @@
-import * as types from '../../../types';
-import { openDialog } from '../../../actions/ui';
+// import * as types from '../../../types';
+// import { openDialog } from '../../../actions/ui';
 
-export default function createWorkspacesMiddleware(documentManager) {
-  return ({ dispatch }) => next => (action) => {
-    return next(action);
-  };
-}
+const createWorkspaceMiddleware = () => () => next => action => next(action);
+
+export default createWorkspaceMiddleware;

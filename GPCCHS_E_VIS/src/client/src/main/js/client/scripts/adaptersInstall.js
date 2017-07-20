f9a1f893eef679bc12cc06640277eaf41676a65c
Paul GAUCHER <paul.gaucher.external@atos.net>
2017-07-20 12:51:57 +0000

    [FT:#7299] Fix postinstall script adapters : hardcoded paths (relatives)

--- GPCCHS_E_VIS/src/client/src/main/js/client/scripts/adaptersInstall.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/scripts/adaptersInstall.js
@@ -1,13 +1,9 @@
 const { join } = require('path');
-const fs = require('fs');
-const _each = require('lodash/each');
 const { execSync } = require('child_process');
 
-const readConf = () => JSON.parse(fs.readFileSync('./config.default.json', 'utf8'));
+const ADAPTERS_PATH = '../../../../../adapters/';
+const ISIS_NS = 'isis';
+const DC_NS = 'dc';
 
-const conf = readConf();
-
-_each(conf.MESSAGES_NAMESPACES, (adapter) => {
-  const path = join(adapter.path, adapter.ns);
-  execSync('npm i', { stdio: [0, 1, 2], cwd: path });
-});
+execSync('npm i', { stdio: [0, 1, 2], cwd: join(ADAPTERS_PATH, ISIS_NS) });
+execSync('npm i', { stdio: [0, 1, 2], cwd: join(ADAPTERS_PATH, DC_NS) });

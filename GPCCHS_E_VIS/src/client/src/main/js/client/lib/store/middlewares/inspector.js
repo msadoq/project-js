365abc729869c32b8abf1ae9ec28b2153137effb
Guillaume ARM <guillaume.arm.external@atos.net>
2017-07-21 15:28:36 +0000

    [FT:#6069] Add inspector middleware in serverProcess store

---
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/inspector.js
@@ -0,0 +1,60 @@
+import getLogger from '../../common/logManager';
+import { getTelemetryStaticElements } from '../../rtdManager';
+import prepareDataToTree from '../../rtdManager/prepareDataToTree';
+import * as types from '../types';
+import { add } from '../actions/messages';
+import { minimizeExplorer, focusTabInExplorer } from '../actions/pages';
+import {
+  setInspectorGeneralData,
+  deleteInspectorGeneralData,
+  isInspectorDisplayingTM,
+  setInspectorStaticData,
+  isInspectorStaticDataLoading,
+} from '../actions/inspector';
+import { getInspectorEpId } from '../reducers/inspector';
+
+const logger = getLogger('main:controllers:renderer:onOpenInspector');
+
+export default () => ({ getState, dispatch }) => next => (action) => {
+  const nextAction = next(action);
+  if (action.type === types.HSC_ASK_OPEN_INSPECTOR) {
+    const {
+      pageId, viewId, type: viewType,
+      options: { epName, epId, dataId, field },
+    } = action.payload;
+    const { parameterName, catalog, sessionId, domainId } = dataId;
+
+    dispatch(minimizeExplorer(pageId, false));
+    dispatch(focusTabInExplorer(pageId, 'inspector'));
+
+    if (getInspectorEpId(getState()) === epId) {
+      return nextAction;
+    }
+    dispatch(setInspectorStaticData(null));
+    dispatch(setInspectorGeneralData(viewId, viewType, epId, epName, dataId, field));
+
+    if (catalog !== 'Reporting') {
+      return nextAction;
+    }
+    dispatch(isInspectorStaticDataLoading(true));
+    dispatch(isInspectorDisplayingTM(true));
+
+    logger.info(`request ${parameterName} for session ${sessionId} and domain ${domainId}`);
+
+    getTelemetryStaticElements({ sessionId, domainId }, parameterName, (err, data) => {
+      if (err || !data) {
+        dispatch(isInspectorStaticDataLoading(false));
+        dispatch(deleteInspectorGeneralData());
+        dispatch(add(
+          'global',
+          'warning',
+          `Parameter ${parameterName} was not found in catalog Reporting for session ${sessionId} and domain ${domainId}`
+        ));
+        return;
+      }
+      const staticData = prepareDataToTree(data);
+      dispatch(setInspectorStaticData(staticData));
+    });
+  }
+  return nextAction;
+};

84d169a863ada74e19649b996ba9100f668a4c6b
Guillaume ARM <guillaume.arm.external@atos.net>
2017-07-21 12:46:48 +0000

    [FT:#6069] Clean windowProcess View Links .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/View/Links.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/View/Links.js
@@ -2,7 +2,6 @@ import React, { PropTypes, PureComponent } from 'react';
 import { Glyphicon, Button, Col, Row } from 'react-bootstrap';
 import classnames from 'classnames';
 import styles from './Links.css';
-// import { resolveFmdPath } from '../../common/pathResolver';
 
 export default class Links extends PureComponent {
   static propTypes = {
@@ -22,55 +21,7 @@ export default class Links extends PureComponent {
 
   onClick = (linkId) => {
     this.props.askOpenLink(this.props.viewId, linkId);
-    // Get link path
-    // const path = this.props.links[key].path;
-    // const resolvedPath = {};
-    // resolve link: abs path, fmd path or OID
-    // if (path.startsWith('/')) {
-    //   resolveFmdPath(path, (err, resPath) => {
-    //     if (!err) {
-    //       resolvedPath.absolutePath = resPath.resolvedPath;
-    //       this.openFile(resolvedPath);
-    //     }
-    //   });
-    // } else {
-    //   resolveOid(path, (err, resPath) => {
-    //     if (!err) {
-    //       resolvedPath.absolutePath = resPath.resolvedPath;
-    //       this.openFile(resolvedPath);
-    //     }
-    //   });
-    // }
   }
-
-  // openFile = (resolvedPath) => {
-  //   const { pages, views, focusPage, focusView, pageId, windowId, addMessage, askOpenLink }
-  //     = this.props;
-  //
-  //   if (!resolvedPath.absolutePath) {
-  //     addMessage('global', 'danger', 'Unable to open link');
-  //     return;
-  //   }
-  //   const linkedPage = _find(pages, resolvedPath);
-  //   // Link is a page already opened
-  //   if (linkedPage) {
-  //     if (linkedPage === pageId) {
-  //       return;
-  //     }
-  //     // setFocus on the page linked
-  //     focusPage(linkedPage.uuid);
-  //     return;
-  //   }
-  //   const linkedView = _find(views, resolvedPath);
-  //   // link is a view already opened
-  //   if (linkedView) {
-  //     // setFocus on the page which contains the view linked
-  //     focusView(linkedView.uuid);
-  //     return;
-  //   }
-  //   // Open the link
-  //   askOpenLink(windowId, resolvedPath.absolutePath);
-  // }
 
   render() {
     const { links, show, removeLink } = this.props;

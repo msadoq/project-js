9e09135e293676f6e95a012bfba39d157d560f66
Guillaume ARM <guillaume.arm.external@atos.net>
2017-07-24 11:49:48 +0000

    [FT:#7145] Write documents/onReloadView middleware unit tests

---
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/documents/views/onReloadView.spec.js
@@ -0,0 +1,47 @@
+import _ from 'lodash/fp';
+import configureMockStore from 'redux-mock-store';
+import onReloadView from './onReloadView';
+import * as types from '../../../types';
+
+const documentManager = {
+  reloadView: viewId => ({
+    type: 'RELOAD_VIEW',
+    payload: { viewId },
+  }),
+};
+
+const askReloadView = viewId => ({
+  type: types.WS_ASK_RELOAD_VIEW,
+  payload: { viewId },
+});
+
+const mockStore = configureMockStore([onReloadView(documentManager)]);
+
+describe('store:serverProcess:middlewares:documents:onSaveViewAsModel', () => {
+  const store = mockStore({
+    windows: { w1: { focusPage: 'p1', pages: ['p1'] } },
+    pages: { p1: { uuid: 'p1', views: ['v1', 'v2'] } },
+    views: { v1: { uuid: 'v1' }, v2: { uuid: 'v2', isModified: true } },
+  });
+
+  beforeEach(() => {
+    store.clearActions();
+  });
+
+  test('reload view', () => {
+    store.dispatch(askReloadView('v1'));
+    expect(store.getActions()).toMatchSnapshot();
+  });
+  test('display modal, then reload view', () => {
+    store.dispatch(askReloadView('v2'));
+    const modalId = _.last(store.getActions()).payload.props.modalId;
+    store.dispatch({ type: types.WS_MODAL_CLOSE, payload: { choice: 'reload', props: { modalId } } });
+    expect(store.getActions()).toMatchSnapshot();
+  });
+  test('display modal, then do not reload view (cancel)', () => {
+    store.dispatch(askReloadView('v2'));
+    const modalId = _.last(store.getActions()).payload.props.modalId;
+    store.dispatch({ type: types.WS_MODAL_CLOSE, payload: { props: { modalId } } });
+    expect(store.getActions()).toMatchSnapshot();
+  });
+});

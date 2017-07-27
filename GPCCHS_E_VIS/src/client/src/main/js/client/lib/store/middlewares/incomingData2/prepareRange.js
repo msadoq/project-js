7d9c61fbe507eba5aa9a91b8537d8c2802b93d7d
Paul GAUCHER <paul.gaucher.external@atos.net>
2017-07-27 13:22:30 +0000

    [FT:#6700] Prepare range code implementation and its test

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/incomingData2/prepareRange.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/incomingData2/prepareRange.js
@@ -1,37 +1,73 @@
+import _isEmpty from 'lodash/isEmpty';
+import _isBuffer from 'lodash/isBuffer';
 import * as types from '../../types';
-// import { addRecords } from '../../../serverProcess/models/lokiKnownRangesData';
-// import { injectData } from '../../actions/incomingData';
+import { injectNewData } from '../../actions/incomingData';
+import { decode, getType } from '../../../utils/adapters';
+import dataMapGenerator from '../../../dataManager/map';
+import { isTimestampInLastInterval } from '../../../dataManager/mapSelector';
 
-const prepareRange = () => ({ dispatch }) => next => (action) => { // eslint-disable-line
+const { dumpBuffer } = require('../../../utils/adapters');
+
+const prepareRange = lokiManager => ({ dispatch, getState }) => next => (action) => { // eslint-disable-line
   if (action.type === types.INCOMING_RANGE_DATA) {
     console.log('[PrepareRangeMiddleware] ON_INCOMING_RANGE_DATA action');
-    // const tbdId = action.payload.tbdId;
-    // const peers = action.payload.peers;
+    const tbdId = action.payload.tbdId;
+    const dataId = action.payload.dataId;
+    const peers = action.payload.peers;
+    const payloadProtobufType = getType(dataId.comObject);
 
-    // DECODE RANGE
-    // const decodedData = proto.decode(peers)
+    if (typeof payloadProtobufType === 'undefined') {
+      console.log('protobufType unknwon');
+      return next(action);
+    }
 
-    // ADD DATA IN LOKIs
-    // addRecords(tbdId, peersDecodedInterval)
+    const payloadsJson = {};
+    const dataMap = dataMapGenerator(getState());
+    if (dataMap.expectedRangeIntervals[tbdId]) {
+      while (peers.length) {
+        // pop the first two buffers from list
+        const payloadBuffer = peers.splice(0, 2);
+        // robustness code, LPISIS could send empty ZeroMQ frame
+        if (!_isBuffer(payloadBuffer[0]) || !_isBuffer(payloadBuffer[1])) {
+          // loggerData.warn(`received an empty ZeroMQ frame from DC for ${remoteId}`);
+          // eslint-disable-next-line no-continue, "DV6 TBC_CNES LPISIS use continue to preserve readability and avoid long block in an if condition"
+          continue;
+        }
 
-    // EXTRACT DATA TO DISPLAY
-    // const displayedData = extractDisplayedData(tbdId, intervals, dataMap);
-    // dispatch(injectData(tbdId, displayedData));
+        const timestamp = decode('dc.dataControllerUtils.Timestamp', payloadBuffer[0]).ms;
+        const payload = decode(payloadProtobufType, payloadBuffer[1]);
+        lokiManager.addRecord(tbdId, { timestamp, payload });
+        // dump: if activated, save a file per timestamp with binary payload
+        dumpBuffer(dataId, timestamp, payloadBuffer[1]);
+
+        // queue new data in spool
+        payloadsJson[timestamp] = payload;
+      }
+    } else {
+      while (peers.length) {
+        // pop the first two buffers from list
+        const payloadBuffer = peers.splice(0, 2);
+
+        // robustness code, LPISIS could send empty ZeroMQ frame
+        if (!_isBuffer(payloadBuffer[0]) || !_isBuffer(payloadBuffer[1])) {
+          // loggerData.warn(`received an empty ZeroMQ frame from DC for ${remoteId}`);
+          // eslint-disable-next-line no-continue, "DV6 TBC_CNES LPISIS use continue to preserve readability and avoid long block in an if condition"
+          continue;
+        }
+        const timestamp = decode('dc.dataControllerUtils.Timestamp', payloadBuffer[0]).ms;
+        console.log(dataMap.expectedLastIntervals);
+        if (isTimestampInLastInterval(dataMap, { tbdId, timestamp } )) {
+          const payload = decode(payloadProtobufType, payloadBuffer[1]);
+          payloadsJson[timestamp] = payload;
+        }
+
+        // dump: if activated, save a file per timestamp with binary payload
+        dumpBuffer(dataId, timestamp, payloadBuffer[1]);
+      }
+    }
+    // If data nedds to be send to reducers, dispatch action
+    if (!_isEmpty(payloadsJson)) dispatch(injectNewData(tbdId, payloadsJson));
   }
-
-  /**
-   * Get currently displayed data on the VIMA from the decoded data
-   * @param string tbdId
-   * @param Array<interval> intervals List of intervals received from the TBD
-   * @param Object dataMap The current DataMap
-   */
-  const extractDisplayedData = (tbdId, intervals, dataMap) => {
-    const displayedData = 'displayed data';
-    // TODO
-    // Algorithm to get a subset of intervals from the comparison of decodedData with the dataMap
-
-    return displayedData;
-  };
   return next(action);
 };
 

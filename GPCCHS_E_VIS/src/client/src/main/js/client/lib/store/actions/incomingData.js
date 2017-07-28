6a79a3e3089f977971b24ad23ea2d015f76827d8
Paul GAUCHER <paul.gaucher.external@atos.net>
2017-07-28 08:43:40 +0000

    [FT:#6700] Implements prepare LAst middleware and its test

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/actions/incomingData.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/actions/incomingData.js
@@ -2,8 +2,8 @@ import * as types from '../types';
 import simple from '../helpers/simpleActionCreator';
 
 // Actions dispatched from archiveController to the prepareData middlewares
-export const incomingLast = simple(types.INCOMING_LAST_DATA, 'tbdId', 'peers');
-export const incomingRange = simple(types.INCOMING_RANGE_DATA, 'tbdId', 'peers');
+export const incomingLast = simple(types.INCOMING_LAST_DATA, 'tbdId', 'peers', 'dataId');
+export const incomingRange = simple(types.INCOMING_RANGE_DATA, 'tbdId', 'peers', 'dataId');
 export const incomingPubSub = simple(types.INCOMING_PUBSUB_DATA, 'dataId', 'peers');
 
 // Action dispatch by the prepareData middlewares
--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/incomingData2/prepareLast.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/incomingData2/prepareLast.js
@@ -1,16 +1,37 @@
+import _isEmpty from 'lodash/isEmpty';
+import _isBuffer from 'lodash/isBuffer';
 import * as types from '../../types';
-// import { injectData } from '../../actions/incomingData';
+import { injectNewData } from '../../actions/incomingData';
+import { decode, getType } from '../../../utils/adapters';
 
-const prepareLast = () => ({ dispatch }) => next => (action) => { // eslint-disable-line
+const { dumpBuffer } = require('../../../serverProcess/utils/dumpBuffer');
+
+const prepareLast = () => ({ dispatch }) => next => (action) => {
   if (action.type === types.INCOMING_LAST_DATA) {
     console.log('[PrepareLastMiddleware] ON_INCOMING_LAST_DATA action');
-    // const tbdId = action.payload.tbdId;
-    // const peers = action.payload.peers;
+    const tbdId = action.payload.tbdId;
+    const dataId = action.payload.dataId;
+    const peers = action.payload.peers;
 
-    // const timestamp = decoded from peers;
-    // const payload = decoded from peers;
+    const payloadProtobufType = getType(dataId.comObject);
+    const payloadsJson = {};
+    let index = 0;
+    while (index + 1 < peers.length) {
+      // pop the first two buffers from list
 
-    // dispatch(injectData(tbdId, {[timestamp] : payload}))
+      // robustness code, LPISIS could send empty ZeroMQ frame
+      if ( _isBuffer(peers[index]) && _isBuffer(peers[index + 1])) {
+        const timestamp = decode('dc.dataControllerUtils.Timestamp', peers[index]).ms;
+        const payload = decode(payloadProtobufType, peers[index + 1]);
+        // dump: if activated, save a file per timestamp with binary payload
+        dumpBuffer(dataId, timestamp, peers[index + 1]);
+
+        // queue new data in spool
+        payloadsJson[timestamp] = payload;
+      }
+      index += 2;
+    }
+    if (!_isEmpty(payloadsJson)) dispatch(injectNewData(tbdId, payloadsJson));
   }
   return next(action);
 };

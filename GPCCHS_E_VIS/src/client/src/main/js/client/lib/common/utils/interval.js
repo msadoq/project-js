93b39697b2413601adb43c776e9641ca21c64d1f
Guillaume ARM <guillaume.arm.external@atos.net>
2017-08-02 15:47:11 +0000

    [FT:#7145] Compute delta time in createInterval utils

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/common/utils/interval.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/common/utils/interval.js
@@ -3,13 +3,13 @@ import _ from 'lodash/fp';
 const createInterval = (ms, f = _.noop) => {
   let id;
   const clear = () => clearTimeout(id);
-  let timer = (cb) => {
+  let timer = (cb, delta = 0) => {
     const tick = Date.now();
     id = setTimeout(() => {
-      const delta = Date.now() - tick;
-      cb(delta);
-      timer(cb);
-    }, ms);
+      const waitedMs = Date.now() - tick;
+      cb(waitedMs);
+      timer(cb, Math.max(0, waitedMs - ms));
+    }, ms - delta);
   };
   timer(f);
   return {

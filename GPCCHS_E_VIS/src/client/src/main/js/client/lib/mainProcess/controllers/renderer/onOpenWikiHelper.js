1c0ee8efd749bc5c3ae73193ab2ede07d1628e85
Guillaume ARM <guillaume.arm.external@atos.net>
2017-08-03 12:43:34 +0000

    [FT:#7145] Catch errors when open wiki helper

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/mainProcess/controllers/renderer/onOpenWikiHelper.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/mainProcess/controllers/renderer/onOpenWikiHelper.js
@@ -6,6 +6,7 @@ import { add as addMessage } from '../../../store/actions/messages';
 import { get } from '../../../common/configurationManager';
 
 const addGlobalWarning = msg => addMessage('global', 'warning', msg);
+const addGlobalError = msg => addMessage('global', 'error', msg);
 
 const isUrl = _.anypass([
   _.startsWith('http://'),
@@ -15,9 +16,11 @@ const isUrl = _.anypass([
 export default function () {
   const url = get('USER_MANUAL_URL');
   if (!url) {
-    getStore().dispatch(addGlobalWarning('no USER_MANUAL_URL provided'));
+    getStore().dispatch(addGlobalWarning('Wiki helper : no USER_MANUAL_URL provided'));
     return;
   }
   const fullUrl = isUrl(url) ? url : `http://${url}`;
-  open(fullUrl);
+  open(fullUrl).catch((e) => {
+    getStore().dispatch(addGlobalError(`Wiki helper : ${e.message}`));
+  });
 }

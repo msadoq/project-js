f5ed070bedaa518de6e3bbe1f25c5a3f8f98898b
Alexis BESSON <alexis.besson@external.atos.net>
2017-08-04 08:00:12 +0000

    [HL] add HSS & Main process bench

--- GPCCHS_E_VIS/src/client/src/main/js/client/benchmarks/bench_main.sh
+++ GPCCHS_E_VIS/src/client/src/main/js/client/benchmarks/bench_main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 function getRendererPID {
-  local PID="`pgrep gpcchs_renderer | tr -d '\n'`"
+  local PID="`pgrep gpcchs_main | tr -d '\n'`"
   if [ -z $PID ]; then
-    echo "ERROR: Aucun process du nom de gpcchs_renderer est en cours d'execution"
+    echo "ERROR: Aucun process du nom de gpcchs_main est en cours d'execution"
     exit 1
   else
     echo $PID
@@ -12,21 +12,21 @@ function getRendererPID {
 SLEEP_DELAY=5
 CPT=0
 ZERO=0
-echo 'window.benchRendererData = [];' > bench_renderer_data.js
+echo 'window.benchMainData = [];' > bench_main_data.js
 RENDERER_PID="$(getRendererPID)"
 TOP=`top -b -n 1 -p $RENDERER_PID | sed -n '8p' | awk '{print $6}'`
 CPU=`top -b -n 1 -p $RENDERER_PID | sed -n '8p' | awk '{print $9}' | xargs printf "%.*f\n" 0`
 CONTENT=''
 
 while [ -n "$TOP" ]; do
-  CONTENT=`cat bench_renderer_data.js`
+  CONTENT=`cat bench_main_data.js`
   CONTENT=${CONTENT::-2}
   if [ $CPT -eq $ZERO ]; then
     CONTENT="$CONTENT[$CPT,$TOP,$CPU]];"
   else
     CONTENT="$CONTENT,[$(($CPT*$SLEEP_DELAY)),$TOP,$CPU]];"
   fi
-  echo $CONTENT > bench_renderer_data.js
+  echo $CONTENT > bench_main_data.js
   sleep $SLEEP_DELAY
   CPT=$(($CPT+1))
   TOP=`top -b -n 1 -p $RENDERER_PID | sed -n '8p' | awk '{print $6}'`

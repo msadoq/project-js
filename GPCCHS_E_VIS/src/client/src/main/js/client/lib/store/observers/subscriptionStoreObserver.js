b94c8b17953f089880f6a49be1604466fc8eae4e
Paul GAUCHER <paul.gaucher.external@atos.net>
2017-08-24 08:01:09 +0000

    [FT:#7578] Update subscription store observer . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/observers/subscriptionStoreObserver.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/observers/subscriptionStoreObserver.js
@@ -2,6 +2,7 @@ import _differenceWith from 'lodash/differenceWith';
 import { getWindowsOpened, getIsWorkspaceOpening } from '../../store/reducers/hsc';
 import { getTbdIdsAndDataIdList } from '../reducers/knownRanges';
 import { dc } from '../../serverProcess/ipc';
+import { getCurrentDisplayedLastTbdId } from '../../dataManager/mapSelector';
 
 const { requestSubscriptionAdd, requestSubscriptionDelete } = dc;
 
@@ -11,8 +12,8 @@ const { requestSubscriptionAdd, requestSubscriptionDelete } = dc;
  * @return {function} The new state.
  */
 export default function makeSubscriptionStoreObserver(store) {
+  let previousDisplayedTbdId;
   let previousKnownRanges;
-
   const compareTbdId = (obj1, obj2) => obj1.tbdId === obj2.tbdId;
 
   return function subscriptionStoreObserver() {
@@ -22,18 +23,34 @@ export default function makeSubscriptionStoreObserver(store) {
     if (getIsWorkspaceOpening(state) === true && getWindowsOpened(state) === false) {
       return;
     }
+    const displayedLastTbdId = getCurrentDisplayedLastTbdId(state);
     const allKnownRanges = getTbdIdsAndDataIdList(state);
-    const toSubscribe = _differenceWith(allKnownRanges, previousKnownRanges, compareTbdId);
-    const toUnsubscribe = _differenceWith(previousKnownRanges, allKnownRanges, compareTbdId);
-    previousKnownRanges = allKnownRanges;
 
-    for (let i = 0; i < toSubscribe.length; i += 1){
-      const { tbdId, dataId } = toSubscribe[i];
+    const toSubscribeLast = _differenceWith(displayedLastTbdId, previousDisplayedTbdId, compareTbdId);
+    const toUnsubscribeLast = _differenceWith(previousDisplayedTbdId, displayedLastTbdId, compareTbdId);
+
+    const toSubscribeRange = _differenceWith(allKnownRanges, previousKnownRanges, compareTbdId);
+    const toUnsubscribeRange = _differenceWith(previousKnownRanges, allKnownRanges, compareTbdId);
+
+    previousDisplayedTbdId = displayedLastTbdId;
+
+    for (let i = 0; i < toSubscribeLast.length; i += 1){
+      const { tbdId, dataId } = toSubscribeLast[i];
       requestSubscriptionAdd(tbdId, dataId);
     }
 
-    for (let j = 0; j < toUnsubscribe.length; j += 1){
-      const { tbdId, dataId } = toUnsubscribe[j];
+    for (let j = 0; j < toUnsubscribeLast.length; j += 1) {
+      const { tbdId, dataId } = toUnsubscribeLast[j];
+      requestSubscriptionDelete(tbdId, dataId);
+    }
+
+    for (let i = 0; i < toSubscribeRange.length; i += 1){
+      const { tbdId, dataId } = toSubscribeRange[i];
+      requestSubscriptionAdd(tbdId, dataId);
+    }
+
+    for (let j = 0; j < toUnsubscribeRange.length; j += 1) {
+      const { tbdId, dataId } = toUnsubscribeRange[j];
       requestSubscriptionDelete(tbdId, dataId);
     }
   };

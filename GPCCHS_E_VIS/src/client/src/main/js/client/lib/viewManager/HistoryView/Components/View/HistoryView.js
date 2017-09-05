b1af91c7501611777332915809001a107f7cb1c5
Guillaume ARM <guillaume.arm.external@atos.net>
2017-09-05 10:58:24 +0000

    [FT:#6127] Create first basic table for HistoryView

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/HistoryView/Components/View/HistoryView.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/HistoryView/Components/View/HistoryView.js
@@ -1,5 +1,60 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import Dimensions from '../../../../windowProcess/common/Dimensions';
 
-const HistoryView = () => <div>Here is the view</div>;
+import styles from './HistoryView.css';
 
-export default HistoryView;
+console.warn(styles);
+
+const Table = ({ lines, cols, labels }) => {
+  return (
+    <table className={styles.table}>
+      <thead className={styles.thead}>
+        <tr className={styles.tr}>
+          {
+            cols.map(col => (
+              <th key={col} className={styles.th}>
+                {labels[col] || col}
+              </th>
+            ))
+          }
+        </tr>
+      </thead>
+      <tbody className={styles.tbody}>
+        {
+          lines.map(line => (
+            <tr key={line.name} className={styles.tr}>
+              {
+                cols.map(col => (
+                  <td key={col} className={styles.td}>
+                    {line[col]}
+                  </td>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  );
+};
+
+const HistoryView = (props) => {
+  console.warn(props);
+  const style = {
+    height: props.containerHeight,
+    width: props.containerWidth,
+  };
+  return (
+    <div className={styles.container} style={style}>
+      <Table {...props.data} />
+    </div>
+  );
+};
+
+HistoryView.propTypes = {
+  data: PropTypes.shape({}).isRequired,
+  containerWidth: PropTypes.number.isRequired,
+  containerHeight: PropTypes.number.isRequired,
+};
+
+export default Dimensions({ elementResize: true })(HistoryView);
--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/HistoryView/Components/View/HistoryViewContainer.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/HistoryView/Components/View/HistoryViewContainer.js
@@ -1,13 +1,26 @@
 import { PropTypes } from 'react';
+import _ from 'lodash/fp';
+import { v4 } from 'uuid';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import HistoryView from './HistoryView';
 
+const createLines = _.times(n => ({
+  name: v4() + '-' + v4() + '-' + v4() + '-' + v4(),
+  value: n + 1,
+  timestamp: (n + 1) * 1000,
+  a: !n ? _.times(_.always('z'), 1000) : 'aaaaaaaaaaaaaaaaaaaa',
+  b: 'bbbbbbbbbbbbbbbbbbbb',
+  c: 'cccccccccccccccccccc',
+}));
+
+const lines = createLines(100);
 
 const mapStateToProps = createStructuredSelector({
   data: () => ({
-    indexes: {},
-    values: {},
+    labels: { name: 'Name', timestamp: 'Time', value: 'Value' },
+    cols: ['timestamp', 'name', 'value', 'a', 'b', 'c'],
+    lines,
   }),
 });
 

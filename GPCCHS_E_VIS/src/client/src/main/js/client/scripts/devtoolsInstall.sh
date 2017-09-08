c49fb4fc75d46bd7c10db67298227f999f66a520
Guillaume ARM <guillaume.arm.external@atos.net>
2017-09-08 10:12:47 +0000

    [FT:#7750] Add install:devtools npm script .

---
+++ GPCCHS_E_VIS/src/client/src/main/js/client/scripts/devtoolsInstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+WGET=/usr/bin/wget
+
+EXTENSIONS_FOLDER=$HOME/.config/Electron/extensions
+REDUX_DEVTOOLS=lmhkpmbekcpmknklioeibfkpmmfibljd
+REACT_DEVELOPER_TOOLS=fmkadmapgofadopljbjfkapdkoienihi
+
+mkdir -p $EXTENSIONS_FOLDER
+cd $EXTENSIONS_FOLDER
+
+$WGET "https://clients2.google.com/service/update2/crx?response=redirect&x=id%3D$REDUX_DEVTOOLS%26uc&prodversion=32" -O $REDUX_DEVTOOLS.crx
+$WGET "https://clients2.google.com/service/update2/crx?response=redirect&x=id%3D$REACT_DEVELOPER_TOOLS%26uc&prodversion=32" -O $REACT_DEVELOPER_TOOLS.crx
+
+rm -rf $REDUX_DEVTOOLS
+rm -rf $REACT_DEVELOPER_TOOLS
+
+unzip $REDUX_DEVTOOLS.crx -d $REDUX_DEVTOOLS || true
+unzip $REACT_DEVELOPER_TOOLS.crx -d $REACT_DEVELOPER_TOOLS || true

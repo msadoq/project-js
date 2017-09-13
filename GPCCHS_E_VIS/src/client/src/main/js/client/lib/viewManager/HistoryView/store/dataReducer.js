fef0277ee83b1033a728e03209abec2617ad5cf9
Guillaume ARM <guillaume.arm.external@atos.net>
2017-09-13 16:56:19 +0000

    [HL] Comments INJECT_DATA_RANGE in HistoryView dataReducer
    because getStore cannot be used in a redux reducer

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/HistoryView/store/dataReducer.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/HistoryView/store/dataReducer.js
@@ -2,9 +2,9 @@ import _omit from 'lodash/omit';
 import _concat from 'lodash/concat';
 import _without from 'lodash/without';
 import cleanCurrentViewData from './cleanViewData';
-import { viewRangeAdd, selectDataPerView } from './viewDataUpdate';
-import { getStore } from '../../../mainProcess/store';
-import { getConfigurationByViewId } from '../../selectors';
+// import { viewRangeAdd, selectDataPerView } from './viewDataUpdate';
+// import { getStore } from '../../../serverProcess/store';
+// import { getConfigurationByViewId } from '../../selectors';
 
 import * as types from '../../../store/types';
 import * as constants from '../../constants';
@@ -96,33 +96,33 @@ export default function historyViewData(state = {}, action) {
         },
       };
     }
-    case types.INJECT_DATA_RANGE: {
-      const { dataToInject, newViewMap, newExpectedRangeIntervals } = action.payload;
-      const dataKeys = Object.keys(dataToInject);
-      // If nothing changed and no data to import, return state
-      if (!dataKeys.length) {
-        return state;
-      }
-      // since now, state will changed
-      let newState = state;
-      const viewIds = Object.keys(state);
-      for (let i = 0; i < viewIds.length; i += 1) {
-        const viewId = viewIds[i];
-        const historyConfig = getConfigurationByViewId(getStore().getState(), { viewId });
-        const sorting = historyConfig.sorting; // colName, direction
-        // Data Selection
-        const epSubState =
-          selectDataPerView(newViewMap[viewId], newExpectedRangeIntervals, dataToInject, sorting);
-        if (Object.keys(epSubState).length !== 0) {
-          // Data injection
-          const viewState = viewRangeAdd(newState[viewId], viewId, epSubState, historyConfig);
-          if (viewState !== newState[viewId]) {
-            newState = { ...newState, [viewId]: viewState };
-          }
-        }
-      }
-      return newState || {};
-    }
+    // case types.INJECT_DATA_RANGE: {
+    //   const { dataToInject, newViewMap, newExpectedRangeIntervals } = action.payload;
+    //   const dataKeys = Object.keys(dataToInject);
+    //   // If nothing changed and no data to import, return state
+    //   if (!dataKeys.length) {
+    //     return state;
+    //   }
+    //   // since now, state will changed
+    //   let newState = state;
+    //   const viewIds = Object.keys(state);
+    //   for (let i = 0; i < viewIds.length; i += 1) {
+    //     const viewId = viewIds[i];
+    //     const historyConfig = getConfigurationByViewId(getStore().getState(), { viewId });
+    //     const sorting = historyConfig.sorting; // colName, direction
+    //     // Data Selection
+    //     const epSubState =
+    //       selectDataPerView(newViewMap[viewId], newExpectedRangeIntervals, dataToInject, sorting);
+    //     if (Object.keys(epSubState).length !== 0) {
+    //       // Data injection
+    //       const viewState = viewRangeAdd(newState[viewId], viewId, epSubState, historyConfig);
+    //       if (viewState !== newState[viewId]) {
+    //         newState = { ...newState, [viewId]: viewState };
+    //       }
+    //     }
+    //   }
+    //   return newState || {};
+    // }
     case types.WS_VIEWDATA_CLEAN: {
       const { previousDataMap, dataMap } = action.payload;
 

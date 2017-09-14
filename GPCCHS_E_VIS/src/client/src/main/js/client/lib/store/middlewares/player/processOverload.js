4e551a55304a289a07fa41b724ada39af3d8be60
Guillaume ARM <guillaume.arm.external@atos.net>
2017-09-14 09:44:34 +0000

    [FT:#6127] Add warning message when autopause the timebar because process is overloaded

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/player/processOverload.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/player/processOverload.js
@@ -1,5 +1,6 @@
 import * as types from '../../types';
 import { pause } from '../../actions/hsc';
+import { add as addMessage } from '../../actions/messages';
 /**
  * Middleware to apply pause on master overload
  */
@@ -8,6 +9,7 @@ const onProcessOverload = () => ({ dispatch }) => next => (action) => {
   if (action.type === types.HSS_UPDATE_HEALTH_STATUS) {
     if (action.payload.status === 'CRITICAL') {
       dispatch(pause());
+      dispatch(addMessage('global', 'warning', 'Process overloaded : pause the timebar'));
     }
   }
   return nextAction;

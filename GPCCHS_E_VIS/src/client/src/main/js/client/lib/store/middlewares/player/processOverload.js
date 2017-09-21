134bf2e6d886d74975fe72b1e90b039b3a82163c
Guillaume ARM <guillaume.arm.external@atos.net>
2017-09-21 10:40:56 +0000

    [HL] Check if vima is playing before pause in processOverload middleware

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/player/processOverload.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/middlewares/player/processOverload.js
@@ -1,13 +1,15 @@
 import * as types from '../../types';
 import { pause } from '../../actions/hsc';
+import { getIsPlaying } from '../../reducers/hsc';
 import { add as addMessage } from '../../actions/messages';
 /**
  * Middleware to apply pause on master overload
  */
-const onProcessOverload = () => ({ dispatch }) => next => (action) => {
+const onProcessOverload = () => ({ dispatch, getState }) => next => (action) => {
   const nextAction = next(action);
   if (action.type === types.HSS_UPDATE_HEALTH_STATUS) {
-    if (action.payload.status === 'CRITICAL') {
+    const isPlaying = getIsPlaying(getState());
+    if (isPlaying && action.payload.status === 'CRITICAL') {
       dispatch(pause());
       dispatch(addMessage('global', 'warning', 'Process overloaded : pause the timebar'));
     }

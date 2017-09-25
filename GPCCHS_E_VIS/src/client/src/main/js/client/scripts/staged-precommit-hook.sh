e395f3ed73addf1523743878c554d4452ecdf0dc
Guillaume ARM <guillaume.arm.external@atos.net>
2017-09-25 13:15:41 +0000

    [HL] Fix staged precommit git hook (do not lint adapters)

--- GPCCHS_E_VIS/src/client/src/main/js/client/scripts/staged-precommit-hook.sh
+++ GPCCHS_E_VIS/src/client/src/main/js/client/scripts/staged-precommit-hook.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-git stash -u --keep-index 2> /dev/null
+git stash -u --keep-index &> /dev/null
 
-./node_modules/.bin/eslint --max-warnings 0 $@
+# Do not lint adapters (workaround for https://stackoverflow.com/questions/37927772/how-to-silence-warnings-about-ignored-files-in-eslint)
+./node_modules/.bin/eslint --max-warnings 0 `echo $@ | tr ' ' '\n' | grep -ve '^/data/work/gitRepositories/LPISIS/GPCCHS/GPCCHS_E_VIS/src/client/src/main/js/client/adapters' | xargs`
 EXIT_CODE=$?
 
-git stash pop 2> /dev/null
+git stash pop &> /dev/null
 exit $EXIT_CODE

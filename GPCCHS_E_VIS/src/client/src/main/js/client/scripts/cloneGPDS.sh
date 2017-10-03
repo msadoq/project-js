90d5a576f5f5dbe7f081ebf6fa821c4abcc578b1
Guillaume ARM <guillaume.arm.external@atos.net>
2017-10-03 15:35:27 +0000

    [HL] Add cloneGPDS script to automatically clone GPDS repo

---
+++ GPCCHS_E_VIS/src/client/src/main/js/client/scripts/cloneGPDS.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+### This scripts automatically clone GPDS repo in /data/work/gitRepositories/LPISIS
+
+GPDS_FOLDER=GPDS
+
+cd /data/work/gitRepositories/LPISIS
+[ -d ./$GPDS_FOLDER ] && echo GPDS is already cloned. || git clone gitolite@isis.cnes-isis.toulouse.atos.net:gpds/LPISIS/GPDS.git ./$GPDS_FOLDER
+
+cd $GPDS_FOLDER
+git checkout R8-fwk
+git pull origin R8-fwk

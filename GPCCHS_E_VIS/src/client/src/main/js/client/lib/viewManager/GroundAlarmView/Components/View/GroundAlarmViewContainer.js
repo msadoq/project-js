aa5c4b1f177003e8c658122f6219cb20e3697e29
Guillaume ARM <guillaume.arm.external@atos.net>
2017-10-03 16:08:09 +0000

    [FT:#5806] Add withStub hoc . .
    + use it temporary in GroundAlarmViewContainer

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/GroundAlarmView/Components/View/GroundAlarmViewContainer.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/GroundAlarmView/Components/View/GroundAlarmViewContainer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash/fp';
 import { PropTypes } from 'react';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
@@ -5,7 +6,32 @@ import GroundAlarmView from './GroundAlarmView';
 import { getAlarmDomain, getAlarmTimeline } from '../../store/configurationSelectors';
 import { getData } from '../../store/dataReducer';
 
-import withStub from './withStub';
+import withStub from '../../../../windowProcess/common/hoc/withStub';
+
+const append = _.curry((val, t) => _.concat(t, val));
+
+const connectStub = withStub({
+  frequency: 1000,
+  updater: _.pipe(
+    _.update('data.tick', _.add(1)),
+    state => _.update('data.lines', append(`ep1 ${state.data.tick}`), state),
+    state => _.set(['data', 'data', 'ep1', state.data.tick], {
+      name: state.data.tick,
+      timestamp: state.data.tick,
+      epName: state.data.tick,
+      masterTime: state.data.tick,
+    }, state)
+  ),
+  initialState: {
+    data: {
+      cols: ['name', 'timestamp', 'epName', 'masterTime'],
+      lines: [],
+      data: { ep1: {} },
+    },
+  },
+});
+
+const GroundAlarmViewStubbed = connectStub(GroundAlarmView);
 
 const mapStateToProps = createStructuredSelector({
   data: getData,
@@ -13,9 +39,10 @@ const mapStateToProps = createStructuredSelector({
   alarmTimeline: getAlarmTimeline,
 });
 
-const GroundAlarmViewContainer = connect(mapStateToProps)(GroundAlarmView);
+const GroundAlarmViewContainer = connect(mapStateToProps)(GroundAlarmViewStubbed);
 
 GroundAlarmViewContainer.propTypes = {
   viewId: PropTypes.string.isRequired,
 };
-export default withStub()(GroundAlarmViewContainer);
+
+export default GroundAlarmViewContainer;

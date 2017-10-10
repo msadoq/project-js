f87426e4d18378bc09baee7e3b42ff1d6d738c37
Guillaume ARM <guillaume.arm.external@atos.net>
2017-10-10 08:55:06 +0000

    [HL] Trap SIGINT in staged-precommit-hook .

--- GPCCHS_E_VIS/src/client/src/main/js/client/scripts/staged-precommit-hook.sh
+++ GPCCHS_E_VIS/src/client/src/main/js/client/scripts/staged-precommit-hook.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 git stash -u --keep-index &> /dev/null
 
+trap noop INT
+
+function noop() {
+  return
+}
+
 # Do not lint adapters (workaround for https://stackoverflow.com/questions/37927772/how-to-silence-warnings-about-ignored-files-in-eslint)
 ./node_modules/.bin/eslint --max-warnings 0 `echo $@ | tr ' ' '\n' | grep -ve '^/data/work/gitRepositories/LPISIS/GPCCHS/GPCCHS_E_VIS/src/client/src/main/js/client/adapters' | xargs`
+
 EXIT_CODE=$?
 
 git stash pop &> /dev/null

8b4e7fe8e09962e94c55b15509f10c83edfe0ded
Johan Maupetit <jmaupetit@squad.fr>
2017-10-10 08:39:34 +0000

    [FT:#5806] add payload generation for grounAlarms

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/stubProcess/utils/getPayload.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/stubProcess/utils/getPayload.js
@@ -9,8 +9,20 @@ const stubs = require('../../utils/stubs');
 
 const stubData = stubs.getStubData();
 
-function getValue(timestamp) {
-  return (1 + Math.sin(timestamp / 6000));
+/**
+ * Generate sinusoidal value within [offset, offset+2] range,
+ * offset is a unique float value computed from epName.
+ *
+ * @param  {int} timestamp
+ * @return {float}
+ */
+function getSinValue(timestamp, epName) {
+  let offset = 0;
+  Buffer.from(epName).forEach((val) => {
+    offset += val;
+  });
+
+  return (1 + Math.sin(timestamp / 6000)) + (offset / 10);
 }
 
 // function getMonitoringState(timestamp) {
@@ -29,9 +41,34 @@ function getMonitoringState() {
   return arr[n];
 }
 
-const getComObject = (comObject, timestamp, value) => {
+const getComObject = (comObject, timestamp, epName) => {
   switch (comObject) {
+    case 'groundMonitoringAlarm': {
+      const value = getSinValue(timestamp, epName);
+
+      return stubData.getGroundMonitoringAlarmProtobuf({
+        creationDate: timestamp - 100,
+        // paramUid: null,
+        updateDate: timestamp - 50,
+        // closingDate: null,
+        hasAckRequest: false,
+        // alarmId: null,
+        transitions: [
+          stubData.getTransitionProtobuf({
+            onboardDate: timestamp,
+            groundDate: timestamp + 20,
+            convertedValue: value,
+            extractedValue: value,
+            rawValue: value,
+            monitoringState: getMonitoringState(timestamp),
+          }),
+        ],
+        // isNominal: false
+      });
+    }
     case 'ReportingParameter': {
+      const value = getSinValue(timestamp, epName);
+
       return stubData.getReportingParameterProtobuf({
         groundDate: timestamp + 20,
         onboardDate: timestamp,
@@ -41,27 +78,26 @@ const getComObject = (comObject, timestamp, value) => {
         monitoringState: getMonitoringState(timestamp),
       });
     }
-    case 'DecommutedPacket':
-      {
-        return stubData.getDecommutedPacketProtobuf({
-          groundDate: timestamp + 20,
-          onboardDate: timestamp,
-          decommutedValues: [
-            stubData.getDecommutedValue({
-              name: 'GENE_AM_CCSDSVERS1',
-              extractedValue: Buffer.alloc(10, 12),
-              rawValue: 2,
-              convertedValue: 2,
-            }),
-            stubData.getDecommutedValue({
-              name: 'GENE_AM_CCSDSVERS2',
-              extractedValue: Buffer.alloc(10, 1),
-              rawValue: 0,
-              convertedValue: 0,
-            }),
-          ],
-        });
-      }
+    case 'DecommutedPacket': {
+      return stubData.getDecommutedPacketProtobuf({
+        groundDate: timestamp + 20,
+        onboardDate: timestamp,
+        decommutedValues: [
+          stubData.getDecommutedValue({
+            name: 'GENE_AM_CCSDSVERS1',
+            extractedValue: Buffer.alloc(10, 12),
+            rawValue: 2,
+            convertedValue: 2,
+          }),
+          stubData.getDecommutedValue({
+            name: 'GENE_AM_CCSDSVERS2',
+            extractedValue: Buffer.alloc(10, 1),
+            rawValue: 0,
+            convertedValue: 0,
+          }),
+        ],
+      });
+    }
     case 'Pus003Model':
       return stubData.getPus003ModelProtobuf({
         groundDate: timestamp,
@@ -80,13 +116,12 @@ const getComObject = (comObject, timestamp, value) => {
   }
 };
 
+/**
+ * @param  {String} epName    entry Point Name
+ */
 module.exports = function getPayload(timestamp, comObject, epName = 'todo') {
-  let epNumber = 0;
-  Buffer.from(epName)
-    .forEach((val) => { epNumber += val; });
-  const value = getValue(timestamp) + (epNumber / 10);
   return {
     timestamp: stubData.getTimestampProtobuf({ ms: timestamp }),
-    payload: getComObject(comObject, timestamp, value),
+    payload: getComObject(comObject, timestamp, epName),
   };
 };

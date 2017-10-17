362fa4cb37215f89d80ee02b33fe14f5858fd0bb
Paul GAUCHER <paul.gaucher.external@atos.net>
2017-10-17 14:47:12 +0000

    [FT:#8394] Add spec file for store subscriptions observer

---
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/observers/subscriptionStoreObserver.spec.js
@@ -0,0 +1,24 @@
+// import subscriptionStoreObserver from './subscriptionStoreObserver';
+
+
+describe('store/observers/computeMissingLastIntervals', () => {
+  test('Add new tbdId in ranges', () => {
+
+  });
+
+  test('Add new tbdId in last', () => {
+
+  });
+
+  test('Remove one tbdId in ranges', () => {
+
+  });
+
+  test('Remove one tbdId in last', () => {
+
+  });
+
+  test('Remove one tbdId in last but tbdId is also in ranges', () => {
+
+  });
+});

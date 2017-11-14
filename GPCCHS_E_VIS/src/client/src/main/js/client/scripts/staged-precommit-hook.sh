0dbef672b573b7178f72857e746420fad85e9f03
Guillaume ARM <guillaume.arm.external@atos.net>
2017-11-14 15:07:09 +0000

    [FT:#5806] Use jest --findRelatedTests for each commit

--- GPCCHS_E_VIS/src/client/src/main/js/client/scripts/staged-precommit-hook.sh
+++ GPCCHS_E_VIS/src/client/src/main/js/client/scripts/staged-precommit-hook.sh
@@ -7,10 +7,27 @@ function noop() {
   return
 }
 
+function exitSuccess() {
+  git stash pop &> /dev/null
+  exit 0
+}
+
+function exitError() {
+  git stash pop &> /dev/null
+  exit 1
+}
+
 # Do not lint adapters (workaround for https://stackoverflow.com/questions/37927772/how-to-silence-warnings-about-ignored-files-in-eslint)
 ./node_modules/.bin/eslint --max-warnings 0 `echo $@ | tr ' ' '\n' | grep -ve '^/data/work/gitRepositories/LPISIS/GPCCHS/GPCCHS_E_VIS/src/client/src/main/js/client/benchmarks' | grep -ve '^/data/work/gitRepositories/LPISIS/GPCCHS/GPCCHS_E_VIS/src/client/src/main/js/client/adapters' | xargs`
-
 EXIT_CODE=$?
+if [ $EXIT_CODE -gt 0 ]; then
+  exitError
+fi
 
-git stash pop &> /dev/null
-exit $EXIT_CODE
+NODE_ENV=test ./node_modules/.bin/jest --findRelatedTests $@
+EXIT_CODE=$?
+if [ $EXIT_CODE -gt 0 ]; then
+  exitError
+fi
+
+exitSuccess

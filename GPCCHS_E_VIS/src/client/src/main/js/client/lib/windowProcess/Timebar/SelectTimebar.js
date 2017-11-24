e5b176f63460e133dffbdf2e524f2c5a13075bef
Yann Lastapis <yann.lastapis.external@atos.net>
2017-11-24 13:37:23 +0000

    [FT:#8466] creation page VIMA default select timebar

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Timebar/SelectTimebar.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/windowProcess/Timebar/SelectTimebar.js
@@ -20,7 +20,7 @@ export default class SelectTimebar extends PureComponent {
    * @param nextProps
    */
   componentWillReceiveProps(nextProps) {
-    // timebars has changed
+    // timebars has changed. forced to do it here, dispatch (in onCreate) is asynchronous
     if (nextProps.timebars !== this.props.timebars) {
       const diff = _difference(Object.keys(nextProps.timebars), Object.keys(this.props.timebars));
       // only one more, and this one is the new created one
@@ -62,69 +62,122 @@ export default class SelectTimebar extends PureComponent {
       form: this.state.form === 'select' ? 'create' : 'select',
     });
   };
-
+  assignTimebarId = (el) => { this.timebarId = el; };
+  assignTimebarSelect = (el) => { this.timebarsSelect = el; };
   render() {
-    const {
-      timebars,
-      pageId,
-    } = this.props;
-    const {
-      form,
-      error,
-    } = this.state;
-
-    if (!pageId) {
+    if (!this.props.pageId) {
       return null;
     }
 
     return (
       <div>
-        <div className={form === 'create' ? 'hidden' : 'p10'}>
-          <h4>Select a timebar</h4>
-          <form onSubmit={this.onSelect} className="form-horizontal" style={{ maxWidth: '200px', width: '95%' }}>
-            <FormGroup>
-              <select
-                ref={(el) => { this.timebarsSelect = el; }}
-                className="form-control"
-              >
-                {
-                  Object.keys(timebars).map(
-                    key => (
-                      <option key={timebars[key].id} value={key}>
-                        {timebars[key].id}
-                      </option>
-                    )
-                  )
-                }
-              </select>
-            </FormGroup>
-            <FormGroup>
-              <input type="submit" className="btn btn-primary" value="Select" />
-              {' '}
-              <button className="btn btn-info" onClick={this.switchForm}>Create a timebar</button>
-            </FormGroup>
-          </form>
-        </div>
-        <div className={form === 'select' ? 'hidden' : 'p10'}>
-          <h4>Create a timebar</h4>
-          <form onSubmit={this.onCreate} className="form-horizontal" style={{ maxWidth: '200px', width: '95%' }}>
-            <FormGroup>
-              { error && <Alert bsStyle="danger">{ error }</Alert> }
-              <input
-                ref={(el) => { this.timebarId = el; }}
-                onChange={this.onChange}
-                className="form-control"
-                placeholder="Timebar name"
-              />
-            </FormGroup>
-            <FormGroup>
-              <input disabled={!!error} type="submit" className="btn btn-primary" value="Create" />
-              {' '}
-              <button className="btn btn-info" onClick={this.switchForm}>Select a timebar</button>
-            </FormGroup>
-          </form>
-        </div>
+        <TimebarCreation
+          form={this.state.form}
+          timebars={this.props.timebars}
+          onSelect={this.onSelect}
+          switchForm={this.switchForm}
+          assignTimebarSelect={this.assignTimebarSelect}
+        />
+        <TimebarSelection
+          form={this.state.form}
+          error={this.state.error}
+          onCreate={this.onCreate}
+          onChange={this.onChange}
+          switchForm={this.switchForm}
+          assignTimebarId={this.assignTimebarId}
+        />
       </div>
     );
   }
 }
+
+/**
+ * @param form
+ * @param timebars
+ * @param onSelect
+ * @param switchForm
+ * @param assignTimebarSelect
+ * @returns {*}
+ * @constructor
+ */
+export const TimebarCreation = ({
+  form,
+  timebars,
+  onSelect,
+  switchForm,
+  assignTimebarSelect,
+}) => (
+  <div className={form === 'create' ? 'hidden' : 'p10'}>
+    <h4>Select a timebar</h4>
+    <form onSubmit={onSelect} className="form-horizontal" style={{ maxWidth: '200px', width: '95%' }}>
+      <FormGroup>
+        <select
+          ref={assignTimebarSelect}
+          className="form-control"
+        >
+          {
+            Object.keys(timebars).map(
+              key => (
+                <option key={timebars[key].id} value={key}>
+                  {timebars[key].id}
+                </option>
+              )
+            )
+          }
+        </select>
+      </FormGroup>
+      <FormGroup>
+        <input type="submit" className="btn btn-primary" value="Select" />
+        {' '}
+        <button className="btn btn-info" onClick={switchForm}>Create a timebar</button>
+      </FormGroup>
+    </form>
+  </div>
+);
+TimebarCreation.propTypes = {
+  form: string.isRequired,
+  timebars: shape().isRequired,
+  onSelect: func.isRequired,
+  switchForm: func.isRequired,
+  assignTimebarSelect: func.isRequired,
+};
+
+export const TimebarSelection = ({
+  form,
+  error,
+  onCreate,
+  onChange,
+  switchForm,
+  assignTimebarId,
+}) => (
+  <div className={form === 'select' ? 'hidden' : 'p10'}>
+    <h4>Create a timebar</h4>
+    <form onSubmit={onCreate} className="form-horizontal" style={{ maxWidth: '200px', width: '95%' }}>
+      <FormGroup>
+        { error && <Alert bsStyle="danger">{ error }</Alert> }
+        <input
+          ref={assignTimebarId}
+          onChange={onChange}
+          className="form-control"
+          placeholder="Timebar name"
+        />
+      </FormGroup>
+      <FormGroup>
+        <input disabled={!!error} type="submit" className="btn btn-primary" value="Create" />
+        {' '}
+        <button className="btn btn-info" onClick={switchForm}>Select a timebar</button>
+      </FormGroup>
+    </form>
+  </div>
+);
+TimebarSelection.propTypes = {
+  form: string.isRequired,
+  error: string,
+  onCreate: func.isRequired,
+  onChange: func.isRequired,
+  switchForm: func.isRequired,
+  assignTimebarId: func.isRequired,
+};
+TimebarSelection.defaultProps = {
+  error: null,
+};

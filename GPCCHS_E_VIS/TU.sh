c19bf6dc237f49957d03e37a403f4ae1e3de4301
Guillaume ARM <guillaume.arm.external@atos.net>
2018-01-16 09:57:57 +0000

    [HL] Add test:pic npm script (used by TU.sh)

--- GPCCHS_E_VIS/TU.sh
+++ GPCCHS_E_VIS/TU.sh
@@ -10,5 +10,12 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PRGDIR/target/dependencies/lib
 
 BUILD_DIR=$(cd $EXEC_DIR/src/launcher/target/work/js/client ; echo $PWD)
 
+# Prepare/clean older to store test reports
+if [ -d $PRGDIR/target/test-reports ] ; then
+    rm $PRGDIR/target/test-reports/*.xml
+else
+    mkdir -p $PRGDIR/target/test-reports
+fi
+
 cd $BUILD_DIR
-npm run test
+TEST_REPORT_PATH=$PRGDIR/target/test-reports npm run test:pic

7ec6fd403fbd09cabbc467dbcfc7732d1caf1c5b
Paul GAUCHER <paul.gaucher.external@atos.fr>
2018-03-05 15:54:57 +0000

    [FT:#8368] Fix unit test . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/common/Components/Editor/EntryPointUnitContainer.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/common/Components/Editor/EntryPointUnitContainer.spec.js
@@ -5,6 +5,55 @@ import { Provider } from 'react-redux';
 import { reduxForm } from 'redux-form';
 import EntryPointUnitContainer from './EntryPointUnitContainer';
 
+const stubState = {
+  domains: [
+    {
+      domainId: 13214,
+    },
+  ],
+  sessions: [
+    {
+      name: 'master',
+      id: 1321,
+    },
+  ],
+  timelines: {
+    'timeline-uuid': {
+      sessionName: 'master',
+      id: 'timeline-id',
+    },
+  },
+  catalogs: {
+    'domain-id-session-id': [
+      {
+        name: 'catalogName',
+        items: [
+          {
+            name: 'itemName',
+            unit: 'J',
+          }
+        ]
+      },
+    ],
+  },
+  pages: {
+    'page-id': {
+      domainName: 'pageDomainName',
+    },
+  },
+  views: {
+    'view-id': {
+      domainName: 'viewDomainName',
+    },
+  },
+  hsc: {
+    domainName: 'viewDomainName',
+  },
+  comObjectMap: {
+    fields: [],
+  },
+};
+
 const propsStub = {
   pageId: 'page1',
   viewId: 'view1',
@@ -16,11 +65,17 @@ const propsStub = {
   updateViewPanels: () => null,
   removeEntryPoint: () => null,
   entryPointsPanels: {},
+  connectedData: {
+    domain: 13214,
+    catalog: 'catalogName',
+    timeline: 'timeline-id',
+    catalogItem: 'itemName',
+  },
 };
 describe('EntryPointUnitContainer :: render', () => {
   test('EntryPointUnitContainer :: render', () => {
     const Decorated = reduxForm({ form: 'testForm' })(EntryPointUnitContainer);
-    const store = createStore(state => state, {});
+    const store = createStore(state => state, stubState);
     const tree = renderer.create(
       <Provider store={store}>
         <Decorated

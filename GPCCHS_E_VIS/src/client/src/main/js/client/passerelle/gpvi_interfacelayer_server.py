ba786740c9bd23ce5a6fa4d304c166a71d883257
Julien MIRA <jmira@cnes-isis.toulouse.atos.net>
2018-03-12 13:30:32 +0000

    [FT:#10790] change zmq url from tcp to ipc for bundled version

--- GPCCHS_E_VIS/src/client/src/main/js/client/passerelle/gpvi_interfacelayer_server.py
+++ GPCCHS_E_VIS/src/client/src/main/js/client/passerelle/gpvi_interfacelayer_server.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from bridgeServer_v2 import BridgeServer
 
-server = BridgeServer(None, "tcp://127.0.0.1:5052", "tcp://127.0.0.1:5053")
+server = BridgeServer(None, "ipc://convertUnitRequest.ipc", "ipc://convertUnitResponse.ipc")
 server.start()
 
 

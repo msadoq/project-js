d964a423535e0cef42e439cdf548c33e987e7a46
Julien MIRA <jmira@cnes-isis.toulouse.atos.net>
2018-03-26 13:51:05 +0000

    [FT:#10846] fix panels tests . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/pages/panels.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/reducers/pages/panels.spec.js
@@ -34,7 +34,10 @@ describe('store:pages:reducer:panels', () => {
       timebarHeight: 130,
       timebarIsMinimized: false,
       explorerWidth: 250,
+      searchIsMinimized: true,
       explorerTab: undefined,
+      searchViewId: undefined,
+      searchWidth: 300,
       explorerIsMinimized: true,
     });
   });

1181b6bcf276b9d96bd8a3cb9290669afbc4910f
Yann lastapis <yann.lastapis.external@atos.net>
2018-05-22 13:50:36 +0000

    [FT:#0008401] drag drop paquet dans dynamicView VIMA

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/common/index.spec.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/common/index.spec.js
@@ -1,18 +1,36 @@
+/* eslint-disable max-len */
 import {
   buildFormula,
 } from 'viewManager/common';
 
+const constants = require('constants');
+
+const PACKETS_TYPES = constants.PACKETS_TYPES;
+
 describe('buildFormula', () => {
   [undefined, null, 'Reporting'].forEach((catalog) => {
     [undefined, null, 'AGA_AM_ACQPRIORITY'].forEach((catalogItem) => {
-      [undefined, null, 'ReportingParameter'].forEach((comObject) => {
+      [
+        undefined,
+        null,
+        PACKETS_TYPES.REPORTING_PARAMETER,
+        PACKETS_TYPES.DECOMMUTED_PACKET,
+        PACKETS_TYPES.CLCW,
+        PACKETS_TYPES.ISIS_AGGREGATION,
+        PACKETS_TYPES.RM,
+        PACKETS_TYPES.TM,
+      ].forEach((comObject) => {
         [undefined, null, 'convertedValue'].forEach((field) => {
           const formula = buildFormula(catalog, catalogItem, comObject, field);
           test(`buildFormula :: [${catalog}][${catalogItem}][${comObject}][${field}] || ${formula}`, () => {
-            if (catalog && catalogItem && comObject && field) {
-              expect(formula).toEqual('Reporting.AGA_AM_ACQPRIORITY<ReportingParameter>.convertedValue');
+            if (catalog && catalogItem && comObject && comObject === PACKETS_TYPES.REPORTING_PARAMETER) {
+              expect(formula).toEqual(`Reporting.AGA_AM_ACQPRIORITY<${PACKETS_TYPES.REPORTING_PARAMETER}>.convertedValue`);
+            } else if (catalog && catalogItem && comObject && comObject === PACKETS_TYPES.DECOMMUTED_PACKET) {
+              expect(formula).toEqual(`Reporting.AGA_AM_ACQPRIORITY<${PACKETS_TYPES.DECOMMUTED_PACKET}>`);
+            } else if (catalog && catalogItem && comObject && field) {
+              expect(formula).toEqual(`Reporting.AGA_AM_ACQPRIORITY<${comObject}>.convertedValue`);
             } else if (catalog && catalogItem && comObject) {
-              expect(formula).toEqual('Reporting.AGA_AM_ACQPRIORITY<ReportingParameter>.convertedValue');
+              expect(formula).toEqual(`Reporting.AGA_AM_ACQPRIORITY<${comObject}>`);
             } else if (catalog && catalogItem) {
               expect(formula).toEqual('Reporting.AGA_AM_ACQPRIORITY');
             } else if (catalog) {

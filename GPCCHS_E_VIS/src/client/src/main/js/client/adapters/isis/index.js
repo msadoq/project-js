c2b39a6d9dd0917197c45dca194a3fb7c707c493
Paul GAUCHER <paul.gaucher.external@atos.fr>
2018-06-25 09:30:24 +0000

    [HL] Fix update of generators . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/adapters/isis/index.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/adapters/isis/index.js
@@ -1,10 +1,22 @@
+// ====================================================================
+// HISTORY
+// VERSION : 1.1.2 : FA : #7453 : 07/08/2017 : Move adapters folder in client folder
+// END-HISTORY
+// ====================================================================
+
 // Produced by Acceleo JavaScript Generator 1.1.2
 /* eslint-disable max-len, "DV6 TBC_CNES generated code can't avoid too long lines" */
 /* eslint-disable complexity, "DV6 TBC_CNES generated code can't avoid complexity" */
 /* eslint-disable camelcase, "DV6 TBC_CNES generated code" */
-const queries = require('./queries');
-const timeBasedDataType = require('./timeBasedDataType');
-const soo = require('./soo');
+const pusGroundModel = require('./pusGroundModel');
+const connection = require('./connection');
+const cop1 = require('./cop1');
+const decommutedPacket = require('./decommutedPacket');
+const decommutedParameter = require('./decommutedParameter');
+const encode = require('./encode');
+const groundAlarm = require('./groundAlarm');
+const packet = require('./packet');
+const rawData = require('./rawData');
 const ackRequest = require('./ackRequest');
 const ccsds_com = require('./ccsds_com');
 const ccsds_cs = require('./ccsds_cs');
@@ -24,21 +36,32 @@ const statAggregation = require('./statAggregation');
 const tcHistory = require('./tcHistory');
 const userEvent = require('./userEvent');
 const logModel = require('./logModel');
-const pusGroundModel = require('./pusGroundModel');
-const connection = require('./connection');
-const packet = require('./packet');
-const rawData = require('./rawData');
+const soo = require('./soo');
+const queries = require('./queries');
+const timeBasedDataType = require('./timeBasedDataType');
 const briefcase = require('./briefcase');
 const memoryImage = require('./memoryImage');
 const opAlert = require('./opAlert');
 const sessionModel = require('./sessionModel');
 
 module.exports = {
-  queries,
+  pusGroundModel,
   
-  timeBasedDataType,
+  connection,
   
-  soo,
+  cop1,
+  
+  decommutedPacket,
+  
+  decommutedParameter,
+  
+  encode,
+  
+  groundAlarm,
+  
+  packet,
+  
+  rawData,
   
   ackRequest,
   
@@ -78,13 +101,11 @@ module.exports = {
   
   logModel,
   
-  pusGroundModel,
+  soo,
   
-  connection,
+  queries,
   
-  packet,
-  
-  rawData,
+  timeBasedDataType,
   
   briefcase,
   

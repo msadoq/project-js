84e61f53936e11846f6e9bc61b7dbcd5522ff973
Hicham Benjelloun <hicham.benjelloun.external.atos.net>
2018-06-28 11:22:56 +0000

    [FT:#11919] Add sample tabular data management in PUS11 data reducer

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/PUS11View/store/dataReducer.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/PUS11View/store/dataReducer.js
@@ -12,57 +12,41 @@ import _ from 'lodash/fp';
 import { VM_VIEW_PUS11 } from '../../constants';
 import createScopedDataReducer from '../../commonData/createScopedDataReducer';
 import { INJECT_PUS_RANGE_DATA } from '../../../store/types';
-import { viewRangeAdd } from '../../HistoryView/store/viewDataUpdate';
+import { injectTabularData } from '../../commonData/reducer';
 
 // eslint-disable-next-line no-unused-vars
 function pus11DataReducer(state = {}, action, viewId) {
   switch (action.type) {
     case INJECT_PUS_RANGE_DATA: {
-      // const { viewId } = action.payload;
       console.log('INJECT_PUS_RANGE_DATA');
+
+      // const { configurations } = action.payload;
       /**
+       * TODO: update injected data format in stubs
+       *
        * action.payload: {
+       *  timestamp: number,
        *  data: {
-       *    dataToInject: {
-       *      <timestamp>: {
        *        PUS011PModel: {...},
-       *        Pus011SubSchedule: {},
-       *        Pus011Apid: {},
-       *        Pus011Command: {},
-       *      },
-       *    },
+       *        Pus011SubSchedule: [],
+       *        Pus011Apid: [],
+       *        Pus011Command: [],
        *  },
        * },
        */
-      const dataToInject = _.getOr([], ['payload', 'data', 'dataToInject'], action);
-      const pus11Configuration = {
-        entryPoints: [
-          {
-            connectedData: {},
-            id: [viewId],
-            name: 'PUS11EP',
-          },
-        ],
-        tables: [
+      const data = _.getOr([], ['payload', 'data'], action);
+      // const pus11Configuration = configurations.PUS11ViewConfiguration[viewId];
 
-        ],
-      }; // @todo fetch that configuration ??
+      let updatedState = state;
 
-      const dataKeys = Object.keys(dataToInject);
-      if (!dataKeys.length) {
-        console.log('!dataKeys.length');
-        return state;
-      }
+      updatedState =
+        injectTabularData(updatedState, 'subSchedules', data.Pus011SubSchedule);
+      updatedState =
+        injectTabularData(updatedState, 'enablesApids', data.Pus011Apid);
+      updatedState =
+        injectTabularData(updatedState, 'commands', data.Pus011Command);
 
-      console.log('state', state);
-      const updatedState = viewRangeAdd(state, viewId, dataToInject, pus11Configuration);
-      console.log('updatedState', updatedState);
-
-      // const updatedState = viewRangeAdd(state, viewId, dataToInject, {});
-
-      // console.log('updatedState', updatedState);
-
-      return state;
+      return updatedState;
     }
     default:
       return state;

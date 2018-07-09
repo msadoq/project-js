867538d37b8b24006bcf35e3a968dc7845c6f677
Jean MARTINEAU <jmartine@cnes-isis.toulouse.atos.net>
2018-07-09 14:13:13 +0000

    [HL] this is a commit which fixes failing tests, even (and particularly) those introduced by invalid commits. The kind of problem a continuous integration tool would detect automatically. Notice that even the merge would not have been possible, preventing people to have to fix something from someone else's code.

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/DynamicView/store/cleanViewData.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/DynamicView/store/cleanViewData.js
@@ -28,7 +28,7 @@ export default function cleanCurrentViewData(
     return currentViewState;
   }
   // new visible view
-  if (!oldViewDef || !currentViewState || !currentViewState.index || {}) {
+  if (!oldViewDef || !currentViewState || !currentViewState.index) {
     return currentViewState;
   }
   // invisible view

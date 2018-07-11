26bc85b8ba749f7cb3396981f3d4b143d8a78277
Jean MARTINEAU <jmartine@cnes-isis.toulouse.atos.net>
2018-07-11 13:50:27 +0000

    [HL] update protos with changes of ADE, episode II

---
+++ GPCCHS_E_VIS/src/client/src/main/js/client/adapters/dc/dataControllerUtils/ADEMonitoringItem.stub.js
@@ -0,0 +1,24 @@
+const ProtoBuf = require('protobufjs');
+const applyOverride = require('../applyOverride');
+const Adapter = require('./ADEMonitoringItem');
+
+const { getADEMonitoringLaw } = require('./ADEMonitoringLaw.stub');
+
+const Builder = new ProtoBuf.Root()
+  .loadSync(`${__dirname}/ADEMonitoringItem.proto`, { keepCase: true })
+  .lookup('dataControllerUtils.protobuf.ADEMonitoringItem');
+
+const getADEMonitoringItem = override => applyOverride({
+  item: 'inator',
+  law: getADEMonitoringLaw({ type: 'monitoringJude' }),
+}, override);
+
+const getADEMonitoringItemProtobuf = override => {
+  const toEncode = getADEMonitoringItem(override);
+  return Builder.encode(Adapter.encode(toEncode)).finish();
+};
+
+module.exports = {
+  getADEMonitoringItem,
+  getADEMonitoringItemProtobuf,
+};
---
+++ GPCCHS_E_VIS/src/client/src/main/js/client/adapters/dc/dataControllerUtils/ADEMonitoringLaw.stub.js
@@ -0,0 +1,24 @@
+const ProtoBuf = require('protobufjs');
+const applyOverride = require('../applyOverride');
+const Adapter = require('./ADEMonitoringLaw');
+
+const Builder = new ProtoBuf.Root()
+  .loadSync(`${__dirname}/ADEMonitoringLaw.proto`, { keepCase: true })
+  .lookup('dataControllerUtils.protobuf.ADEMonitoringLaw');
+
+const getADEMonitoringLaw = override => applyOverride({
+  type: 'ster',
+  expectedValue: '42',
+  maskType: 'bat',
+  applicationCondition: '',
+}, override);
+
+const getADEMonitoringLawProtobuf = override => {
+  const toEncode = getADEMonitoringLaw(override);
+  return Builder.encode(Adapter.encode(toEncode)).finish();
+};
+
+module.exports = {
+  getADEMonitoringLaw,
+  getADEMonitoringLawProtobuf,
+};

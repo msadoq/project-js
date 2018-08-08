483d3fa1bcd66c79daa57a499e3a1530538be67c
Yann Lastapis <yann.lastapis.external@atos.net>
2018-08-08 12:18:57 +0000

    [HL] Fix pic . . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/scripts/adaptersInstall.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/scripts/adaptersInstall.js
@@ -15,7 +15,7 @@ const { execSync } = require('child_process');
 
 const ADAPTERS_PATH = './adapters/';
 
-const adapters = ['isis', 'dc', 'pusActor'];
+const adapters = ['isis', 'dc'];
 
 adapters.forEach(adapter => (
   execSync('npm i', { stdio: [0, 1, 2], cwd: join(ADAPTERS_PATH, adapter) })

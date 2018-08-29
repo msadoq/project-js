37cd991588ec2ea5e3dd96204e740641d82e4fba
Jean-Baptiste RABREAU <jbrabrea@isisdev182.cnes-isis.toulouse.atos.net>
2018-08-29 12:34:34 +0000

    [FT:#11919]table header tooltip . .

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/PUS144View/store/configurationReducer.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/viewManager/PUS144View/store/configurationReducer.js
@@ -3,31 +3,6 @@ import _getOr from 'lodash/fp/getOr';
 import _has from 'lodash/fp/has';
 import _set from 'lodash/fp/set';
 
-const initialConfiguration = {
-  tables: {
-    onBoardPartitions: {
-      name: 'On-Board Partitions',
-      sorting: {
-        colName: 'serviceApidName',
-        direction: 'DESC',
-      },
-      cols: [
-        { label: 'APID Name', title: 'serviceApidName', displayed: true },
-        { label: 'ID', title: 'partitionId', displayed: true },
-        { label: 'File ID', title: 'fileId', displayed: true },
-        { label: 'File Type', title: 'fileType', displayed: true },
-        { label: 'File Size', title: 'fileSize', displayed: true },
-        { label: 'Creation Time', title: 'fileCreationTime', displayed: true },
-        { label: 'Protection Status', title: 'fileProtectionStatus', displayed: true },
-        { label: 'Mode', title: 'fileMode', displayed: true },
-        { label: 'Address', title: 'fileAddress', displayed: true },
-        { label: 'Uploaded Checksum', title: 'uploadedFileChecksum', displayed: true },
-        { label: 'Reported Checksum', title: 'computedFileChecksum', displayed: true },
-      ],
-    },
-  },
-};
-
 export default (stateConf = {}, action) => {
   switch (action.type) {
     case types.WS_VIEW_RELOAD:
@@ -36,7 +11,6 @@ export default (stateConf = {}, action) => {
     case types.WS_WORKSPACE_OPENED:
     case types.WS_VIEW_ADD_BLANK: {
       return {
-        ...initialConfiguration,
         ...action.payload.view.configuration,
       };
     }

0d0e889b96f972cf0470d270a5484e24e50fc853
Julien MIRA <jmira@cnes-isis.toulouse.atos.net>
2018-08-30 14:10:07 +0000

    [FT:#11919] fix compute missing interval for pus

--- GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/observers/computeMissingPusIntervals.js
+++ GPCCHS_E_VIS/src/client/src/main/js/client/lib/store/observers/computeMissingPusIntervals.js
@@ -1,3 +1,4 @@
+import _ from 'lodash/fp';
 import _get from 'lodash/get';
 import _each from 'lodash/each';
 import _isEqual from 'lodash/isEqual';
@@ -22,25 +23,30 @@ export default function computeMissingPusIntervals(state, dataMap, previousDataM
         _get(dataMap, ['expectedPusIntervals', tbdId, localId, 'expectedInterval']);
       needed = retrieveNeededIntervals(knownInterval, expectedInterval);
 
+      const lastViews = _.getOr([], ['perPusId', tbdId, 'views'], previousDataMap);
+
       if (!needed.length) {
         // Check if there is not a new view requesting the data
-        const lastViews = previousDataMap.perPusId[tbdId].views;
         // same views or view removed
         if (_isEqual(views, lastViews) || _isEqual(_intersection(views, lastViews), views)) {
           return;
         }
         // If a new view has been opened, add a request on the whole expected interval
-        // Normally data are in cache as a request has been already done before
-        needed.push(expectedInterval);
+        const query = {
+          dataId,
+          interval: expectedInterval,
+        };
+        queries[tbdId] = query;
       }
 
-      _each(views, (() => {
+      _each(views, ((view) => {
+        const newView = lastViews.indexOf(view) === -1;
         const playingTimebarId = getPlayingTimebarId(state);
         const timebarUuid = dataMap.perPusId[tbdId].localIds[localId].timebarUuid;
         // this condition is checking if timebar is currently in playing mode
         // if it is, data are given by the forecast
         // view is not needing data
-        if ((!playingTimebarId || timebarUuid !== playingTimebarId) && !queries[tbdId]) {
+        if ((!playingTimebarId || timebarUuid !== playingTimebarId || newView) && !queries[tbdId]) {
           const query = {
             dataId,
             interval: needed[0],
